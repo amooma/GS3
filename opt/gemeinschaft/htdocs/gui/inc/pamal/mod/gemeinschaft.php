3a6f27ac713e4bde6eb2355aa766095b34d20625
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-04 18:25:25 +0000

    git-svn-id: https://svn.amooma.com/gemeinschaft/branches/1.0@1921 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/htdocs/gui/inc/pamal/mod/gemeinschaft.php
@@ -0,0 +1,60 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+defined('PAMAL_DIR') or die('No direct access.');
+
+
+#####################################################################
+#	authenticates user by Gemeinschaft's internal database
+#####################################################################
+class PAMAL_auth_gemeinschaft extends PAMAL_auth
+{
+	# constructor:
+	function PAMAL_auth_gemeinschaft() {
+		$this->_user = $this->_getUser();
+	}
+	
+	# private, returns the user:
+	function _getUser() {
+		$user_entered = trim( @$_REQUEST['login_user'] );
+		$pwd_entered =        @$_REQUEST['login_pwd' ]  ;
+		
+		if ($user_entered=='' || $pwd_entered=='')
+			return false;
+		
+		$db = gs_db_slave_connect();
+		if (!$db) return false;
+		
+		$pin = $db->executeGetOne( 'SELECT `pin` FROM `users` WHERE `user`=\''. $db->escape($user_entered) .'\'' );
+		if ($pin == $pwd_entered)
+			return $user_entered;
+		
+		return false;
+	}
+}
+
+?>
