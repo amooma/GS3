f1da9a58ca22af70bdcb2dfdd7f95d1ce75e6f45
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-11 01:56:58 +0000

    @chmod
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2006 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/log.php
+++ opt/gemeinschaft/inc/log.php
@@ -52,7 +52,7 @@ function gs_log( $level, $msg, $logfile=null )
 		}
 		$logfiles[$logfile] = @ fOpen($logfile, 'ab');  # probably permission denied
 		if (! $logfiles[$logfile]) return false;
-		chmod($logfile, 00666);  # in octal mode!
+		@chmod($logfile, 00666);  # in octal mode!
 	}
 	$vLevel = @ $levels[$level];
 	if (! $vLevel) $vLevel = 'UNKNOWN';
