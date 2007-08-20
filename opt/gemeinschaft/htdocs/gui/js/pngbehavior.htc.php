4f08b3a6b79dd406dc5d739dfdb025ed1554877a
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-20 01:13:44 +0000

    HTTP-Caching
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2069 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/js/pngbehavior.htc.php
+++ opt/gemeinschaft/htdocs/gui/js/pngbehavior.htc.php
@@ -1,9 +1,31 @@
 <?php
 
 header( 'Content-Type: text/x-component' );
+header( 'Pragma: cache' );
+header( 'Last-Modified: '. gmDate('D, d M Y 00:00:00') .' GMT');
+header( 'Expires: '. gmDate('D, d M Y 23:59:59') .' GMT');
+header( 'ETag: '. date('Ymd') );
 
-?>
-<public:component lightWeight="true">
+function _not_modified()
+{
+	header( 'HTTP/1.0 304 Not Modified', true, 304 );
+	header( 'Status: 304 Not Modified', true, 304 );
+	die();
+}
+
+if (array_key_exists('HTTP_IF_NONE_MATCH', $_SERVER)
+&&  $_SERVER['HTTP_IF_NONE_MATCH'] == date('Ymd') ) {
+	_not_modified();
+}
+$tmp = gmDate('D, d M Y');
+if (array_key_exists('HTTP_IF_MODIFIED_SINCE', $_SERVER)
+&&  subStr($_SERVER['HTTP_IF_MODIFIED_SINCE'],0,strLen($tmp)) == $tmp ) {
+	_not_modified();
+}
+
+ob_start();
+
+?><public:component lightWeight="true">
 <public:attach event="onpropertychange" onevent="pngb_property_changed()" />
 <public:attach event="onbeforeprint" onevent="pngb_before_print()" for="window" />
 <public:attach event="onafterprint" onevent="pngb_after_print()" for="window" />
@@ -31,7 +53,7 @@ header( 'Content-Type: text/x-component' );
 License info from the URL above: "we've decided to re-license our
 components under the Apache Software License 2.0, allowing anyone to
 use them free of charge."
-Some changes by me.
+Some improvements by me.
 -- Philipp Kempgen
 */
 
@@ -95,3 +117,12 @@ if (pngb_supported) pngb_fix_png();
 
 </script>
 </public:component>
+
+<?php
+
+if (! headers_sent())
+	header( 'Content-Length: '. (int)ob_get_length() );
+ob_end_flush();
+die();
+
+?>
