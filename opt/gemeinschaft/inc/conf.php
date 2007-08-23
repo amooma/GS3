b2d188d3615973e379933bff4bcaa3a74d7f9cc0
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-23 11:34:17 +0000

    in manchen Ländern muß auch beim Wählen im internationalen Format vor der Ortskennzahl die Verkehrsausscheidungsziffer stehen (so als würde man in Dtl. 00490261... wählen)
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2109 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/conf.php
+++ opt/gemeinschaft/inc/conf.php
@@ -170,20 +170,25 @@ define( 'GS_PROV_SNOM_PB_NUM_RESULTS', 15 );
 
 
 /***********************************************************
-*    CANONICAL PHONE NUMBERS
+*    CANONICAL PHONE NUMBERS (FQTN)
 ***********************************************************/
 
-define( 'GS_CANONIZE_INTL_PREFIX' , '00'  );  # Präfix international
-define( 'GS_CANONIZE_COUNTRY_CODE', '49'  );  # Landesvorwahl ohne 00
-define( 'GS_CANONIZE_NATL_PREFIX' , '0'   );  # Präfix national
-define( 'GS_CANONIZE_AREA_CODE'   , '251' );  # Ortsvorwahl ohne 0
-define( 'GS_CANONIZE_LOCAL_BRANCH', '702' );  # private Kopfnummer
-# if all you have is a single phone number put your local number in
-# here, i.e. the rest after the area code
-define( 'GS_CANONIZE_SPECIAL'     , '110,112,19222' );
-# these numbers will not be prefixed with anything
-define( 'GS_CANONIZE_CBC_PREFIX'  , '010'  );  # Call-by-Call-Präfix
-# Germany: 010
+define( 'GS_CANONIZE_INTL_PREFIX' , '00'  ); # international prefix. Do not use "+",
+                                             # we know the canonical format is "+".
+define( 'GS_CANONIZE_COUNTRY_CODE', '49'  ); # country code (Landesvorwahl) without
+                                             # prefix
+define( 'GS_CANONIZE_NATL_PREFIX' , '0'   ); # National prefix (Verkehrsausscheidungsziffer)
+define( 'GS_CANONIZE_AREA_CODE'   , '251' ); # Area code (Ortsvorwahl) without prefix
+define( 'GS_CANONIZE_AREA_CODE_INTL',false); # Whether the area code needs the
+                                             # national prefix even when dialing
+                                             # in international format (in Italy)
+define( 'GS_CANONIZE_LOCAL_BRANCH', '702' ); # Private branch (private Kopfnummer).
+                                             # If all you have is a single phone
+                                             # number put your local number in here,
+                                             # i.e. the rest after the area code
+define( 'GS_CANONIZE_SPECIAL', '110,112,19222' ); # these numbers will not be
+                                                  # prefixed with anything
+define( 'GS_CANONIZE_CBC_PREFIX'  , '010' ); # Call-by-Call prefix (Germany: 010)
 
 
 /***********************************************************
