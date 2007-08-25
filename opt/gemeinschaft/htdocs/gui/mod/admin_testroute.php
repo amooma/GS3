92928458c02a0bbb0d8496ea93bfb86008e032d9
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-25 23:09:09 +0000

    jetzt mit Routing-Test ("Welches Pattern matcht wann auf die gewählte Nummer?")
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2173 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/admin_testroute.php
+++ opt/gemeinschaft/htdocs/gui/mod/admin_testroute.php
@@ -50,11 +50,8 @@ $sudo_url = (@$_SESSION['sudo_user']['name'] == @$_SESSION['real_user']['name'])
 
 ?>
 
-<p class="text"><?php echo __('$$$ Hier k&ouml;nnen Sie &uuml;berpr&uuml;fen, wie nach extern gew&auml;hlte Telefonnummern entsprechend Ihrer Einstellungen kanonisiert werden.'); ?></p>
+<p class="text"><?php echo __('$$$ Hier k&ouml;nnen Sie &uuml;berpr&uuml;fen, wie nach extern gew&auml;hlte Telefonnummern entsprechend Ihrer Einstellungen kanonisiert und geroutet werden.'); ?></p>
 
-<?php if (! gs_get_conf('GS_CANONIZE_OUTBOUND')) { ?>
-<p class="text">(<?php echo __('$$$ Kanonisierung ist nicht aktiviert.'); ?>)</p>
-<?php } ?>
 
 <form method="post" action="<?php echo GS_URL_PATH; ?>">
 <?php echo gs_form_hidden($SECTION, $MODULE); ?>
@@ -63,13 +60,18 @@ $sudo_url = (@$_SESSION['sudo_user']['name'] == @$_SESSION['real_user']['name'])
 <label for="ipt-number"><?php echo __('$$$ Telefonnummer'); ?>:</label><br />
 <input type="text" name="number" id="ipt-number" value="<?php echo $number; ?>" size="25" maxlength="30" />
 
-<input type="submit" value="<?php echo __('$$$ Kanonisieren'); ?>" />
+<input type="submit" value="<?php echo __('$$$ Testen'); ?>" />
 </form>
 <br />
 
 
+
+<h3><?php echo __('$$$ Kanonisierung'); ?></h3>
+
 <?php
-$canonical = new CanonicalPhoneNumber( $number );
+###################################################### CANONIZATION {
+if (gs_get_conf('GS_CANONIZE_OUTBOUND')) {
+	$canonical = new CanonicalPhoneNumber( $number );
 ?>
 
 <table cellspacing="1">
@@ -126,6 +128,7 @@ $canonical = new CanonicalPhoneNumber( $number );
 <br />
 <p>
 <?php
+$dial = '';
 switch (@$canonical->errt) {
 	case 'empty':
 		echo "$$$ Keine Telefonnummer angegeben.";
@@ -138,18 +141,132 @@ switch (@$canonical->errt) {
 		break;
 	case '':
 	default:
-		echo sPrintF("$$$ Die Nummer w&uuml;rde als <b>%s</b> gew&auml;hlt.", $canonical->dial);
+		$dial = $canonical->dial;
+		echo sPrintF("$$$ Die Nummer w&uuml;rde als <b>%s</b> mit den Mustern in der Routing-Tabelle verglichen.", $dial);
 }
 ?>
 </p>
 
+<?php
+}
+###################################################### CANONIZATION }
 
-<br />
+
+###################################################### NO CANONIZATION {
+else {
+	echo '<p class="text">(', __('$$$ Kanonisierung ist nicht aktiviert!') ,')</p>', "\n";
+	$dial = trim($number);
+}
+###################################################### NO CANONIZATION }
+
+
+
+
+###################################################### ROUTING {
+if ($dial != '') {
+?>
+
+<h3><?php echo __('$$$ Zutreffende Suchmuster'); ?></h3>
+
+<table cellspacing="1">
+<thead>
+<tr>
+	<th><?php echo __('$$$ Wochentage'); ?></th>
+	<th><?php echo __('$$$ Uhrzeit'); ?></th>
+	<th><?php echo __('$$$ Muster'); ?></th>
+	<?php /* ?>
+	<th><?php echo sPrintF(__('$$$ Route Prio. %d'), 1); ?></th>
+	<th><?php echo sPrintF(__('$$$ Route Prio. %d'), 2); ?></th>
+	<th><?php echo sPrintF(__('$$$ Route Prio. %d'), 3); ?></th>
+	<?php */ ?>
+	<th><?php echo __('$$$ Route'); ?></th>
+	<th><?php echo __('$$$ Fallback'); ?></th>
+	<th><?php echo __('$$$ Fallback'); ?></th>
+</tr>
+</thead>
+<tbody>
 
 <?php
-/*
-echo "<pre>\n";
-print_r($canonical);
-echo "</pre>\n";
-*/
+	$wdays = array( 'mo'=>'Mo', 'tu'=>'Di', 'we'=>'Mi', 'th'=>'Do', 'fr'=>'Fr', 'sa'=>'Sa', 'su'=>'So',  );
+	
+	$rsR = $DB->execute(
+'SELECT
+	`pattern` `pat`,
+	`d_mo`, `d_tu`, `d_we`, `d_th`, `d_fr`, `d_sa`, `d_su`,
+	SUBSTR(`h_from`,1,5) `hf`, SUBSTR(`h_to`,1,5) `ht`,
+	`gw_grp_id_1` `gg1`, `gw_grp_id_2` `gg2`, `gw_grp_id_3` `gg3`
+FROM `routes` USE INDEX(`ord`)
+WHERE `active`=1
+ORDER BY `ord`'
+	);
+	$i=0;
+	while ($route = $rsR->fetchRow()) {
+		if (! @preg_match( '/'.$route['pat'].'/', $dial )) continue;
+		
+		echo '<tr class="', ($i%2 ? 'even':'odd') ,'">', "\n";
+		
+		/*
+		echo '<td>', "\n";
+		$wd_out = array();
+		foreach ($wdays as $col => $v)
+			if (@$route['d_'.$col]) $wd_out[] = $v;
+		echo htmlEnt(implode(', ', $wd_out));
+		echo '</td>', "\n";
+		*/
+		echo '<td class="pre"><nobr>';
+		foreach ($wdays as $col => $v) {
+			echo (@$route['d_'.$col] ? htmlEnt($v) : '&nbsp;&nbsp;'), ' ';
+		}
+		echo '</nobr></td>', "\n";
+		
+		echo '<td class="pre">';
+		/*
+		echo (subStr($route['hf'],2,3) != ':00')
+			? $route['hf']
+			: subStr($route['hf'],0,2) .'<span style="color:#999;">'. subStr($route['hf'],2,3) .'</span>';
+		*/
+		echo subStr($route['hf'],0,2) .'<sup>'. subStr($route['hf'],3,2) .'</sup>';
+		echo '&thinsp;-&thinsp;';
+		echo subStr($route['ht'],0,2) .'<sup>'. subStr($route['ht'],3,2) .'</sup>';
+		echo ' </td>', "\n";
+		
+		echo '<td class="pre">', htmlEnt($route['pat']) ,' </td>', "\n";
+		
+		$gate_grps = array();
+		if ($route['gg1'] != 0) $gate_grps[] = (int)$route['gg1'];
+		if ($route['gg2'] != 0) $gate_grps[] = (int)$route['gg2'];
+		if ($route['gg3'] != 0) $gate_grps[] = (int)$route['gg3'];
+		unset($route);
+		$gg_cnt = 0;
+		foreach ($gate_grps as $ggrp_id) {
+			$gg_title = $DB->executeGetOne( 'SELECT `title` FROM `gate_grps` WHERE `id`='. $ggrp_id );
+			if ($gg_title === null || $gg_title === false) continue;
+			echo '<td>', htmlEnt($gg_title) ,'</td>';
+			++$gg_cnt;
+		}
+		for ($k=$gg_cnt; $k<3; ++$k) {
+			echo '<td>&nbsp;</td>';
+		}
+		
+		
+		echo '</tr>', "\n";
+	}
+	
+	
 ?>
+
+</tbody>
+</table>
+
+<br />
+<p class="text"><small>(<?php echo __('$$$ Dabei ist die Reihenfolge entscheidend; das erste zutreffende Muster gewinnt.'); ?>)</small></p>
+
+<?php
+}
+###################################################### ROUTING }
+
+?>
+
+<br />
+<br />
+
