5ac7daac9bc25e6740b3bcb97082e9e2cb3c4b9d
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-28 17:48:25 +0000

    alten Kram rausgeworfen, Fullscreen-Button eingebaut
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2197 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/stats_qclassical.php
+++ opt/gemeinschaft/htdocs/gui/mod/stats_qclassical.php
@@ -26,6 +26,13 @@
 * MA 02110-1301, USA.
 \*******************************************************************/
 
+######################################################
+##
+##   ALL STRINGS IN HERE NEED TO BE TRANSLATED!
+##
+######################################################
+
+
 defined('GS_VALID') or die('No direct access.');
 include_once( GS_DIR .'inc/pcre_check.php' );
 
@@ -72,7 +79,7 @@ if ($action == 'report') {
 <?php echo gs_form_hidden($SECTION, $MODULE); ?>
 <input type="hidden" name="action" value="report" />
 
-<label for="ipt-queue_id"><?php echo __('$$$ Warteschlange'); ?>:</label>
+<label for="ipt-queue_id"><?php echo __('Warteschlange'); ?>:</label>
 <select name="queue_id" id="ipt-queue_id">
 <?php
 $rs = $DB->execute( 'SELECT `_id`, `name`, `_title` FROM `ast_queues` ORDER BY `name`' );
@@ -84,7 +91,7 @@ while ($r = $rs->fetchrow()) {
 
 &nbsp;&nbsp;&nbsp;
 
-<label for="ipt-month"><?php echo __('$$$ Monat'); ?>:</label>
+<label for="ipt-month"><?php echo __('Monat'); ?>:</label>
 <select name="month" id="ipt-month">
 <?php
 $t = time();
@@ -96,7 +103,7 @@ for ($i=-3; $i<=0; ++$i) {
 
 &nbsp;&nbsp;&nbsp;
 
-<input type="submit" value="<?php echo __('$$$ Report'); ?>" />
+<input type="submit" value="<?php echo __('Report'); ?>" />
 </form>
 
 <hr />
@@ -117,25 +124,62 @@ $y         = (int)date('Y', $t);
 $m         = (int)date('n', $t);
 $today_day = (int)date('j', $t);
 
+
 ?>
 
-<div style="position:absolute; left:189px; right:19px; top:14em; bottom:14px; overflow:scroll; border:1px solid #ccc; background:#fff;">
+<?php /*
+<div id="chart" style="position:absolute; left:189px; right:12px; top:14em; bottom:10px; overflow:scroll; border:1px solid #ccc; background:#fff;">
+*/ ?>
 
-<table cellspacing="1" class="phonebook">
+
+<script type="text/javascript">
+function chart_fullscreen_toggle()
+{
+	var chart = document.getElementById('chart');
+	var toggle = document.getElementById('chart-fullscreen-toggle');
+	if (chart && toggle) {
+		if (chart.style.position == 'absolute') {
+			chart.style.position = 'static';
+			chart.style.top = '';
+			chart.style.left = '';
+			chart.style.right = '';
+			chart.style.bottom = '';
+			chart.style.background = 'transparent';
+			toggle.src = '<?php echo GS_URL_PATH; ?>crystal-svg/16/act/window_fullscreen.png';
+		} else {
+			chart.style.position = 'absolute';
+			chart.style.top = '0';
+			chart.style.left = '0';
+			chart.style.right = '0';
+			chart.style.bottom = '0';
+			chart.style.background = '#fff';
+			toggle.src = '<?php echo GS_URL_PATH; ?>crystal-svg/16/act/window_nofullscreen.png';
+		}
+	}
+}
+</script>
+
+
+
+<div id="chart" style="">
+<img id="chart-fullscreen-toggle" class="fr" style="cursor:pointer; margin:4px 7px 0 0;" title="Fullscreen" alt="Fullscreen" onclick="chart_fullscreen_toggle();" src="<?php echo GS_URL_PATH; ?>crystal-svg/16/act/window_fullscreen.png" />
+<br style="clear:right;" />
+
+<table cellspacing="1" class="phonebook" style="border:1px solid #ccc; background:#fff;">
 <thead>
 <tr>
-	<th style="font-weight:normal;"><?php echo __('$$$ Tag'); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Tag'); ?></th>
 	<th style="font-weight:normal;"><?php echo __('Anrufe'); ?></th>
-	<th style="font-weight:normal;"><?php echo __('Angen.'); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Angenommen'); ?></th>
 	<th style="font-weight:normal;"><?php echo __('Aufgelegt'); ?></th>
 	<th style="font-weight:normal;"><?php echo __('Timeout'); ?></th>
 	<th style="font-weight:normal;"><?php echo __('keine Ag.'); ?></th>
-	<th style="font-weight:normal;"><?php echo __('Erfolg'); ?></th>
-	<th style="font-weight:normal;"><?php echo '&le;', _secs_to_minsecs($service_level); ?></th>
-	<th style="font-weight:normal;"><?php echo '&gt;', _secs_to_minsecs($service_level); ?></th>
-	<th style="font-weight:normal;"><?php echo '&empty; Gespr.'; ?></th>
-	<th style="font-weight:normal;"><?php echo __('tW') ,' &le;', _secs_to_minsecs($waittime_level); ?></th>
-	<th style="font-weight:normal;"><?php echo __('tW') ,' &gt;', _secs_to_minsecs($waittime_level); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Erfolgsquote'); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Dauer'), ' &le;', _secs_to_minsecs($service_level); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Dauer'), ' &gt;', _secs_to_minsecs($service_level); ?></th>
+	<th style="font-weight:normal;"><?php echo '&empty; ', __('Dauer'); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Wartez.') ,' &le;', _secs_to_minsecs($waittime_level); ?></th>
+	<th style="font-weight:normal;"><?php echo __('Wartez.') ,' &gt;', _secs_to_minsecs($waittime_level); ?></th>
 </tr>
 </thead>
 <tbody>
@@ -319,216 +363,3 @@ AND '. $sql_time
 
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php return; ?>
-
-<table cellspacing="1" class="phonebook">
-<thead>
-<tr>
-	<th><?php echo __('$$$ Aktiv'); ?></th>
-	<th><?php echo __('$$$ Muster'); ?><sup>[1]</sup></th>
-	<th><?php echo __('$$$ Wochentage'); ?></th>
-	<th><?php echo __('$$$ Uhrzeit'); ?></th>
-	<th><?php echo __('$$$ Gateway / Fallback'); ?></th>
-	<th><?php echo __('$$$ Reihenfolge'); ?></th>
-</tr>
-</thead>
-<tbody>
-
-<?php
-$rs = $DB->execute(
-'SELECT `id`, `title`, `type`
-FROM `gate_grps`
-ORDER BY `title`'
-);
-$gate_grps = array();
-while ($r = $rs->fetchRow()) {
-	$gate_grps[(int)$r['id']] = array(
-		'title'      => $r['title'],
-		'title_html' => htmlEnt($r['title']),
-		'type'       => $r['type']
-	);
-}
-
-$rs = $DB->execute(
-'SELECT
-	`id`, `active`, `pattern`,
-	`d_mo`, `d_tu`, `d_we`, `d_th`, `d_fr`, `d_sa`, `d_su`, `h_from`, `h_to`,
-	`gw_grp_id_1` `gg1`, `gw_grp_id_2` `gg2`, `gw_grp_id_3` `gg3`, `descr`
-FROM `routes`
-ORDER BY `ord`'
-);
-$i=0;
-while ($route = $rs->fetchRow()) {
-	$id = $route['id'];
-	echo '<tr class="', (($i%2) ? 'even':'odd'), '">', "\n";
-	
-	echo '<td>';
-	echo '<input type="checkbox" name="r_',$id,'_active" value="1" ', ($route['active'] ? 'checked="checked" ' : ''), '/>';
-	echo '</td>', "\n";
-	
-	echo '<td>';
-	echo '<input type="text" name="r_',$id,'_pattern" value="', htmlEnt($route['pattern']), '" size="17" maxlength="30" class="pre" style="font-weight:bold;" />';
-	echo '</td>', "\n";
-	
-	echo '<td>';
-	foreach ($wdaysl as $col => $v) {
-		echo '<nobr><input type="checkbox" name="r_',$id,'_d_',$col,'" id="ipt-r_',$id,'_d_',$col,'" value="1" ', ($route['d_'.$col] ? 'checked="checked" ' : ''), '/>';
-		echo '<label for="ipt-r_',$id,'_d_',$col,'">', $v, '</label></nobr>';
-	}
-	echo '</td>', "\n";
-	
-	echo '<td>';
-	$tmp = explode(':', $route['h_from']);
-	$hf = (int)lTrim(@$tmp[0], '0-');
-	if     ($hf <  0) $hf =  0;
-	elseif ($hf > 23) $hf = 23;
-	$hf = str_pad($hf, 2, '0', STR_PAD_LEFT);
-	$mf = (int)lTrim(@$tmp[1], '0-');
-	if     ($mf <  0) $mf =  0;
-	elseif ($mf > 59) $mf = 59;
-	$mf = str_pad($mf, 2, '0', STR_PAD_LEFT);
-	echo '<nobr><input type="text" name="r_',$id,'_h_from_h" value="', $hf, '" size="2" maxlength="2" />';
-	echo ':<input type="text" name="r_',$id,'_h_from_m" value="', $mf, '" size="2" maxlength="2" /></nobr>';
-	$tmp = explode(':', $route['h_to']);
-	$ht = (int)lTrim(@$tmp[0], '0-');
-	if     ($ht <  0) $ht =  0;
-	elseif ($ht > 24) $ht = 24;
-	$ht = str_pad($ht, 2, '0', STR_PAD_LEFT);
-	$mt = (int)lTrim(@$tmp[1], '0-');
-	if     ($mt <  0) $mt =  0;
-	elseif ($mt > 59) $mt = 59;
-	$mt = str_pad($mt, 2, '0', STR_PAD_LEFT);
-	if ($ht.':'.$mt < $hf.':'.$mf) {
-		$ht = $hf;
-		$hm = $mf;
-	}
-	echo ' - <nobr><input type="text" name="r_',$id,'_h_to_h" value="', $ht, '" size="2" maxlength="2" />';
-	echo ':<input type="text" name="r_',$id,'_h_to_m" value="', $mt, '" size="2" maxlength="2" /></nobr>';
-	echo '</td>', "\n";
-	
-	echo '<td rowspan="2">';
-	$gw_grp_idxs = array(1,2,3);
-	foreach ($gw_grp_idxs as $gw_grp_idx) {
-		echo '<select name="r_',$id,'_ggrpid',$gw_grp_idx,'">', "\n";
-		$route_ggrp_id = $route['gg'.$gw_grp_idx];
-		echo '<option value=""', ($route_ggrp_id == 0 || ! array_key_exists($route_ggrp_id, $gate_grps) ? ' selected="selected"' : ''), '>', '-', '</option>', "\n";
-		foreach ($gate_grps as $ggid => $gg) {
-			echo '<option value="', $ggid, '"', ($ggid == $route_ggrp_id ? ' selected="selected"' : ''), '>', $gg['title_html'], '</option>', "\n";
-		}
-		echo '</select><br />', "\n";
-	}
-	echo '</td>', "\n";
-	
-	echo '<td rowspan="2" class="r transp">';
-	if ($i > 0)
-		echo '<a href="', gs_url($SECTION, $MODULE), '&amp;action=move-up&amp;id=', $route['id'], '"><img alt="&uarr;" src="', GS_URL_PATH, 'img/move_up.gif" /></a>';
-	else
-		echo '<img alt="&uarr;" src="', GS_URL_PATH, 'img/move_up_d.gif" />';
-	if ($i < $rs->numRows()-1)
-		echo ' <a href="', gs_url($SECTION, $MODULE), '&amp;action=move-down&amp;id=', $route['id'], '"><img alt="&darr;" src="', GS_URL_PATH, 'img/move_down.gif" /></a>';
-	else
-		echo ' <img alt="&darr;" src="', GS_URL_PATH, 'img/move_down_d.gif" />';
-	echo ' &nbsp; <a href="', gs_url($SECTION, $MODULE), '&amp;action=del&amp;id=', $route['id'], '"><img alt="-;" src="', GS_URL_PATH, 'img/minus.gif" /></a>';
-	echo '</td>', "\n";
-	
-	echo '</tr>', "\n";
-	
-	echo '<tr class="', (($i%2) ? 'even':'odd'), '">', "\n";
-	echo '<td colspan="2" class="r"><label for="ipt-r_',$id,'_descr">', __('$$$ Beschr.:'), '</label></td>';
-	
-	echo '<td colspan="2">';
-	echo '<input type="text" name="r_',$id,'_descr" id="ipt-r_',$id,'_descr" value="', htmlEnt(trim($route['descr'])), '" size="45" maxlength="60" style="width:97%; font-weight:bold;" />';
-	echo '</td>', "\n";
-	
-	echo '</tr>', "\n";
-	++$i;
-}
-
-
-echo '<tr>', "\n";
-echo '<td colspan="5" class="transp">&nbsp;</td>', "\n";
-echo '<td class="r transp">';
-echo '<input type="submit" value="', __('$$$ Speichern'), '" />';
-echo '</td>', "\n";
-echo '</tr>', "\n";
-
-
-$id = 0;
-echo '<tr class="', ((++$i % 2 == 0) ? 'even':'odd'), '">', "\n";
-
-echo '<td>';
-echo '<input type="checkbox" name="r_',$id,'_active" value="1" checked="checked" />';
-echo '</td>', "\n";
-
-echo '<td>';
-echo '<input type="text" name="r_',$id,'_pattern" value="" size="17" maxlength="30" class="pre" style="font-weight:bold;" />';
-echo '</td>', "\n";
-
-echo '<td>';
-foreach ($wdaysl as $col => $v) {
-	echo '<nobr><input type="checkbox" name="r_',$id,'_d_',$col,'" id="ipt-r_',$id,'_d_',$col,'" value="1" checked="checked" />';
-	echo '<label for="ipt-r_',$id,'_d_',$col,'">', $v, '</label></nobr>';
-}
-echo '</td>', "\n";
-
-echo '<td>';
-echo '<nobr><input type="text" name="r_',$id,'_h_from_h" value="00" size="2" maxlength="2" />';
-echo ':<input type="text" name="r_',$id,'_h_from_m" value="00" size="2" maxlength="2" /></nobr>';
-echo ' - <nobr><input type="text" name="r_',$id,'_h_to_h" value="24" size="2" maxlength="2" />';
-echo ':<input type="text" name="r_',$id,'_h_to_m" value="00" size="2" maxlength="2" /></nobr>';
-echo '</td>', "\n";
-
-echo '<td rowspan="2">';
-foreach ($gw_grp_idxs as $gw_grp_idx) {
-	echo '<select name="r_',$id,'_ggrpid',$gw_grp_idx,'">', "\n";
-	$route_ggrp_id = $route['gg'.$gw_grp_idx];
-	echo '<option value="" selected="selected">-</option>', "\n";
-	foreach ($gate_grps as $ggid => $gg) {
-		echo '<option value="', $ggid, '">', $gg['title_html'], '</option>', "\n";
-	}
-	echo '</select><br />', "\n";
-}
-echo '</td>', "\n";
-
-echo '<td rowspan="2" class="r transp">&nbsp;';
-echo '</td>', "\n";
-
-echo '</tr>', "\n";
-
-echo '<tr class="', (($i % 2 == 0) ? 'even':'odd'), '">', "\n";
-echo '<td colspan="2" class="r"><label for="ipt-r_',$id,'_descr">', __('$$$ Beschr.:'), '</label></td>';
-
-echo '<td colspan="2">';
-echo '<input type="text" name="r_',$id,'_descr" id="ipt-r_',$id,'_descr" value="', htmlEnt(trim($route['descr'])), '" size="45" maxlength="60" style="width:97%;font-weight:bold;" />';
-echo '</td>', "\n";
-
-echo '</tr>', "\n";
-
-?>
-
-</tbody>
-</table>
-
-<br />
-<p class="text"><small><sup>[1]</sup> <?php
-/*
-echo __('$$$ PCRE-Syntax ohne <code>/</code> als Begrenzer, d.h. <code>^</code> f&uuml;r den Anfang, <code>$</code> f&uuml;r das Ende, z.B. <code>[5-8]</code> oder <code>[57]</code> f&uuml;r Ziffern-Bereiche, <code>+</code> f&uuml;r eine Wiederholung des vorangehenden Zeichens (1 oder mehr) oder <code>*</code> f&uuml;r 0 oder mehr. Zus&auml;tzlich m&ouml;glich: <code>x</code> f&uuml;r <code>[0-9]</code>, <code>z</code> f&uuml;r <code>[1-9]</code>');
-*/
-echo __('$$$ PCRE-Syntax (&quot;Perl Compatible Regular Expression&quot;) ohne <code>/</code> als Begrenzer, d.h. <code>^</code> f&uuml;r den Anfang, <code>$</code> f&uuml;r das Ende, z.B. <code>[5-8]</code> oder <code>[57]</code> f&uuml;r Ziffern-Bereiche, <code>+</code> f&uuml;r eine Wiederholung des vorangehenden Zeichens (1 oder mehr) oder <code>*</code> f&uuml;r 0 oder mehr.');
-?></small></p>
