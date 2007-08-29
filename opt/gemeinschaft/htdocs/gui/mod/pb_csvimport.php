544c1fd699f44cd8d7388d3696d0e7c0f42b3266
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-29 20:12:36 +0000

    Modul CSV-Import zum Importieren einer CSV-Datei in das persönliche Tel.Buch eines Benutzers
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2208 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/pb_csvimport.php
+++ opt/gemeinschaft/htdocs/gui/mod/pb_csvimport.php
@@ -45,17 +45,18 @@ echo '</h2>', "\n";
 
 
 $action = @$_REQUEST['action'];
-if (! in_array($action, array('', 'upload', 'preview'), true))
+if (! in_array($action, array('', 'upload', 'preview', 'import'), true))
 	$action = '';
 
 
+/*
 if ($action == ''
 &&  @array_key_exists('pb-csv-file', @$_SESSION['sudo_user'])
 &&  is_file(@$_SESSION['sudo_user']['pb-csv-file']) )
 {
 	$action = 'preview';
 }
-
+*/
 
 
 if ($action == 'upload') {
@@ -99,7 +100,9 @@ if ($action == 'upload') {
 	}
 	
 }
-if ($action == 'preview') {
+
+
+if ($action == 'preview' || $action == 'import') {
 	
 	if (! @array_key_exists('pb-csv-file', @$_SESSION['sudo_user'])
 	||  ! is_file(@$_SESSION['sudo_user']['pb-csv-file']) )
@@ -111,6 +114,7 @@ if ($action == 'preview') {
 		if (! $fh) {
 			echo 'Could not read file.';
 		} else {
+			
 			$sep  = @$_REQUEST['sep' ];
 			$encl = @$_REQUEST['encl'];
 			$enc  = @$_REQUEST['enc' ];
@@ -140,94 +144,12 @@ if ($action == 'preview') {
 			
 			$skip_1st = (bool)@$_REQUEST['skip_1st'];
 			
-			
-			echo '<form method="post" action="', GS_URL_PATH, '" enctype="multipart/form-data">', "\n";
-			echo gs_form_hidden($SECTION, $MODULE), "\n";
-			echo '<input type="hidden" name="action" value="preview" />', "\n";
-			
-			echo '<table cellspacing="1" class="phonebook">', "\n";
-			echo '<tbody>', "\n";
-			
-			echo '<tr>', "\n";
-			echo '<td>', __('Begrenzer') ,':</td>', "\n";
-			echo '<td>', "\n";
-			echo '<nobr><input type="radio" name="sep" id="ipt-sep-s" value="s" ', ($sep==='s' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-sep-s">', __('Semikolon') ,' (<code>;</code>)</label></nobr> ', "\n";
-			echo '<nobr><input type="radio" name="sep" id="ipt-sep-c" value="c" ', ($sep==='c' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-sep-c">', __('Komma') ,' (<code>,</code>)</label></nobr> ', "\n";
-			echo '<nobr><input type="radio" name="sep" id="ipt-sep-t" value="t" ', ($sep==='t' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-sep-t">', __('Tabulator') ,' (<code>\\t</code>)</label></nobr> ', "\n";
-			echo '</td>', "\n";
-			echo '</tr>', "\n";
-			
-			echo '<tr>', "\n";
-			echo '<td>', __('Umschlie&szlig;ung') ,':</td>', "\n";
-			echo '<td>', "\n";
-			echo '<nobr><input type="radio" name="encl" id="ipt-encl-q" value="q" ', ($encl==='q' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-encl-q">', __('Anf&uuml;hrungszeichen') ,' (<code>&quot;</code>)</label></nobr> ', "\n";
-			echo '<nobr><input type="radio" name="encl" id="ipt-encl-s" value="s" ', ($encl==='s' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-encl-s">', __('Apostroph') ,' (<code>&apos;</code>)</label></nobr> ', "\n";
-			echo '<nobr><input type="radio" name="encl" id="ipt-encl-n" value="n" ', ($encl==='n' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-encl-n">', __('nichts') ,'</label></nobr> ', "\n";
-			echo '</td>', "\n";
-			echo '</tr>', "\n";
-			
-			echo '<tr>', "\n";
-			echo '<td>', __('Zeichenkodierung') ,':</td>', "\n";
-			echo '<td>', "\n";
-			echo '<nobr><input type="radio" name="enc" id="ipt-enc-utf8" value="utf8" ', ($enc==='utf8' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-enc-utf8">UTF-8</label></nobr> ', "\n";
-			echo '<nobr><input type="radio" name="enc" id="ipt-enc-iso88591" value="iso88591" ', ($enc==='iso88591' ? 'checked="checked" ' : '') ,'/>',
-			     '<label for="ipt-enc-iso88591">ISO-8859-1</label></nobr> ', "\n";
-			echo '</td>', "\n";
-			echo '</tr>', "\n";
-			
-			echo '<tr>', "\n";
-			echo '<td>', __('Spalten') ,':</td>', "\n";
-			echo '<td>', "\n";
-			echo '<nobr><label for="ipt-col_ln">', __('Nachname'), ':</label>', "\n",
-			     '<input type="text" name="col_ln" id="ipt-col_ln" size="2" maxlength="2" value="', $col_ln ,'"></nobr> &nbsp;', "\n";
-			echo '<nobr><label for="ipt-col_fn">', __('Vorname'), ':</label>', "\n",
-			     '<input type="text" name="col_fn" id="ipt-col_fn" size="2" maxlength="2" value="', $col_fn ,'"></nobr> &nbsp;', "\n";
-			echo '<nobr><label for="ipt-col_nr">', __('Telefonnummer'), ':</label>', "\n",
-			     '<input type="text" name="col_nr" id="ipt-col_nr" size="2" maxlength="2" value="', $col_nr ,'"></nobr> &nbsp;', "\n";
-			echo '</td>', "\n";
-			echo '</tr>', "\n";
-			
-			echo '<tr>', "\n";
-			echo '<td>', __('Kopfzeile') ,':</td>', "\n";
-			echo '<td>', "\n";
-			echo '<input type="checkbox" name="skip_1st" id="ipt-skip_1st" value="1" ', ($skip_1st ? 'checked="checked" ' : '') ,'/>', "\n",
-			     '<label for="ipt-skip_1st">', __('erste Zeile auslassen') ,'</label>', "\n";
-			echo '</td>', "\n";
-			echo '</tr>', "\n";
-			
-			echo '</tbody>', "\n";
-			echo '</table>', "\n";
-			
-			echo '<input type="submit" value="', __('Vorschau') ,'" /><br />', "\n";	
-			echo '</form>', "\n";
-			echo '<br />', "\n";
-			
-			
-			
-			echo '<p class="text">', __('Bitte kontrollieren Sie, ob die Daten so importiert werden sollen wie sie hier angezeigt werden. Dr&uuml;cken Sie danach den Import-Knopf am Ende dieser Seite.') ,'</p>', "\n";
-			
-			echo '<table cellspacing="1" class="phonebook">', "\n";
-			echo '<thead>', "\n";
-			echo '<tr>', "\n";
-			echo '<th>', __('Nachname') ,'</th>', "\n";
-			echo '<th>', __('Vorname') ,'</th>', "\n";
-			echo '<th>', __('Tel.Nr.') ,'</th>', "\n";
-			echo '<th>', __('Tel.Nr. (Import)') ,'</th>', "\n";
-			echo '</tr>', "\n";
-			echo '</thead>', "\n";
-			echo '<tbody>', "\n";
+			$records = array();
 			$i=0;
 			while (true) {
 				$csv = ($enclosure != '')
-					? fGetCsv($fh, 8000, $separator, $enclosure)
-					: fGetCsv($fh, 8000, $separator);
+					? @fGetCsv($fh, 8000, $separator, $enclosure)
+					: @fGetCsv($fh, 8000, $separator);
 				if (! is_array($csv)) break;
 				if ($i===0 && $skip_1st) {++$i; continue;}
 				
@@ -244,47 +166,206 @@ if ($action == 'preview') {
 				$nrimp = preg_replace('/^[^0-9]+/S', '', $nrimp);
 				$nrimp = preg_replace('/[^0-9]+$/S', '', $nrimp);
 				
-				echo '<tr class="', ($i%2? 'even':'odd') ,'">', "\n";
-				echo '<td>', $ln ,'</td>', "\n";
-				echo '<td>', $fn ,'</td>', "\n";
-				echo '<td>', $nr ,'</td>', "\n";
-				echo '<td>', $nrimp ,'</td>', "\n";
-				echo '</tr>', "\n";
+				if ($action !== 'import') {
+					$records[] = array(
+						'ln'   => $ln,
+						'fn'   => $fn,
+						'nr'   => $nr,
+						'nrimp'=> $nrimp
+					);
+				} else {
+					$records[] = array(
+						'ln'   => $ln,
+						'fn'   => $fn,
+						'nrimp'=> $nrimp
+					);
+				}
 				++$i;
 			}
-			echo '</tbody>', "\n";
-			echo '</table>', "\n";
-			echo '<br />', "\n";
 			
-			
-			echo '<form method="post" action="', GS_URL_PATH, '" enctype="multipart/form-data">', "\n";
-			echo gs_form_hidden($SECTION, $MODULE), "\n";
-			echo '<input type="hidden" name="action" value="import" />', "\n";
-			
-			echo '<input type="hidden" name="sep"      value="', $sep    ,'" />', "\n";
-			echo '<input type="hidden" name="encl"     value="', $encl   ,'" />', "\n";
-			echo '<input type="hidden" name="enc"      value="', $enc    ,'" />', "\n";
-			echo '<input type="hidden" name="col_ln"   value="', $col_ln ,'" />', "\n";
-			echo '<input type="hidden" name="col_fn"   value="', $col_fn ,'" />', "\n";
-			echo '<input type="hidden" name="col_nr"   value="', $col_nr ,'" />', "\n";
-			echo '<input type="hidden" name="skip_1st" value="', (int)$skip_1st ,'" />', "\n";
-			
-			echo '<p class="text">',
-				@sPrintF(__('Daten in Ihr pers&ouml;nliches Telefonbuch (Benutzer <code>%s</code>, %s %s, Durchwahl <code>%s</code>) importieren?'),
-					@$_SESSION['sudo_user']['name'],
-					@$_SESSION['sudo_user']['info']['firstname'],
-					@$_SESSION['sudo_user']['info']['lastname'],
-					@$_SESSION['sudo_user']['info']['ext']
-				),
-				'</p>', "\n";
-			
-			echo '<input type="submit" value="', __('Import') ,'" /><br />', "\n";	
-			echo '</form>', "\n";
-			echo '<br />', "\n";
 		}
 	}
 	
 }
+
+
+if ($action == 'preview') {
+	
+	if (! @is_array(@$records) || count($records)<1) {
+		echo 'Error.';
+	} else {
+		
+		echo '<form method="post" action="', GS_URL_PATH, '" enctype="multipart/form-data">', "\n";
+		echo gs_form_hidden($SECTION, $MODULE), "\n";
+		echo '<input type="hidden" name="action" value="preview" />', "\n";
+		
+		echo '<table cellspacing="1" class="phonebook">', "\n";
+		echo '<tbody>', "\n";
+		
+		echo '<tr>', "\n";
+		echo '<td>', __('Begrenzer') ,':</td>', "\n";
+		echo '<td>', "\n";
+		echo '<nobr><input type="radio" name="sep" id="ipt-sep-s" value="s" ', ($sep==='s' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-sep-s">', __('Semikolon') ,' (<code>;</code>)</label></nobr> ', "\n";
+		echo '<nobr><input type="radio" name="sep" id="ipt-sep-c" value="c" ', ($sep==='c' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-sep-c">', __('Komma') ,' (<code>,</code>)</label></nobr> ', "\n";
+		echo '<nobr><input type="radio" name="sep" id="ipt-sep-t" value="t" ', ($sep==='t' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-sep-t">', __('Tabulator') ,' (<code>\\t</code>)</label></nobr> ', "\n";
+		echo '</td>', "\n";
+		echo '</tr>', "\n";
+		
+		echo '<tr>', "\n";
+		echo '<td>', __('Umschlie&szlig;ung') ,':</td>', "\n";
+		echo '<td>', "\n";
+		echo '<nobr><input type="radio" name="encl" id="ipt-encl-q" value="q" ', ($encl==='q' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-encl-q">', __('Anf&uuml;hrungszeichen') ,' (<code>&quot;</code>)</label></nobr> ', "\n";
+		echo '<nobr><input type="radio" name="encl" id="ipt-encl-s" value="s" ', ($encl==='s' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-encl-s">', __('Apostroph') ,' (<code>&apos;</code>)</label></nobr> ', "\n";
+		echo '<nobr><input type="radio" name="encl" id="ipt-encl-n" value="n" ', ($encl==='n' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-encl-n">', __('nichts') ,'</label></nobr> ', "\n";
+		echo '</td>', "\n";
+		echo '</tr>', "\n";
+		
+		echo '<tr>', "\n";
+		echo '<td>', __('Zeichenkodierung') ,':</td>', "\n";
+		echo '<td>', "\n";
+		echo '<nobr><input type="radio" name="enc" id="ipt-enc-utf8" value="utf8" ', ($enc==='utf8' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-enc-utf8">UTF-8</label></nobr> ', "\n";
+		echo '<nobr><input type="radio" name="enc" id="ipt-enc-iso88591" value="iso88591" ', ($enc==='iso88591' ? 'checked="checked" ' : '') ,'/>',
+			 '<label for="ipt-enc-iso88591">ISO-8859-1</label></nobr> ', "\n";
+		echo '</td>', "\n";
+		echo '</tr>', "\n";
+		
+		echo '<tr>', "\n";
+		echo '<td>', __('Spalten') ,':</td>', "\n";
+		echo '<td>', "\n";
+		echo '<nobr><label for="ipt-col_ln">', __('Nachname'), ':</label>', "\n",
+			 '<input type="text" name="col_ln" id="ipt-col_ln" size="2" maxlength="2" value="', $col_ln ,'"></nobr> &nbsp;', "\n";
+		echo '<nobr><label for="ipt-col_fn">', __('Vorname'), ':</label>', "\n",
+			 '<input type="text" name="col_fn" id="ipt-col_fn" size="2" maxlength="2" value="', $col_fn ,'"></nobr> &nbsp;', "\n";
+		echo '<nobr><label for="ipt-col_nr">', __('Telefonnummer'), ':</label>', "\n",
+			 '<input type="text" name="col_nr" id="ipt-col_nr" size="2" maxlength="2" value="', $col_nr ,'"></nobr> &nbsp;', "\n";
+		echo '</td>', "\n";
+		echo '</tr>', "\n";
+		
+		echo '<tr>', "\n";
+		echo '<td>', __('Kopfzeile') ,':</td>', "\n";
+		echo '<td>', "\n";
+		echo '<input type="checkbox" name="skip_1st" id="ipt-skip_1st" value="1" ', ($skip_1st ? 'checked="checked" ' : '') ,'/>', "\n",
+			 '<label for="ipt-skip_1st">', __('erste Zeile auslassen') ,'</label>', "\n";
+		echo '</td>', "\n";
+		echo '</tr>', "\n";
+		
+		echo '</tbody>', "\n";
+		echo '</table>', "\n";
+		
+		echo '<input type="submit" value="', __('Vorschau') ,'" /><br />', "\n";	
+		echo '</form>', "\n";
+		echo '<br />', "\n";
+		
+		
+		
+		echo '<p class="text">', __('Bitte kontrollieren Sie, ob die Daten so importiert werden sollen wie sie hier angezeigt werden. Dr&uuml;cken Sie danach den Import-Knopf am Ende dieser Seite.') ,'</p>', "\n";
+		
+		echo '<table cellspacing="1" class="phonebook">', "\n";
+		echo '<thead>', "\n";
+		echo '<tr>', "\n";
+		echo '<th>', __('Nachname') ,'</th>', "\n";
+		echo '<th>', __('Vorname') ,'</th>', "\n";
+		echo '<th>', __('Tel.Nr.') ,'</th>', "\n";
+		echo '<th>', __('Tel.Nr. (Import)') ,'</th>', "\n";
+		echo '</tr>', "\n";
+		echo '</thead>', "\n";
+		echo '<tbody>', "\n";
+		$i=0;
+		foreach ($records as $r) {
+			echo '<tr class="', ($i%2? 'even':'odd') ,'">', "\n";
+			echo '<td>', $r['ln'   ] ,'</td>', "\n";
+			echo '<td>', $r['fn'   ] ,'</td>', "\n";
+			echo '<td>', $r['nr'   ] ,'</td>', "\n";
+			echo '<td>', $r['nrimp'] ,'</td>', "\n";
+			echo '</tr>', "\n";
+			++$i;
+		}
+		echo '</tbody>', "\n";
+		echo '</table>', "\n";
+		echo '<br />', "\n";
+		
+		
+		echo '<form method="post" action="', GS_URL_PATH, '" enctype="multipart/form-data">', "\n";
+		echo gs_form_hidden($SECTION, $MODULE), "\n";
+		echo '<input type="hidden" name="action" value="import" />', "\n";
+		
+		echo '<input type="hidden" name="sep"      value="', $sep    ,'" />', "\n";
+		echo '<input type="hidden" name="encl"     value="', $encl   ,'" />', "\n";
+		echo '<input type="hidden" name="enc"      value="', $enc    ,'" />', "\n";
+		echo '<input type="hidden" name="col_ln"   value="', $col_ln ,'" />', "\n";
+		echo '<input type="hidden" name="col_fn"   value="', $col_fn ,'" />', "\n";
+		echo '<input type="hidden" name="col_nr"   value="', $col_nr ,'" />', "\n";
+		echo '<input type="hidden" name="skip_1st" value="', (int)$skip_1st ,'" />', "\n";
+		
+		echo '<p class="text">',
+			@sPrintF(__('Daten in Ihr pers&ouml;nliches Telefonbuch (Benutzer <code>%s</code>, %s %s, Durchwahl <code>%s</code>) importieren?'),
+				@$_SESSION['sudo_user']['name'],
+				@$_SESSION['sudo_user']['info']['firstname'],
+				@$_SESSION['sudo_user']['info']['lastname'],
+				@$_SESSION['sudo_user']['info']['ext']
+			),
+			'</p>', "\n";
+		
+		echo '<input type="submit" value="', __('Import') ,'" /><br />', "\n";	
+		echo '</form>', "\n";
+		echo '<br />', "\n";
+		
+	}
+	
+}
+
+
+if ($action == 'import') {
+	
+	if (! @is_array(@$records) || count($records)<1) {
+		echo 'Error.';
+	} else {
+		
+		$user_id = (int)@$_SESSION['sudo_user']['info']['id'];
+		$query_start = 'INSERT INTO `pb_prv` (`user_id`, `firstname`, `lastname`, `number`) VALUES '."\n";
+		$sql_values = array();
+		if (gs_get_conf('GS_DB_MASTER_TRANSACTIONS'))
+			$DB->startTrans();
+		foreach ($records as $r) {
+			$sql_values[] = '('. $user_id .', \''. $DB->escape($r['fn']) .'\', \''. $DB->escape($r['ln']) .'\', \''. $DB->escape($r['nrimp']) .'\')';
+			if (count($sql_values) == 10) {
+				$query = $query_start . implode(",\n", $sql_values);
+				$sql_values = array();
+				$DB->execute($query);
+			}
+		}
+		if (count($sql_values) > 0) {
+			$query = $query_start . implode(",\n", $sql_values);
+			$sql_values = array();
+			$DB->execute($query);
+		}
+		$ok = (gs_get_conf('GS_DB_MASTER_TRANSACTIONS') ? @$DB->completeTrans() : true);
+		$file = @$_SESSION['sudo_user']['pb-csv-file'];
+		@$_SESSION['sudo_user']['pb-csv-file'] = null;
+		if (@is_file($file)) {
+			$err=0; $out=array();
+			echo $file;
+			@exec( 'rm -f '. escapeShellArg($file) );
+		}
+		
+		if ($ok) {
+			echo 'Done.';
+		} else {
+			echo 'DB Error.';
+		}
+		
+	}
+	
+}
+
+
 if ($action == '') {
 	
 	echo '<form method="post" action="', GS_URL_PATH, '" enctype="multipart/form-data">', "\n";
