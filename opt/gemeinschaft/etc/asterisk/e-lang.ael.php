c6c2242369ee4e93a37b99bc15f5ee063456a0cb
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-08-30 06:13:32 +0000

    GS_INTL_ASTERISK_LANG beachten
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2218 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/etc/asterisk/e-lang.ael.php
@@ -0,0 +1,49 @@
+#!/usr/bin/php -q
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+define( 'GS_VALID', true );  /// this is a parent file
+
+ini_set('implicit_flush', 1);
+ob_implicit_flush(1);
+
+error_reporting(0);
+
+require_once( dirName(__FILE__) .'/../../inc/conf.php' );
+set_error_handler('err_handler_quiet');
+
+
+$lang = gs_get_conf('GS_INTL_ASTERISK_LANG', 'de');
+$lang = preg_replace('/[^a-z]/', '', strToLower(trim($lang)));
+if ($lang=='') $lang = 'de';
+
+
+echo "\t\t", 'Set(CHANNEL(language)=', $lang, ');', "\n";
+
+
+?>
