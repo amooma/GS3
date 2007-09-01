bb5803c0e935331c9be59377584a3faf67ce604e
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-01 07:39:15 +0000

    Präfix für Privatgespräche einstellbar
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2249 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/etc/asterisk/e-number-private.ael.php
@@ -0,0 +1,54 @@
+#!/usr/bin/php -q
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+define( 'GS_VALID', true );  /// this is a parent file
+
+ini_set('implicit_flush', 1);
+ob_implicit_flush(1);
+
+error_reporting(0);
+
+require_once( dirName(__FILE__) .'/../../inc/conf.php' );
+set_error_handler('err_handler_quiet');
+
+
+echo "\n";
+
+//$prv_prfx = (int)gs_get_conf('GS_DP_DIALTIMEOUT_IN', 45);
+//if ($timeout < 1) $timeout = 1;
+$prv_prfx = '96';
+$prv_prfx = preg_replace('/[^0-9*]/', '', $prv_prfx);
+if ($prv_prfx == '') die();
+if (preg_match('/^\*[7*02589]/', $prv_prfx)) die();
+
+
+echo "\t", '_', $prv_prfx ,'. => jump *7*${EXTEN:', strLen($prv_prfx) ,'}', "\n";
+
+
+?>
