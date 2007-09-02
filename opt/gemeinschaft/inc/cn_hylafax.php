2144c647c0d0d9ece6e37e54c025770c77669ca5
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-02 15:10:37 +0000

    kleinere Fixes
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2260 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/cn_hylafax.php
+++ opt/gemeinschaft/inc/cn_hylafax.php
@@ -27,197 +27,182 @@
 
 
 function fax_get_jobs_rec() {
-
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
 	
-	if (!$conn_id) {
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
 	
-		echo "NOK";
-		return 0;
-	}
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-
-	if (!ftp_raw($conn_id, 'RCVFMT "%a|%b|%d|%e|%f|%h|%i|%j|%l|%m|%n|%o|%p|%q|%r|%s|%w|%z|%Z"'))
-		return 0;
-	$jobs_r=Array();
-	$rlist = ftp_rawlist($conn_id,"recvq");
+	if (! ftp_raw($conn_id, 'RCVFMT "%a|%b|%d|%e|%f|%h|%i|%j|%l|%m|%n|%o|%p|%q|%r|%s|%w|%z|%Z"'))
+		return false;
+	$jobs_r = array();
+	$rlist = ftp_rawlist($conn_id,'recvq');
 	foreach ($rlist as $rlist_line) {
-		$rlist_line_array = explode("|",$rlist_line);
-		$jobs_r[]=$rlist_line_array;
+		$jobs_r[] = explode('|',$rlist_line);
 	}
 	ftp_close($conn_id);
 	return $jobs_r;
-
 }
 
 function fax_get_jobs_done() {
-
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-
-	if (!ftp_raw($conn_id, 'JOBFMT "%a|%b|%c|%d|%e|%f|%g|%h|%i|%j|%l|%n|%o|%p|%q|%r|%s|%t|%u|%v|%w|%x|%y|%z|%I|%K|%M|%R|%S|%V|%W|%X|%Z"'))
-		return 0;
-	$jobs_r=Array();
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'JOBFMT "%a|%b|%c|%d|%e|%f|%g|%h|%i|%j|%l|%n|%o|%p|%q|%r|%s|%t|%u|%v|%w|%x|%y|%z|%I|%K|%M|%R|%S|%V|%W|%X|%Z"'))
+		return false;
+	$jobs_r = array();
  	$rlist = ftp_rawlist($conn_id,"doneq");
 	foreach ($rlist as $rlist_line) {
-		$rlist_line_array = explode("|",$rlist_line);
-		$jobs_r[]=$rlist_line_array;
+		$jobs_r[] = explode('|',$rlist_line);
 	}
 	ftp_close($conn_id);
 	return $jobs_r;
-
 }
 
 function fax_get_jobs_send() {
-
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-
-	if (!ftp_raw($conn_id, 'JOBFMT "%a|%b|%c|%d|%e|%f|%g|%h|%i|%j|%l|%n|%o|%p|%q|%r|%s|%t|%u|%v|%w|%x|%y|%z|%I|%K|%M|%R|%S|%V|%W|%X|%Z"'))
-		return 0;
-	$jobs_r=Array();
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'JOBFMT "%a|%b|%c|%d|%e|%f|%g|%h|%i|%j|%l|%n|%o|%p|%q|%r|%s|%t|%u|%v|%w|%x|%y|%z|%I|%K|%M|%R|%S|%V|%W|%X|%Z"'))
+		return false;
+	$jobs_r = array();
  	$rlist = ftp_rawlist($conn_id,"sendq");
 	foreach ($rlist as $rlist_line) {
-		$rlist_line_array = explode("|",$rlist_line);
-		$jobs_r[]=$rlist_line_array;
+		$jobs_r[] = explode('|',$rlist_line);
 	}
 	ftp_close($conn_id);
 	return $jobs_r;
-
 }
 
 function fax_delete_file($file) {
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-	if (!ftp_raw($conn_id, 'admin ablue7')) return 0;
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'admin ablue7')) return false;
 	$ret_val = ftp_delete($conn_id, $file);
 	ftp_close($conn_id);
 	return $ret_val;
 }
 
 function fax_delete_job($job) {
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-	if (!ftp_raw($conn_id, 'admin ablue7')) return 0;
-	$ret_val = ftp_raw($conn_id, "jdele ".$job);
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'admin ablue7')) return false;
+	$ret_val = ftp_raw($conn_id, 'jdele '.$job);
 	ftp_close($conn_id);
 	return $ret_val;
 }
 
 function fax_kill_job($job) {
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-	if (!ftp_raw($conn_id, 'admin ablue7')) return 0;
-	$ret_val = ftp_raw($conn_id, "jkill ".$job);
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'admin ablue7')) return false;
+	$ret_val = ftp_raw($conn_id, 'jkill '.$job);
 	ftp_close($conn_id);
 	return $ret_val;
 }
 
 function fax_send($user_id, $user_name, $to_num, $from_num, $file, $user_email, $resolution) {
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
+	
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
 	
 	if ($resolution < 98) $resolution = 98; 
-
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-	$ret_val = ftp_raw($conn_id, "jnew");
-	$ret_par = explode(" ",$ret_val[0]);
-	$jobid = (int)@$ret_par[array_search("jobid:",$ret_par)+1];
-	echo "<pre>\n";
-
-//print_r($ret_val);
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	$ret_val = ftp_raw($conn_id, 'jnew');
+	//echo "<pre>\n";
+	//print_r($ret_val);
+	//echo "</pre>\n";
+	$ret_par = explode(' ',$ret_val[0]);
+	$jobid = (int)@$ret_par[array_search('jobid:',$ret_par)+1];
+	
 	if ($jobid) {
-		
-		$ret_val = ftp_put ($conn_id, $file, $file, FTP_BINARY);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "job ".$jobid);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm DIALSTRING ".$to_num);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm TSI ".$from_num);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm FAXNAME ".$user_id);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm FAXNUMBER ".$from_num);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm FROMUSER ".$user_name);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm LASTTIME 000259");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm NOTIFYADDR ".$user_email);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm MAXDIALS 6");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm MAXTRIES 6");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm NOTIFY NONE");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm PAGECHOP default");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm PAGEWIDTH 209");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm PAGELENGTH 296");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm SCHEDPRI 127");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm SENDTIME NOW");
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm VRES ".$resolution);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jparm DOCUMENT ".$file);
-//print_r($ret_val);
-		$ret_val = ftp_raw($conn_id, "jsubm ".$jobid);
-//print_r($ret_val);
-		
+		//echo "<pre>\n";
+		$ret_val = ftp_put($conn_id, $file, $file, FTP_BINARY);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'job '.$jobid);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm DIALSTRING '.$to_num);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm TSI '.$from_num);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm FAXNAME '.$user_id);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm FAXNUMBER '.$from_num);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm FROMUSER '.$user_name);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm LASTTIME 000259');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm NOTIFYADDR '.$user_email);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm MAXDIALS 6');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm MAXTRIES 6');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm NOTIFY NONE');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm PAGECHOP default');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm PAGEWIDTH 209');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm PAGELENGTH 296');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm SCHEDPRI 127');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm SENDTIME NOW');
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm VRES '.$resolution);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jparm DOCUMENT '.$file);
+		//print_r($ret_val);
+		$ret_val = ftp_raw($conn_id, 'jsubm '.$jobid);
+		//print_r($ret_val);
+		//echo "</pre>\n";
 	}
-	echo "</pre>\n";
 	ftp_close($conn_id);
-	if ($ret_val) return $jobid;
-	return 0;
+	return ($ret_val ? $jobid : false);
 }
 
-function fax_download($file) {
-	$conn_id = ftp_connect(GS_FAX_SERVER,GS_FAX_PORT);
-	if (!$conn_id) {
-		return 0;
-	}
+function fax_download( $file ) {
 	
-	$login_result = ftp_login($conn_id, "webmanag", "ablue7");
-	if ((!$conn_id) || (!$login_result)) return 0;
-	if (!ftp_raw($conn_id, 'admin ablue7')) return 0;
-	$ret_val = ftp_get($conn_id, "/tmp/".$file,"recvq/".$file,FTP_BINARY);
+	$conn_id = ftp_connect(GS_FAX_SERVER, GS_FAX_PORT);
+	if (! $conn_id) return false;
+	
+	$login_result = ftp_login($conn_id, 'webmanag', 'ablue7');
+	if (! $login_result) return false;
+	
+	if (! ftp_raw($conn_id, 'admin ablue7')) return false;
+	$ret_val = ftp_get($conn_id, '/tmp/'.$file, 'recvq/'.$file, FTP_BINARY);
 	ftp_close($conn_id);
 	return $ret_val;
 }
+
 
 ?>
