ed7ab9eca6ab682194dc5a85b8338d1e937f48ab
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-05 11:49:38 +0000

    neuer Monitor
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2305 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mon/comm.js
+++ opt/gemeinschaft/htdocs/gui/mon/comm.js
@@ -11,240 +11,6 @@
 \*******************************************************************/
 
 
-
-/* ------------- misc ------------- */
-
-function $(el) {
-	if ((typeof el)=='string') return document.getElementById(el);
-	return el;
-}
-
-function nop(){}
-
-var Class = {
-	create: function(){
-		return function(){ this.init.apply(this, arguments); }
-	}
-}
-
-function extend( dst, src ) {
-	if (src) for (var p in src) dst[p]=src[p];
-	return dst;
-}
-
-
-/* ------------- function ------------- */
-
-extend(Function.prototype, {
-	bind: function() {
-		var __method=this,
-		args=Array.from(arguments), obj=args.shift();
-		return function() {
-			return __method.apply(obj, args.concat());
-		}
-	},
-	bindAsEventHandler: function(obj) {
-		var __method=this;
-		return function(evt) {
-			return __method.call(obj, evt||window.event);
-		}
-	}
-});
-
-
-/* ------------- string ------------- */
-
-extend(String.prototype, {
-	trim: function() {
-		return this.replace(/^\s+|\s+$/g, '');
-	}
-});
-
-
-/* ------------- array ------------- */
-
-var $A = Array.from = function(iterable) {
-	if (!iterable) return [];
-	if (iterable.toArray) return iterable.toArray();
-	var arr=[];
-	for (var i=0; i<iterable.length; ++i) arr.push(iterable[i]);
-	return arr;
-}
-
-
-/* ------------- event ------------- */
-
-if (!window.Event) var Event={};
-extend(Event, {
-	_trackListeners: false,
-	observe: function(el, eName, fn, capture) {
-		if (!(el=$(el))) return;
-		if (eName=='keypress' &&
-			(navigator.userAgent.match(/KHTML/)
-			|| el.attachEvent)) eName='keydown';
-		if (this._trackListeners) this._track(el,eName,fn,capture);
-		if (el.addEventListener)
-			el.addEventListener(eName, fn, capture||false);
-		else if (el.attachEvent)
-			el.attachEvent('on'+ eName, fn);
-	},
-	stopObserving: function(el, eName, fn, capture) {
-		if (!(el=$(el))) return;
-		if (eName=='keypress' &&
-			(navigator.userAgent.match(/KHTML/)
-			|| el.detachEvent)) eName='keydown';
-		if (el.removeEventListener)
-			el.removeEventListener(eName, fn, capture||false);
-		else if (el.detachEvent)
-			el.detachEvent('on'+ eName, fn);
-	},
-	_track: function(el, eName, fn, capture) {
-		if (!this._listeners) this._listeners = [];
-		this._listeners.push([el, eName, fn, capture]);
-	},
-	_removeAll: function() {
-		if (Event._listeners) {
-			for (var i=0; i<Event._listeners.length; ++i) {
-				Event.stopObserving.apply(this, Event._listeners[i]);
-				Event._listeners[i][0] = null;
-			}
-			Event._listeners = false;
-		}
-	}
-});
-// prevent memory leaks in IE:
-if (navigator.userAgent.indexOf('IE')!=-1) {
-	Event._trackListeners = true;
-	Event.observe(window, 'unload', Event._removeAll, false);
-}
-
-
-/* ------------- ajax ------------- */
-
-var Ajax = {
-	getHttpReq: function() {
-		try{ return new XMLHttpRequest(); }catch(e){}
-		try{ return new ActiveXObject('Msxml2.XMLHTTP'); }catch(e){}
-		try{ return new ActiveXObject('Microsoft.XMLHTTP'); }catch(e){}
-		return null;
-	},
-	activeReqCnt: 0
-};
-
-var AjaxReq = Class.create();
-AjaxReq.prototype = {
-	init: function(url, opts) {
-		this.x = Ajax.getHttpReq(); if(!this.x)return;
-		this.url = url;
-		this.setOpts(opts);
-		this.msglastpos = 0;
-		this.connect_timeout = null;
-		this.send();
-	},
-	setOpts: function(opts) {
-		this.opts = {
-			method: 'GET',
-			async: true,
-			connect_timeout: 5,
-			params: {},
-			msgsep: "\n==\n"
-			//,headers: {}
-		};
-		if(opts) extend(this.opts, opts);
-	},
-	check_timeout: function() {
-		if (this.x.status < 100) {
-			(this.opts['onErr'] || nop)(this);
-		}
-	},
-	send: function() {
-		//alert(this.el +', '+ this.url +', '+ this.opts);
-		//var parms = this.parms || {};
-		var params = this.opts.params;
-		var q = '';
-		
-		// prevent caching by adding a random parameter:
-		//...
-		
-		if (this.opts.method=='GET' && q.length>0)
-			this.url += (this.url.match(/\?/)?'&':'?') + q;
-		
-		//try {
-			try {
-				this.x.open(this.opts.method, this.url, this.opts.async);
-			} catch(e) {return;}
-			if (this.opts.async) {
-				this.x.onreadystatechange = this.onStateChange.bind(this);
-				try {
-					this.connect_timeout = window.setTimeout(
-						this.check_timeout.bind(this), this.opts.connect_timeout*1000 );
-				} catch(e){}
-				//var me = this;
-				//this.x.onreadystatechange = function(){me.onStateChange(me);}
-			}
-			
-			var headers=[];
-			if (this.opts.method=='POST')
-				headers.push('Content-type',
-					'application/x-www-form-urlencoded');
-			if (this.x.overrideMimeType) {
-				//headers.push('Connection', 'close');
-				this.x.overrideMimeType( 'application/x-www-form-urlencoded' );
-			}
-			if (this.opts.headers)
-				headers.push.apply(headers, this.opts.headers);
-			for (var i=0; i<headers.length; i+=2)
-				this.x.setRequestHeader(headers[i], headers[i+1]);
-			
-			var body = this.opts.postBody ? this.opts.postBody : q;
-			body = '';
-			this.x.send(this.opts.method=='POST'? body :null);
-		//} catch(e){}
-	},
-	isOk: function() {
-		var s = this.x.status;
-		return (s>=200 && s<300) || s==undefined || s==0;
-	},
-	header: function(name) {
-		try {return this.x.getResponseHeader(name);}
-		catch(e){return null;}
-	},
-	onStateChange: function() {
-		if (this.x.readyState==3 || this.x.readyState==4) {  // new data available
-			try {
-				if (this.isOk()) {
-					try { window.clearTimeout( this.connect_timeout ); } catch(e){}
-					
-					(this.opts['onData'] || nop)(this);
-					
-					var pos = 0;
-					while ((pos = this.x.responseText.indexOf(this.opts.msgsep, this.msglastpos)) != -1) {
-						(this.opts['onMsg'] || nop)(
-							this, this.x.responseText.substring(this.msglastpos, pos) );
-						this.msglastpos = pos + this.opts.msgsep.length;
-					}
-				}
-			} catch(e){}
-		}
-		if (this.x.readyState==4) {  // complete
-			//alert( this.x.status );
-			try {
-				(this.opts['on'+(this.isOk()?'Ok':'Err')]
-				|| nop)(this);
-				(this.opts['onDone']
-				|| nop)(this);
-			} catch(e){}
-			try { this.x.onreadystatechange = nop; } catch(e){}
-			//Ajax.activeReqCnt--;
-		}
-	}
-};
-
-/* -------------------------- */
-
-
-
-
 var AST_EXTSTATE_UNKNOWN  = -1  // no hint for the extension
 var AST_EXTSTATE_IDLE     =  0  // registered, idle
 var AST_EXTSTATE_INUSE    =  1  // busy
@@ -252,77 +18,127 @@ var AST_EXTSTATE_OFFLINE  =  4  // unreachable, not registered
 var AST_EXTSTATE_RINGING  =  8  // ringing
 
 
-//var req_start = 0;
-
-function handle_msg( req, msg )
+function get_transport_iframe()
 {
-	try {
-		eval('var exts=' + msg.trim());
-	} catch(e){ document.write(msg.trim()); document.close(); }
-	if (!exts || (typeof exts)!='object') return;
+	var iframe = $('transport');
+	if (iframe) return iframe;
 	
-	var ext;
-	var el;
-	var newclass = 'e_ukn';
-	for (ext in exts) {
-		if (!( el = $('e'+ext) )) continue;
-		switch (exts[ext]['s']) {
-			case AST_EXTSTATE_INUSE  :
-				newclass = (exts[ext]['e'] ? 'e_bse' : 'e_bsi'); break;
-			case AST_EXTSTATE_IDLE   :  newclass = 'e_idl'; break;
-			case AST_EXTSTATE_RINGING:  newclass = 'e_rng'; break;
-			case AST_EXTSTATE_OFFLINE:  newclass = 'e_off'; break;
-		}
-		if (el.className != newclass)
-			el.className = newclass;
-	}
+	// IE 5 Mac:
+	if (document.frames && document.frames['transport'])
+		return document.frames['transport'];
 	
-	/*
-	var req_dur = (new Date).getTime() - req_start;
-	if (req_dur < 1) req_dur = 1; // ms
-	var bytes = req.x.responseText.length;
-	$('bps').innerHTML = (bytes/req_dur).toFixed(1);
-	*/
-	try{ $('mon-status').innerHTML = 'Online'; }catch(e){}
+	return null;
 }
 
-var areq = null;
+/*
+function get_iframe_document_element( iframe )
+{
+	if (! iframe) return null;
+	
+	// NS 6, FF:
+	if (iframe.contentDocument       ) return iframe.contentDocument;
+	
+	// IE 5.5, 6:
+	if (iframe.contentWindow
+	&&  iframe.contentWindow.document) return iframe.contentWindow.document;
+	
+	// IE 5:
+	if (iframe.document              ) return iframe.document;
+	
+	return null;
+}
+
+function get_transport_document_element()
+{
+	var iframe = get_transport_iframe();
+	if (iframe) {
+		return get_iframe_document_element( iframe );
+	}
+	return null;
+}
+*/
+
+function add_extension( ext )
+{
+	/*
+	try { document.location.reload(); }
+	catch(e) { alert('Please reload this page.'); }
+	*/
+}
+
+function gs_m( exts )
+{
+	var el = null;
+	var newclass = '';
+	var classes = '';
+	var link = '';
+	
+	for (ext in exts) {
+		try {
+			if (!( el = $('e'+ext) )) {
+				//add_extension( ext );
+				continue;
+			}
+			if ((typeof el.className) != 'string') continue;
+			
+			switch (exts[ext]['s']) {
+				case AST_EXTSTATE_INUSE  :
+					newclass = (exts[ext]['e'] ? 'e_bse' : 'e_bsi'); break;
+				case AST_EXTSTATE_IDLE   :  newclass = 'e_idl'; break;
+				case AST_EXTSTATE_RINGING:  newclass = 'e_rng'; break;
+				case AST_EXTSTATE_OFFLINE:  newclass = 'e_off'; break;
+				default                  :  newclass = 'e_ukn';
+			}
+			
+			classes  = el.className.replace(/\be_[a-z]+/g, '').replace(/\s{2,}/g, '');
+			classes += ' '+ newclass;
+			if (el.className != classes) el.className = classes;
+			
+			if (!( el = $('e'+ext+'l') )) continue;
+			
+			if (exts[ext]['l']) {
+				if (exts[ext]['l'].substr(0,3) != '*7*')
+					link = '&rarr; '+ exts[ext]['l'];
+				else
+					link = '&rarr; '+ 'privat';
+			} else {
+				link = '';
+			}
+			el.innerHTML = link;
+		}
+		catch(e){}
+	}
+}
 
 function req_msg_stream()
 {
-	//req_start = (new Date).getTime();
-	areq = new AjaxReq( 'proxy.php', {
-		'connect_timeout': 5,
-		'msgsep': "\n==\n",
-		'onMsg' : handle_msg,
-		'onOk'  : function(){
-			cleanup();
-			window.setTimeout('req_msg_stream();', 50);
-		},
-		'onErr' : function(){
-			try{
-				$('mon-status').innerHTML = '<span style="background:#f10; color:#fff;">OFFLINE</span>';
-			}catch(e){}
-			cleanup();
-			window.setTimeout('req_msg_stream();', 3000);
-		}
-	});
+	// the transport is done like "Comet" but with an iframe and
+	// script tags instead for cross-browser compatibility
+	// (stupid  MSIE!)
+	// disadvantage: a browser will only load one of such streams
+	// at a time, so viewing more than one monitor is not possible.
+	// and the page seems to be loading for about half a minute
+	var iframe = get_transport_iframe();
+	if (iframe) {
+		iframe.src = 'proxy.php?rand='+ parseInt(Math.random()*99999999);
+		return true;
+	}
+	
+	alert('There\'s a problem with your browser.');
+	return false;
 }
 
-function cleanup()
-{
-	// try to avoid memory leaks
-	areq.x.onreadystatechange = null;
-	areq.x = null;
-	delete(areq.x);
-	areq = null;
-}
 
-Event.observe(window, 'load', function(){
-	window.setTimeout('req_msg_stream();', 250);
-});
-Event.observe(window, 'pageshow', function(){
-	window.setTimeout('if (!areq) req_msg_stream();', 500);
+Event.observe( window, 'load', function(){
+	var iframe = get_transport_iframe();
+	if (iframe) {
+		Event.observe( iframe, 'load', function(){
+			// our transport iframe is done with loading
+			window.setTimeout('req_msg_stream();', 250);
+		});
+	}
+	
+	window.setTimeout('req_msg_stream();', 2000);
 });
 
 
--- opt/gemeinschaft/htdocs/gui/mon/proxy.php
+++ opt/gemeinschaft/htdocs/gui/mon/proxy.php
@@ -28,14 +28,16 @@
 
 define( 'GS_VALID', true );  /// this is a parent file
 require_once( dirName(__FILE__) .'/../../../inc/conf.php' );
-
+//include_once( GS_DIR .'inc/db_connect.php' );
 
 $host = '127.0.0.1';
 $port = 5039;
 $maxtime = 35;
 
-#header( 'Content-Type: text/json' );
-header( 'Content-Type: application/json' );  # RFC 4627
+@session_write_close();
+
+
+header( 'Content-Type: text/html; charset=utf-8' );
 header( 'Pragma: no-cache' );
 header( 'Cache-Control: private, no-cache, must-revalidate' );
 header( 'Expires: 0' );
@@ -44,7 +46,7 @@ header( 'Vary: *' );
 ini_set('implicit_flush', 1);
 ob_implicit_flush(1);
 
-@ini_set('max_execution_time', $maxtime+2);
+@ini_set('max_execution_time', $maxtime+10);
 
 $sock = @fSockOpen( $host, $port, $err, $errMsg, 5 );
 if (! is_resource($sock)) {
@@ -53,22 +55,79 @@ if (! is_resource($sock)) {
 	die();
 }
 
+function _html_end() {
+	echo "\n", '</body></html>', "\n";
+	die();
+}
+
 @stream_set_blocking( $sock, false );
 $tStart = time();
 $cnt_no_data = 0;
+$buf = '';
+$msg_open  = '<script type="text/javascript">'."\n";
+$msg_close = '</script>'.".\n";
+// padding for the stupid MSIE:
+$msie_pad = str_repeat(' ', 256-1) ."\n";
+
+echo '<html><head><title>-</title>' ,"\n",
+     '<script type="text/javascript">' ,"\n",
+     'function m(o) {try{ window.parent.gs_m(o); }catch(e){}}', "\n",
+     '</script>' ,"\n",
+     '</head><body>' ,"\n\n";
+
+//$db = gs_db_slave_connect();
+
 while (! @fEof( $sock ) && time() < $tStart+$maxtime) {
 	$data = @fRead( $sock, 8190 );
 	if (strLen($data) > 0) {
 		$cnt_no_data = 0;
-		echo $data;
+		$buf .= $data;
 	} else {
 		if (++$cnt_no_data > 500) {
 			# we sleep 0.01 secs so this is 5 secs
-			die();
+			_html_end();
 		}
 	}
+	
+	while (preg_match('/=====/S', $buf, $m, PREG_OFFSET_CAPTURE)) {
+		$pos = (int)@$m[0][1];
+		$msg = subStr($buf, 0, $pos);
+		$buf = subStr($buf, $pos+strLen(@$m[0][0]));
+		
+		@ob_start();
+		$states = null;
+		$ret = eval('$states = '.$msg.';');
+		@ob_end_clean();
+		if ($ret === false || ! is_array(@$states)) continue;
+		
+		echo $msg_open;
+		echo 'm({'."\n";
+		$i = 0;
+		$c = count($states);
+		foreach ($states as $ext => $info) {
+			echo "'$ext':{";
+			echo "s:". (int)@$info['s'];
+			if (@$info['e']) echo ",e:1";
+			if (@$info['l']) echo ",l:'". $info['l'] ."'";
+			/*
+			if (@$info['l']) {
+				$rs = $db->execute('SELECT `u`.`firstname` `fn`, `u`.`lastname` `ln` FROM `users` `u` JOIN `ast_sipfriends` `s` ON (`s`.`_user_id`=`u`.`id`) WHERE `s`.`name`=\''. $db->escape($info['l']) .'\'');
+				//...
+			}
+			*/
+			echo "}";
+			++$i;
+			if ($i < $c) echo ",";
+			echo "\n";
+		}
+		echo "});\n";
+		echo $msie_pad;
+		echo $msg_close;
+	}
+	
 	uSleep(10000);  # sleep 0.01 secs
 }
 
+_html_end();
 
 ?>
