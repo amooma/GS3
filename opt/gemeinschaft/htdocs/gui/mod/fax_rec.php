64eff7746b60fdee1ae7c5ac40b77cdb59d07b29
peter <peter@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-06 14:36:07 +0000

    Fax-Anpassung
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2316 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/fax_rec.php
+++ opt/gemeinschaft/htdocs/gui/mod/fax_rec.php
@@ -147,6 +147,7 @@ for ($i=($page*$per_page); $i < ($per_page*$page)+$per_page; $i++) {
 		echo "<td>\n";
 		echo '<a href="',$mod_url,'&amp;delete=',$jobs_rec[$i][4],'&amp;page=',$page,'" title="', __('l&ouml;schen'), '"><img alt="', __('l&ouml;schen'), '" src="', GS_URL_PATH, 'crystal-svg/16/act/editdelete.png" /></a>';
 		echo ' &nbsp; <a href="faxdown.php?file='.$jobs_rec[$i][4].'"><img alt="PDF" src="', GS_URL_PATH, 'crystal-svg/16/app/pdf.png" /></a>'."\n";
+		echo ' &nbsp; <a href="faxdown.php?raw='.$jobs_rec[$i][4].'"><img alt="PDF" src="', GS_URL_PATH, 'crystal-svg/16/act/colorize.png" /></a>'."\n";
 		echo "</td>\n";
 		
 		echo "</tr>\n";
