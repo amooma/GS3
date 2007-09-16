c30e62853e60d24c2237d30f3584d020c42aaf87
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-16 14:28:06 +0000

    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2333 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/inc/find_executable.php
@@ -0,0 +1,46 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+defined('GS_VALID') or die('No direct access.');
+
+
+function find_executable( $basename, $paths )
+{
+	if (! is_array($paths)) return false;
+	
+	foreach ($paths as $path) {
+		if (subStr($path, -1) != '/') $path .= '/';
+		$full = $path . $basename;
+		if (is_executable( $full ))
+			return $full;
+	}
+	return false;
+}
+
+
+?>
