8ce73d052a2f1ff0c2d47ee9b9c8f5b1cff6e648
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-09-30 18:14:31 +0000

    sauber formatiert und kleine Verbesserungen
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2433 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/_unused/hastenix.pl
+++ opt/gemeinschaft/_unused/hastenix.pl
@@ -46,10 +46,10 @@ use Digest::MD5 qw(md5_hex);  # needed to create unique subdirectory names from 
 ##############################################
 
 # define here the text-to-speech system installed on asterisk
-my $TTS = 'festival';
+#my $TTS = 'festival';
 #my $TTS = 'cepstral';
 #my $TTS = 'generic';
-#my $TTS = 'none';
+my $TTS = 'none';
 
 # if you have installed cepstral voice(s), enter here the location of the swift
 # executable
@@ -66,14 +66,15 @@ my $TEXT2WAVE = '/home/asterisk/download/festival/bin/text2wave';
 # - reads text input from stdin
 # - writes output in 8kHz/16 bit wav-format to stdout
 # the following command sequence works fine for a german MBROLA setup
-#    iconv: convert utf-8 input to ascii
+#    iconv  : convert utf-8 input to ascii
 #    txt2pho: provide pho-input for the mbrola speech synthesizer
-#    mbrola: provide speech output based on a german speech database
-#    sox: convert mbrola speech output to 8kHz/16bit as needed for asterisk
+#    mbrola : provide speech output based on a german speech database
+#    sox    : convert mbrola speech output to 8kHz/16bit as needed for asterisk
 my $GENERIC_TTS_COMMAND = 'iconv -f utf-8 | /opt/txt2pho/txt2pho | /opt/mbrola/mbrola /opt/mbrola/de1/de1 - - | sox -t raw -r 16000 -sw - -t wav -r 8000 -Uw -';
 
 # cache subdirectory
-my $CACHEDIR = '/var/lib/asterisk/sounds/hastenix_cache';
+#my $CACHEDIR = '/var/lib/asterisk/sounds/hastenix_cache';
+my $CACHEDIR = '/tmp/hastenix_cache';
 
 # number of days while cached sound files will be used (time-to-live)
 # 0: do not cache at all
@@ -83,7 +84,7 @@ my $CACHE_TTL = 2;
 my $READLIMIT = 1000000;
 
 # in case of fatal errors asterisk can play an announcement 'tt-somethingwrong.gsm'
-# 0: no playback	1: playback
+# 0: no playback, 1: playback
 my $PLAYBACK_ON_ERRORS = 1;
 
 # asterisk command for transfer handling
@@ -114,7 +115,8 @@ my $FORERUN = 5;
 
 
 # debug output is sent to stderr and is visible on the asterisk console
-use constant DEBUG_LEVEL => 0; # 0: no debug output - each level increases verbosity
+use constant DEBUG_LEVEL => 2;  # 0: no debug output - each level increases verbosity
+                                # 2: maximum level used in the script
 
 use constant SIGNATURE => 'HASTENIX V0.3';
 
@@ -148,24 +150,24 @@ $|=1; # force immediate output at print commands
 
 sub init_vxmlData
 {
-	my @promptArray = ();
-	my %linkArray = ();
+	my @promptArray   = ();
+	my %linkArray     = ();
 	my %transferArray = ();
-	my %recordArray = ();
-	my %vxmlData = (
+	my %recordArray   = ();
+	my %vxmlData      = (
 		isValid   => 0,
 		timeout   => 0,
-		redirect  => "",
+		redirect  => '',
 		prompts   => \@promptArray,
 		links     => \%linkArray,
 		transfers => \%transferArray,
 		record    => \%recordArray,
 	);
-	$vxmlDataRef = \%vxmlData;
+	$vxmlDataRef      = \%vxmlData;
 	
 	$isPrompt = 0;
-	$isAudio = 0;
-	$isCatch = 0;
+	$isAudio  = 0;
+	$isCatch  = 0;
 	$isRecord = 0;
 }
 
@@ -175,14 +177,14 @@ sub xmlStart
 	
 	print STDERR "XML parser - start tag: <$startTag>\n" if (DEBUG_LEVEL >= 2);
 	
-	if ($startTag eq "vxml") {
+	if ($startTag eq 'vxml') {
 		if (defined $attribs{'version'} && ($attribs{'version'} eq '2.0')) {
 			$vxmlDataRef->{'isValid'} = 1;
 		}
 	}
 	
-	if ($startTag eq "prompt") {
-		$isPrompt = 1; # true
+	elsif ($startTag eq 'prompt') {
+		$isPrompt = 1;  # true
 		$prompt_text = "";
 		
 		if (defined $attribs{'timeout'}) {
@@ -194,50 +196,50 @@ sub xmlStart
 		}
 	}
 	
-	if ($startTag eq "audio") {
-		$isAudio = 1; # true
-		$audio_text = "";
+	elsif ($startTag eq 'audio') {
+		$isAudio = 1;  # true
+		$audio_text = '';
 		
 		if ($prompt_text) {
 			# store preceeding prompt text
 			my %prompt = ( text => $prompt_text );
 			push(@{$vxmlDataRef->{'prompts'}}, \%prompt);
-			$prompt_text = "";
+			$prompt_text = '';
 		}
 		
 		if (defined $attribs{'src'}) {
 			$audio_src = $attribs{'src'};
 		} else {
-			$audio_src = "";
+			$audio_src = '';
 		}
 	}
 	
-	if ($startTag eq "link") {
+	elsif ($startTag eq 'link') {
 		if (defined $attribs{'next'} && defined $attribs{'dtmf'}) {
 			$vxmlDataRef->{'links'}->{$attribs{'dtmf'}} = $attribs{'next'};
 		}
 	}
 	
-	if (($startTag eq "form") && defined $attribs{'id'}) {
+	elsif ($startTag eq 'form' && defined $attribs{'id'}) {
 		# save form id (needed for transfer element which should follow immediately)
 		$form_id = $attribs{'id'};
 	}
 	
-	if ($startTag eq "transfer") {
+	elsif ($startTag eq 'transfer') {
 		$vxmlDataRef->{'transfers'}->{$form_id} = $attribs{'dest'};
 	}
 	
-	if ($startTag eq "catch") {
-		$isCatch = 1; # true
+	elsif ($startTag eq 'catch') {
+		$isCatch = 1;  # true
 	}
 	
-	if (($startTag eq "goto") && !$isCatch) {
+	elsif (($startTag eq 'goto') && !$isCatch) {
 		# <goto> element outside of <catch> is used by HAWHAW for redirection only
 		$vxmlDataRef->{'redirect'} = $attribs{'next'};
 	}
 	
-	if ($startTag eq "record") {
-		$isRecord = 1; # true
+	elsif ($startTag eq 'record') {
+		$isRecord = 1;  # true
 		$vxmlDataRef->{'record'}->{'name'} = $attribs{'name'};
 		
 		if (defined $attribs{'beep'} && ($attribs{'beep'} eq 'true')) {
@@ -257,7 +259,7 @@ sub xmlStart
 		}
 	}
 	
-	if (($startTag eq "submit") && $isRecord){
+	elsif (($startTag eq 'submit') && $isRecord){
 		$vxmlDataRef->{'record'}->{'url'} = $attribs{'next'};
 	}
 }
@@ -269,17 +271,17 @@ sub xmlEnd
 	
 	print STDERR "XML parser - end tag: <$endTag>\n" if (DEBUG_LEVEL >= 2);
 	
-	if ($endTag eq "prompt") {
+	if ($endTag eq 'prompt') {
 		
 		if ($prompt_text) {
 			%prompt = ( text => $prompt_text );
 			push(@{$vxmlDataRef->{'prompts'}}, \%prompt);
 		}
 		
-		$isPrompt = 0; # false
+		$isPrompt = 0;  # false
 	}
 	
-	if ($endTag eq "audio") {
+	elsif ($endTag eq 'audio') {
 		
 		if ($audio_text) {
 			$prompt{'text'} = $audio_text;
@@ -291,15 +293,15 @@ sub xmlEnd
 		
 		push(@{$vxmlDataRef->{'prompts'}}, \%prompt);
 		
-		$isAudio = 0; # false
+		$isAudio = 0;  # false
 	}
 	
-	if ($endTag eq "catch") {
-		$isCatch = 0; # false
+	elsif ($endTag eq 'catch') {
+		$isCatch = 0;  # false
 	}
 	
-	if ($endTag eq "record") {
-		$isRecord = 0; # false
+	elsif ($endTag eq 'record') {
+		$isRecord = 0;  # false
 	}
 }
 
@@ -329,9 +331,9 @@ sub create_cache_dir
 		mkdir($CACHEDIR) || die "cannot create cache directory $CACHEDIR";
 	}
 	
-	my $sounddir = $CACHEDIR . '/' . $url_hash;
+	my $sounddir = $CACHEDIR .'/'. $url_hash;
 	
-	$headfile = $sounddir . '/' . $headfile;
+	$headfile = $sounddir .'/'. $headfile;
 	
 	if (-d $sounddir) {
 		unless (-e $headfile) {
@@ -346,7 +348,7 @@ sub create_cache_dir
 			my @cachefiles = readdir(DIR);
 			closedir(DIR);
 			foreach (@cachefiles) {
-				unlink($sounddir . "/" . $_); # delete all files of directory
+				unlink($sounddir .'/'. $_); # delete all files of directory
 			}
 		}
 	}
@@ -364,7 +366,7 @@ sub create_cache_dir
 		my @cachefiles = readdir(DIR);
 		closedir(DIR);
 		foreach (@cachefiles) {
-			unlink($sounddir . "/" . $_); # delete all files of directory
+			unlink($sounddir .'/'. $_); # delete all files of directory
 		}
 	}
 	
@@ -376,25 +378,25 @@ sub retrieve_VoiceXML
 	my (%http_req) = @_;
 	
 	$wavdir = &create_cache_dir($http_req{'url'}, 'cache.vxml');
-	my $vxml_file = $wavdir . '/cache.vxml';
+	my $vxml_file = $wavdir .'/cache.vxml';
 	
 	my $content = "";
 	
 	if (-e $vxml_file) {
 		print STDERR "read VoiceXML document from cache\n" if (DEBUG_LEVEL >= 1);
-		open(VXMLFILE, "<$vxml_file") || die "cannot open $vxml_file";
-		read(VXMLFILE, $content, $READLIMIT);
+		open (VXMLFILE, "<$vxml_file") || die "cannot open $vxml_file";
+		read (VXMLFILE, $content, $READLIMIT);
 		close(VXMLFILE);
 	}
 	else {
 		my $http_response;
 		my $url = $http_req{'url'};
 		
-		if (index($url, "?") < 0) {
-			 $url .= '?' # begin of query parameters
+		if (index($url, '?') < 0) {
+			 $url .= '?';  # begin of query parameters
 		}
 		else {
-			 $url .= '&' # add to query parameters
+			 $url .= '&';  # add to query parameters
 		}
 		
 		# add agi input parameters
@@ -406,7 +408,7 @@ sub retrieve_VoiceXML
 		if ($http_req{'method'} eq 'get') {
 			# send GET request
 			print STDERR "get VoiceXML document from (remote) webserver:\n" .
-				$url . "\n" if (DEBUG_LEVEL >= 1);
+				$url ."\n" if (DEBUG_LEVEL >= 1);
 			my $http_request = HTTP::Request->new(GET => $url);
 			$http_request->header(accept => 'application/voicexml+xml');
 			$http_response = $userAgent->request($http_request);
@@ -414,11 +416,11 @@ sub retrieve_VoiceXML
 		else {
 			# send POST request
 			print STDERR "post file and retrieve VoiceXML response from (remote) webserver:\n" .
-			$url . "\n" if (DEBUG_LEVEL >= 1);
+			$url ."\n" if (DEBUG_LEVEL >= 1);
 			$http_response = $userAgent->request(POST $url,
 				Content_Type => 'form-data',
-				Accept => 'application/voicexml+xml',
-				Content => [ $http_req{'name'} => [$http_req{'filename'}]]
+				Accept       => 'application/voicexml+xml',
+				Content      => [ $http_req{'name'} => [$http_req{'filename'}]]
 			);
 		}
 		
@@ -437,7 +439,7 @@ sub retrieve_VoiceXML
 		if ((index($content, "<meta http-equiv=\"Expires\"") < 0)
 		&&  ($http_req{'method'} eq 'get')) {
 			# write received VoiceXML data into cache
-			open(VXMLFILE, ">$vxml_file");
+			open (VXMLFILE, ">$vxml_file");
 			print VXMLFILE $content;
 			close(VXMLFILE);
 		}
@@ -456,10 +458,10 @@ sub get_soundfile
 	my ($url) = @_;
 	
 	$url =~ /([^\/]*)$/;
-	my $soundfile_name = $1; # everything after last slash character
+	my $soundfile_name = $1;  # everything after last slash character
 	
 	my $sounddir = &create_cache_dir($url, $soundfile_name);
-	my $sound_file = $sounddir . '/' . $soundfile_name;
+	my $sound_file = $sounddir .'/'. $soundfile_name;
 	
 	if (-e $sound_file) {
 		print STDERR "use cache sound file $sound_file\n" if (DEBUG_LEVEL >= 1);
@@ -471,11 +473,11 @@ sub get_soundfile
 		$http_request->header(accept => '*/*');
 		my $http_response = $userAgent->request($http_request);
 		
-		my $sounddata = $http_response->content; # binary sound data
+		my $sounddata = $http_response->content;  # binary sound data
 		
 		unless ($http_response->is_success) {
 			print STDERR "got " . $http_response->status_line . " response\n" if (DEBUG_LEVEL >= 1);
-			return("");
+			return('');
 		}
 		
 		if (length($sounddata) >= $READLIMIT) {
@@ -484,7 +486,7 @@ sub get_soundfile
 		}
 		
 		# write received sound data into cache
-		open(SOUNDFILE, ">$sound_file");
+		open (SOUNDFILE, ">$sound_file");
 		print SOUNDFILE $sounddata;
 		close(SOUNDFILE);
 	}
@@ -493,7 +495,7 @@ sub get_soundfile
 	my $soundfile_name_without_ext = $soundfile_name;
 	$soundfile_name_without_ext =~ s/\.\w*$//;
 	
-	return($sounddir . '/' . $soundfile_name_without_ext);
+	return($sounddir .'/'. $soundfile_name_without_ext);
 }
 
 sub determine_global_url
@@ -513,8 +515,8 @@ sub determine_global_url
 			die('invalid url: ' . $previous_url);
 		}
 		
-		my $prot_host_part = $1; # => "http://www.foo.com"
-		my $path_part = $2; # => "/mypath/"
+		my $prot_host_part = $1;  # => "http://www.foo.com"
+		my $path_part = $2;  # => "/mypath/"
 		
 		if ($url =~ /^\//) {
 			# absolute url
@@ -531,24 +533,24 @@ sub determine_global_url
 
 sub record
 {
-	my $filename = $wavdir . "/rec_" . $agi_input{'uniqueid'};
-	my $format = "gsm";
-	my $escape_digits = "1234567890*#";
+	my $filename = $wavdir .'/rec_'. $agi_input{'uniqueid'};
+	my $format = 'gsm';
+	my $escape_digits = '1234567890*#';
 	
-	my $timeout = 180000; # default 3 min.
+	my $timeout = 180000;  # default 3 min.
 	if (defined($vxmlDataRef->{'record'}->{'maxtime'})) {
 		$timeout = $vxmlDataRef->{'record'}->{'maxtime'} * 1000;
 	}
 	
-	my $beep = "";
-	if (defined($vxmlDataRef->{'record'}->{'beep'}) &&
-		 ($vxmlDataRef->{'record'}->{'beep'} == 1)) {
+	my $beep = '';
+	if (defined($vxmlDataRef->{'record'}->{'beep'})
+	&&  ($vxmlDataRef->{'record'}->{'beep'} == 1)) {
 		$beep = 'beep';
 	}
 	
-	my $silence = "s=5";
+	my $silence = 's=5';
 	if (defined($vxmlDataRef->{'record'}->{'finalsilence'})) {
-		$silence = "s=" . $vxmlDataRef->{'record'}->{'finalsilence'};
+		$silence = 's='. $vxmlDataRef->{'record'}->{'finalsilence'};
 	}
 	
 	my $record_cmd = "RECORD FILE $filename $format $escape_digits $timeout $beep $silence\n";
@@ -556,7 +558,7 @@ sub record
 	
 	print $record_cmd;
 	my $stdin = <STDIN>;
-	print STDERR "result of RECORD FILE: " . $stdin . "\n" if (DEBUG_LEVEL >= 1);
+	print STDERR "result of RECORD FILE: ". $stdin ."\n" if (DEBUG_LEVEL >= 1);
 	
 	return("$filename.$format");
 }
@@ -583,8 +585,8 @@ sub sound_creator_thread
 		my $text = $prompt->{text};
 		if (length($text) > 0)
 		{
-			my $wavname = $wavdir . "/" . $counter;
-			my $wavfile = $wavname . ".wav";
+			my $wavname = $wavdir .'/'. $counter;
+			my $wavfile = $wavname . '.wav';
 			unless (-e $wavfile) {
 				
 				# no wav-file existing for given prompt => create it
@@ -599,10 +601,10 @@ sub sound_creator_thread
 						" -o " . $wavfile .
 						" -n " . $SWIFT_SPEAKER .
 						" -e utf-8" .
-						" \" $text\"" ); # leading hyphen breaks cepstral => leading blank
+						" \" $text\"" );  # leading hyphen breaks cepstral => leading blank
 				}
 				
-				if ($TTS eq 'festival') {
+				elsif ($TTS eq 'festival') {
 					# use festival/text2wave to create wav-file
 					system("echo \"$text\" | $TEXT2WAVE" .
 						" -f -" .
@@ -610,7 +612,7 @@ sub sound_creator_thread
 						" -o " . $wavfile);
 				}
 				
-				if ($TTS eq 'generic') {
+				elsif ($TTS eq 'generic') {
 					# use generic tts system
 					system("echo \"$text\" | $GENERIC_TTS_COMMAND > $wavfile");
 				}
@@ -622,8 +624,9 @@ sub sound_creator_thread
 		
 		$counter++;
 		
-		print STDERR "items in queue: " . $soundfile_queue->pending() . "\n" if (DEBUG_LEVEL >= 2);
-		while (($abort_flag == 0) && ($soundfile_queue->pending() > $FORERUN)) {
+		print STDERR "items in queue: ". $soundfile_queue->pending() ."\n" if (DEBUG_LEVEL >= 2);
+		while (($abort_flag == 0)
+		&&     ($soundfile_queue->pending() > $FORERUN)) {
 			print STDERR "wait a second ...\n" if (DEBUG_LEVEL >= 2);
 			sleep(1);
 		}
@@ -633,7 +636,7 @@ sub sound_creator_thread
 		}
 	}
 	
-	$soundfile_queue->enqueue(""); # done indication
+	$soundfile_queue->enqueue("");  # done indication
 }
 
 sub background_player
@@ -647,19 +650,19 @@ sub background_player
 		
 		if ($sound_name) {
 			# continue with playing sound files ...
-			print "EXEC Background " . $sound_name . "\n";
+			print "EXEC Background ". $sound_name ."\n";
 		}
 		else {
 			# wait a little bit for user input
-			print "WAIT FOR DIGIT " . $vxmlDataRef->{'timeout'} * 1000 . "\n";
+			print "WAIT FOR DIGIT ". ($vxmlDataRef->{'timeout'} * 1000) ."\n";
 		}
 		
-		my $stdin = <STDIN>; # read asterisk's response
+		my $stdin = <STDIN>;  # read asterisk's response
 		print STDERR "asterisk response: $stdin\n" if (DEBUG_LEVEL >= 1);
 		
 		if (!$stdin) {
 			print STDERR "no STDIN response from asterisk - eof.\n" if (DEBUG_LEVEL >= 1);
-			return(""); # caller probably went onhook ...
+			return('');  # caller probably went onhook ...
 		}
 		
 		my $response = $stdin;
@@ -718,7 +721,7 @@ sub background_player
 		return(\%http_req);
 	}
 	
-	return(""); # we are done - no succeeding url available
+	return('');  # we are done - no succeeding url available
 }
 
 sub handle_url
@@ -732,11 +735,11 @@ sub handle_url
 	# remove quote entities
 	$content =~ s/&quot;//g;
 	
-	&init_vxmlData(); # data will be collected during parse callbacks
+	&init_vxmlData();  # data will be collected during parse callbacks
 	
 	# parse VoiceXML
 	my $xmlParser = new XML::Parser();
-	$xmlParser->setHandlers(Start => \&xmlStart, End => \&xmlEnd, Char => \&xmlChar);
+	$xmlParser->setHandlers( Start => \&xmlStart, End => \&xmlEnd, Char => \&xmlChar );
 	$xmlParser->parse($content);
 	
 	unless ($vxmlDataRef->{'isValid'}) {
@@ -748,7 +751,7 @@ sub handle_url
 	# towards background player (producer/consumer pattern)
 	my $queue = new Thread::Queue();
 	
-	$abort_flag = 0; # reset flag
+	$abort_flag = 0;  # reset flag
 	
 	my $sound_creator = threads->new(
 		\&sound_creator_thread,
@@ -758,30 +761,30 @@ sub handle_url
 	
 	my $result = &background_player($queue, $http_req{'url'});
 	
-	$abort_flag = 1; # terminate sound_creator thread in case he's still alive
+	$abort_flag = 1;  # terminate sound_creator thread in case he's still alive
 	$sound_creator->join();
 	
 	if (!$result) {
 		print STDERR "no further destination available - we're done!\n" if (DEBUG_LEVEL >= 1);
-		return; # we're done ...
+		return;  # we're done ...
 	}
 	
 	my %next_dest = %{$result};
 	
 	if ($next_dest{'url'} =~ /^http/) {
-		&handle_url(%next_dest); # be recursive ...
+		&handle_url(%next_dest);  # be recursive ...
 	}
 	else {
 		# must be <transfer> link
 		my $dial_cmd = $DIALOUT;
-		$next_dest{'url'} =~ /:(.*)/; # remove protocol part, e.g. tel: or sip:
+		$next_dest{'url'} =~ /:(.*)/;  # remove protocol part, e.g. tel: or sip:
 		my $dial_target = $1;
 		$dial_cmd =~ s/\*/$dial_target/; # replace '*' in $DIALOUT with $dial_target
 		print STDERR "DIALOUT: execute $dial_cmd\n" if (DEBUG_LEVEL >= 1);
 		
 		print "EXEC $dial_cmd\n";
 		my $stdin = <STDIN>;
-		print STDERR "result of EXEC Dial: " . $stdin . "\n" if (DEBUG_LEVEL >= 1);
+		print STDERR "result of EXEC Dial: ". $stdin ."\n" if (DEBUG_LEVEL >= 1);
 	}
 }
 
@@ -807,6 +810,6 @@ print "EXEC Answer\n";
 my $stdin=<STDIN>;
 print STDERR "answer response: $stdin\n" if (DEBUG_LEVEL >= 1);
 
-my %http_req = ( url => $ARGV[0], method => 'get');
+my %http_req = (url => $ARGV[0], method => 'get');
 
 &handle_url(%http_req);
