a2b9af4682791b038db72686916a114af91deacb
peter <peter@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-10-09 10:43:34 +0000

    Hosts und Nodes vereinigt.
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2649 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/admin_hosts.php
+++ opt/gemeinschaft/htdocs/gui/mod/admin_hosts.php
@@ -34,6 +34,12 @@
 
 defined('GS_VALID') or die('No direct access.');
 
+function microtime_float()
+{
+	list($usec, $sec) = explode(' ', microTime());
+	return ((float)$usec + (float)$sec);
+}
+
 echo '<h2>';
 if (@$MODULES[$SECTION]['icon'])
 	echo '<img alt=" " src="', GS_URL_PATH, str_replace('%s', '32', $MODULES[$SECTION]['icon']), '" /> ';
@@ -76,6 +82,22 @@ if ($delete_host) {
 	$rs = $DB->execute($sql_query);
 }
 
+# get nodes from watchdog conf
+#
+include_once( GS_DIR .'etc/gs-cluster-watchdog.conf' );
+$nodesconf = $node;
+if (! is_array($nodesconf)) {
+	$warnings[] = __('Fehler beim Lesen der Nodes aus der Watchdog-Konfiguration!');
+	$nodesconf = array();
+}
+$nodes = array();
+foreach ($nodesconf as $node) {
+	$ip = normalizeIPs($node['dynamic_ip']);
+	$nodes[$ip] = $node;
+	$nodes[$ip]['active'  ] = false;
+	$nodes[$ip]['watchdog'] = true;
+}
+
 
 $sql_query = 'SELECT SQL_CALC_FOUND_ROWS `id`, `host`, `comment`
 FROM `hosts`
@@ -92,9 +114,14 @@ $num_pages = ceil($num_total / $per_page);
 <table cellspacing="1" class="phonebook">
 <thead>
 <tr>
-	<th style="width:50px;"><?php echo __('Nummer'); ?></th>
-	<th style="width:200px;"><?php echo __('Host IP'); ?></th>
-	<th style="width:200px;"><?php echo __('Kommentar'); ?></th>
+	<th style="width:50px;"><?php echo __('ID'); ?></th>
+	<th style="width:100px;"><?php echo __('IP (stat.)'); ?></th>
+	<th style="width:100px;"><?php echo __('IP (dyn.)'); ?> <sup>[1]</sup></th>
+	<th style="width:85px;"><?php echo __('Kommentar'); ?></th>
+	<th style="width:60px;"><?php echo __('Rolle'); ?></th>
+	<th style="width:45px;"><?php echo __('Stonith'); ?></th>
+	<th style="width:50px;"><?php echo __('Ping'); ?></th>
+	<th style="width:60px;"><?php echo __('SIP Ping'); ?></th>	
 	<th style="width:80px;">
 <?php
 echo ($page+1), ' / ', $num_pages, "&nbsp; \n";
@@ -135,6 +162,16 @@ if (@$rs) {
 	while ($r = $rs->fetchRow()) {
 		echo '<tr class="', ((++$i % 2) ? 'odd':'even'), '">', "\n";
 		
+		$ip = normalizeIPs($r['host']);
+		if (! @is_array($nodes[$ip])) {
+			$nodes[$ip] = array();
+		}
+
+		$nodes[$ip]['host_id' ] = $r['id'];
+		$nodes[$ip]['comment' ] = $r['comment'];
+		$nodes[$ip]['active'  ] = true;
+		$nodes[$ip]['watchdog'] = false;
+
 		if ($edit_host == $r['id']){
 			echo '<form method="post" action="', GS_URL_PATH, '">', "\n";
 			echo gs_form_hidden($SECTION, $MODULE), "\n";
@@ -142,7 +179,7 @@ if (@$rs) {
 			echo '<input type="hidden" name="save" value="', $r['id'] , '" />', "\n";
 			echo '<td>', htmlEnt($r['id']);
 			echo '</td>';
-			
+			echo '<td>', htmlEnt( @$nodes[$ip]['static_ip'] ), '</td>';
 			echo '<td>';
 			echo '<input type="text" name="host" value="'.htmlEnt($r['host']).'" size="20" maxlength="25" />';
 			echo '</td>';
@@ -150,6 +187,54 @@ if (@$rs) {
 			echo '<td>';
 			echo '<input type="text" name="comment" value="'.htmlEnt($r['comment']).'" size="25" maxlength="25" />';
 			echo '</td>';
+			echo '<td>', ($nodes[$ip]['active']
+			? '<span style="color:#0a0;">'. __('Aktiv'  ) .'</span>'
+			: '<span style="color:#777;">'. __('Reserve') .'</span>'),
+			'</td>';
+			echo '<td>';
+			if ($nodes[$ip]['watchdog'])
+				echo '<img alt="ja" src="', GS_URL_PATH, 'crystal-svg/16/act/ok.png" /><sup>&nbsp;</sup>';
+			else
+				echo '<img alt="nein" src="', GS_URL_PATH, 'crystal-svg/16/act/redled.png" /><sup>[2]</sup>';
+			echo '</td>';
+			
+			echo '<td class="r">';
+			$timeout = 1;
+			$cmd = 'ping -n -q -w '. $timeout .' -c 1 '. qsa($ip);
+			$out = array();
+			$start = microtime_float();
+			@exec($cmd .' >>/dev/null 2>&1', $out, $ping_err);
+			$time = (microtime_float() - $start) * 0.5;  # script startup time
+			if ($ping_err==0) {
+				echo '<span style="color:#0a0;">', round($time*1000), '&nbsp;ms</span>';
+			} else {
+				echo '<b style="color:#f00;">?</b>';
+			}
+			echo '</td>';
+			
+			if ($nodes[$ip]['active']) {
+				echo '<td class="r">';
+				if ($ping_err==0) {
+					$timeout = 2;
+					$cmd = 'PATH=$PATH:/usr/local/bin; '. GS_DIR .'sbin/check-sip-alive '. qsa('sip:checkalive@'. $ip) .' '. $timeout;
+					$out = array();
+					$start = microtime_float();
+					@exec($cmd .' 2>&1', $out, $err);
+					$time = (microtime_float() - $start) * 0.7;  # script startup time
+					$out = strToUpper(trim(implode("\n", $out)));
+					if ($err==0 && subStr($out,0,2)=='OK') {
+						echo '<span style="color:#0a0;">', round($time*1000), '&nbsp;ms</span>';
+					} else {
+						if ($out=='FAIL')
+							echo '<b style="color:#f00;">&gt;', $timeout, '&nbsp;s</b>';
+						else
+							echo '<b style="color:#f00;">?</b>';
+					}
+				} else
+					echo '<b style="color:#f00;">?</b>';
+				echo '</td>';
+			} else
+				echo '<td class="r">-</td>';
 			
 			echo '<td>';
 			echo '<button type="submit" title="', __('Speichern'), '" class="plain">';
@@ -165,12 +250,60 @@ if (@$rs) {
 			
 			echo '<td>', htmlEnt($r['id']);
 			echo '</td>';
-			
+			echo '<td>', htmlEnt( @$nodes[$ip]['static_ip'] ), '</td>';
 			echo '<td>', htmlEnt($r['host']);
 			echo '</td>';
-			
 			echo '<td>', htmlEnt($r['comment']),'</td>';
-			//echo '<td>', htmlEnt($r['hnr']), '</td>';
+			echo '<td>', ($nodes[$ip]['active']
+			? '<span style="color:#0a0;">'. __('Aktiv'  ) .'</span>'
+			: '<span style="color:#777;">'. __('Reserve') .'</span>'),
+			'</td>';
+			echo '<td>';
+			if ($nodes[$ip]['watchdog'])
+				echo '<img alt="ja" src="', GS_URL_PATH, 'crystal-svg/16/act/ok.png" /><sup>&nbsp;</sup>';
+			else
+				echo '<img alt="nein" src="', GS_URL_PATH, 'crystal-svg/16/act/redled.png" /><sup>[2]</sup>';
+			echo '</td>';
+			
+			echo '<td class="r">';
+			$timeout = 1;
+			$cmd = 'ping -n -q -w '. $timeout .' -c 1 '. qsa($ip);
+			$out = array();
+			$start = microtime_float();
+			@exec($cmd .' >>/dev/null 2>&1', $out, $ping_err);
+			$time = (microtime_float() - $start) * 0.5;  # script startup time
+			if ($ping_err==0) {
+				echo '<span style="color:#0a0;">', round($time*1000), '&nbsp;ms</span>';
+			} else {
+				echo '<b style="color:#f00;">?</b>';
+			}
+			echo '</td>';
+			
+			if ($nodes[$ip]['active']) {
+				echo '<td class="r">';
+				if ($ping_err==0) {
+					$timeout = 2;
+					$cmd = 'PATH=$PATH:/usr/local/bin; '. GS_DIR .'sbin/check-sip-alive '. qsa('sip:checkalive@'. $ip) .' '. $timeout;
+					$out = array();
+					$start = microtime_float();
+					@exec($cmd .' 2>&1', $out, $err);
+					$time = (microtime_float() - $start) * 0.7;  # script startup time
+					$out = strToUpper(trim(implode("\n", $out)));
+					if ($err==0 && subStr($out,0,2)=='OK') {
+						echo '<span style="color:#0a0;">', round($time*1000), '&nbsp;ms</span>';
+					} else {
+						if ($out=='FAIL')
+							echo '<b style="color:#f00;">&gt;', $timeout, '&nbsp;s</b>';
+						else
+							echo '<b style="color:#f00;">?</b>';
+					}
+				} else
+					echo '<b style="color:#f00;">?</b>';
+				echo '</td>';
+			} else
+				echo '<td class="r">-</td>';
+			
+			
 			
 			echo '<td>';
 			
@@ -196,12 +329,14 @@ if (!$edit_host) {
 	<td>
 		<input type="text" name="hostid" value="" size="5" maxlength="25" />
 	</td>
+	<td></td>
 	<td>
 		<input type="text" name="host" value="" size="20" maxlength="25" />
 	</td>
 	<td>
 		<input type="text" name="comment" value="" size="25" maxlength="45" />
 	</td>
+	<td></td><td></td><td></td><td></td>
 	<td>
 		<button type="submit" title="<?php echo __('Host anlegen'); ?>" class="plain">
 			<img alt="<?php echo __('Speichern'); ?>" src="<?php echo GS_URL_PATH; ?>crystal-svg/16/act/filesave.png" />
@@ -218,3 +353,7 @@ if (!$edit_host) {
 </tbody>
 </table>
 
+<br /><br />
+<p style="max-width:500px;"><small><sup>[1]</sup> <?php echo __('_ Dies ist die Adresse, die ggf. per Stonith &uuml;bernommen werden w&uuml;rde. (Die dynamische Adresse hat hier nichts mit DHCP zu tun.)'); /* //TRANSLATE ME */ ?></small></p>
+
+<p style="max-width:500px;"><small><sup>[2]</sup> <?php echo __('Nicht konfiguriert.'); ?></small></p>
