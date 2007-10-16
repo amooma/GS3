073f576521fa6e1a85dc355d46b80e29871f4670
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-10-16 18:22:00 +0000

    LGPL -> GPL
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2743 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/lib/yadb/drv/mysql.php
+++ opt/gemeinschaft/lib/yadb/drv/mysql.php
@@ -7,26 +7,28 @@
 * reuse = true,  to re-use an existing connection
 ***********************************************************/
 
-/*
+/*******************************************************************\
+*                               YaDB
+* 
 * Copyright 2006/2007, Philipp Kempgen <philipp.kempgen@amooma.de>,
 * amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
-* http://www.amooma.de/;
+* http://www.amooma.de/
 * 
 * This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public License
-* (GNU/LGPL) as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
 * 
 * This program is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Lesser General Public License for more details.
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
 * 
-* You should have received a copy of the GNU Lesser General Public
-* License along with this program; if not, write to the Free
-* Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-* Boston, MA 02110-1301, USA.
-*/
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
 
 if (!defined('YADB_DIR')) die();
 
