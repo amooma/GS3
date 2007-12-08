c1d81cf3cfbe897cb4fd8450324571d0c4426b53
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-08 21:49:13 +0000

    Pfad gefixt, damit das Skript auch bei URL-Rewriting funktioniert
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2952 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/js/pngbehavior.htc.php
+++ opt/gemeinschaft/htdocs/gui/js/pngbehavior.htc.php
@@ -1,27 +1,44 @@
 <?php
 
-header( 'Content-Type: text/x-component' );
-header( 'Pragma: cache' );
-header( 'Last-Modified: '. gmDate('D, d M Y 00:00:00') .' GMT');
-header( 'Expires: '. gmDate('D, d M Y 23:59:59') .' GMT');
-header( 'ETag: '. date('Ymd') );
+header( 'Content-Type: text/plain' );
+//header( 'Content-Type: text/x-component' );
+//header( 'Pragma: cache' );
+header( 'Cache-Control: public, max-age=120, must-revalidate' );
+//header( 'Last-Modified: '. gmDate('D, d M Y 00:00:00') .' GMT');
+//header( 'Expires: '. gmDate('D, d M Y 23:59:59') .' GMT');
+header( 'Expires: '. gmDate('D, d M Y H:i:s', time()+120) .' GMT');
+//header( 'ETag: '. gmDate('Ymd') );
+
+define( 'GS_VALID', true );  /// this is a parent file
+require_once( dirName(__FILE__) .'/../../../inc/conf.php' );
+
+# set paths
+#
+$GS_URL_PATH = dirName(dirName(@$_SERVER['SCRIPT_NAME']));
+if (subStr($GS_URL_PATH,-1,1) != '/') $GS_URL_PATH .= '/';
+
+header( 'ETag: '. gmDate('Ymd').'-'.md5($GS_URL_PATH) );
+
 
 function _not_modified()
 {
 	header( 'HTTP/1.0 304 Not Modified', true, 304 );
 	header( 'Status: 304 Not Modified', true, 304 );
-	die();
+	exit();
 }
 
 if (array_key_exists('HTTP_IF_NONE_MATCH', $_SERVER)
-&&  $_SERVER['HTTP_IF_NONE_MATCH'] == date('Ymd') ) {
+&&  $_SERVER['HTTP_IF_NONE_MATCH'] === gmDate('Ymd').'-'.md5($GS_URL_PATH) ) {
 	_not_modified();
 }
+/*
 $tmp = gmDate('D, d M Y');
 if (array_key_exists('HTTP_IF_MODIFIED_SINCE', $_SERVER)
 &&  subStr($_SERVER['HTTP_IF_MODIFIED_SINCE'],0,strLen($tmp)) == $tmp ) {
 	_not_modified();
 }
+*/
+
 
 ob_start();
 
@@ -57,7 +74,7 @@ Some improvements by me.
 -- Philipp Kempgen
 */
 
-var pngb_blank_src = 'img/blank.gif';
+var pngb_blank_src = '<?php echo $GS_URL_PATH; ?>img/blank.gif';
 var pngb_real_src;
 var pngb_is_printing = false;
 var pngb_supported = element.nodeName.toUpperCase()=='IMG' &&
@@ -126,6 +143,6 @@ if (pngb_supported) pngb_fix_png();
 if (! headers_sent())
 	header( 'Content-Length: '. (int)ob_get_length() );
 ob_end_flush();
-die();
+exit();
 
 ?>
