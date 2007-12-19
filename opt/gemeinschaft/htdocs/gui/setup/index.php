918d6cdfb5cdbd5b4a7cafef167ea63b5de63066
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-19 22:09:11 +0000

    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2978 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/htdocs/gui/setup/index.php
@@ -0,0 +1,42 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+#defined('GS_VALID') or die('No direct access.');
+
+
+# set URL path
+#
+$GS_URL_PATH = dirName(@$_SERVER['SCRIPT_NAME']);
+if (subStr($GS_URL_PATH,-1,1) != '/') $GS_URL_PATH .= '/';
+define( 'GS_URL_PATH', $GS_URL_PATH );
+unset($GS_URL_PATH);
+
+
+echo GS_URL_PATH;
+
+
+?>
