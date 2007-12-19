2fc02e369e05a7917ee84ee43196f1a9d9e0f050
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-19 14:49:15 +0000

    GUI_SHUTDOWN_ENABLED abhängig von INSTALLATION_TYPE
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2960 2d91074e-20bd-43ae-8273-9033e1f9829e

--- etc/gemeinschaft/gemeinschaft.php
+++ etc/gemeinschaft/gemeinschaft.php
@@ -122,8 +122,10 @@ $GUI_MON_PEERS_ENABLED      = false;
   # needs LDAP with Kostenstelle or a similar mechanism, see
   # gui_monitor_which_peers() in htdocs/gui/inc/permissions.php
 
-$GUI_SHUTDOWN_ENABLED       = false;
+#$GUI_SHUTDOWN_ENABLED       = false;
   # enable shutdown via web interface?
+  # default: true for INSTALLATION_TYPE=='embedded', false
+  # otherwise
 
 
 /***********************************************************
