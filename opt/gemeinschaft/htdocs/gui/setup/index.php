258fd1141a804583189ac2aadfbfd7441c15d905
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-21 23:52:46 +0000

    Setup
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@2999 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/index.php
+++ opt/gemeinschaft/htdocs/gui/setup/index.php
@@ -37,7 +37,7 @@ require_once( GS_DIR .'inc/keyval.php' );
 
 # set URL path
 #
-$GS_URL_PATH = dirName(@$_SERVER['SCRIPT_NAME']);
+$GS_URL_PATH = dirName(dirName(@$_SERVER['SCRIPT_NAME']));
 if (subStr($GS_URL_PATH,-1,1) != '/') $GS_URL_PATH .= '/';
 define( 'GS_URL_PATH', $GS_URL_PATH );
 unset($GS_URL_PATH);
@@ -70,10 +70,12 @@ session_start();
 
 # set language
 #
+/*
 if (array_key_exists('setlang', $_REQUEST)) {
 	$setlang = preg_replace('/[^a-z\d_]/i', '', @$_REQUEST['setlang']);
 	@$_SESSION['lang'] = $setlang;
 }
+*/
 if (array_key_exists('lang', $_SESSION))
 	$ret = gs_setlang( $_SESSION['lang'] );
 else
@@ -90,7 +92,8 @@ gs_settextdomain( 'gemeinschaft-gui' );
 $login_info   = '';
 $login_errmsg = '';
 if (! @$_SESSION['login_ok']) {
-	if (gs_setup_autoshow()) {
+	$_SESSION['login_ok'] = false;
+	if (gs_keyval_get('setup_pwd') == '') {
 		$_SESSION['login_ok'] = true;
 	}
 	else {
@@ -114,11 +117,56 @@ if (! @$_SESSION['login_ok']) {
 }
 
 
+# get step
+#
+$step = preg_replace('/[^a-z0-9\-_]/', '', @$_REQUEST['step']);
+if ($step == '') $step = 'system-check';
+$step_file = GS_DIR .'htdocs/gui/setup/mod/'.$step.'.php';
+if (! file_exists($step_file)) {
+	@header( 'HTTP/1.0 404 Not Found', true, 404 );
+	@header( 'Status: 404 Not Found', true, 404 );
+	exit(1);
+}
 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php echo @$_SESSION['isolang']; ?>" xml:lang="<?php echo @$_SESSION['isolang']; ?>">
+<head><!--<![CDATA[
+                Gemeinschaft
+  @(_)=====(_)  (c) 2007, amooma GmbH - http://www.amooma.de
+ @   / ### \    Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+ @  |  ###  |   Philipp Kempgen <philipp.kempgen@amooma.de>
+  @@|_______|   Peter Kozak <peter.kozak@amooma.de>
+                                                      GNU GPL ]]>-->
+<title><?php echo __('Gemeinschaft Setup'); ?></title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php echo GS_URL_PATH; ?>styles/original.css" />
+<link rel="shortcut icon" type="image/x-icon" href="<?php echo GS_URL_PATH; ?>favicon.ico" />
+<meta http-equiv="imagetoolbar" content="no" />
+<meta http-equiv="pragma" content="no-cache" />
+<meta http-equiv="cache-control" content="no-cache" />
+<!-- for stupid MSIE: -->
+<!--[if IE]><link rel="stylesheet" type="text/css" href="<?php echo GS_URL_PATH; ?>styles/msie-fix.css" /><![endif]-->
+<!--[if lte IE 6]><style type="text/css">img {behavior: url("<?php echo GS_URL_PATH; ?>js/pngbehavior.htc.php?msie-sucks=.htc");}</style><![endif]-->
+</head>
+<body>
+<div id="topheader"></div>
+<div id="headerboxes">
+<div id="boxtitle">
+	<img alt=" " src="<?php echo GS_URL_PATH; ?>crystal-svg/32/app/yast_PhoneTTOffhook.png" class="fl" />
+	<h1><?php echo __('Gemeinschaft Setup') /*//TRANSLATE ME*/; ?></h1> 
+</div>
+</div>
 
-echo GS_URL_PATH ,"<br/>\n";
-print_r($_SESSION);
+<?php
 
+//echo GS_URL_PATH ,"<br/>\n";
+//print_r($_SESSION);
+
+@include_once( $step_file );
 
 
 ?>
+
+</body>
+</html>
