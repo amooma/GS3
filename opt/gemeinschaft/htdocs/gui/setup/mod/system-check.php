786ae24f17ce1b07bfd49405b94ecffc30ff5106
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-23 22:59:09 +0000

    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3018 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/mod/system-check.php
+++ opt/gemeinschaft/htdocs/gui/setup/mod/system-check.php
@@ -27,7 +27,7 @@
 \*******************************************************************/
 defined('GS_VALID') or die('No direct access.');
 require_once( GS_DIR .'inc/find_executable.php' );
-$can_continue = false;
+$can_continue = true;
 
 
 ?>
@@ -55,111 +55,423 @@ $can_continue = false;
 		case 'single':  echo 'Einzel-Server'; break;
 		default      :  echo 'unbekannt'    ; break;
 	}
-	echo '</b>' ,"\n";
+	echo '</b>, ';
+	echo 'Sprache' ,': <b>', 'Deutsch' ,'</b><br />' ,"\n";
 ?>
 </p>
 <hr />
-<br />
-
 
 <?php
 
-echo 'Sprache' ,': <b>', 'Deutsch' ,'</b><br />' ,"\n";
 
-echo 'Ausf&uuml;hrungsrechte' ,': <b>';
-/*
-$sapi = php_sapi_name();
-if (subStr($sapi, 0, 6) !== 'apache') {
-	echo 'UNBEKANNT (kein Apache)';
-} else {
-	if (subStr($sapi, 6, 1) === '2')
-		$apachectl_basename = 'apache2ctl';
-	else
-		$apachectl_basename = 'apachectl';
-	$apachectl = find_executable($apachectl_basename, array(
-		'/usr/sbin/',
-		'/usr/bin/'
-	));
-	if (! $apachectl) {
-		echo 'UNBEKANNT ('.$apachectl_basename.' nicht gefunden)';
-	}
-	$err=0; $out=array();
-	@exec( $apachectl.' -V 2>>/dev/null | grep -a SERVER_CONFIG_FILE 2>>/dev/null', $out, $err );
-	if ($err !== 0) {
-		echo 'UNBEKANNT ('.$apachectl_basename.' nicht ausf&uuml;hrbar)';
-	} else {
-		if (preg_match('/SERVER_CONFIG_FILE="(\/[^"]+)/', implode("\n",$out), $m))
-			$apache_conf = $m[1];
-		elseif (preg_match('/(\/etc\/(?:apache|http)[a-z0-9\/\.\-_]+)/', implode("\n",$out), $m))
-			$apache_conf = $m[1];
-		else
-			$apache_conf = null;
-		if (! $apache_conf) {
-			echo 'UNBEKANNT (Apache-Konfiguration nicht gefunden)';
-		} else {
-			echo $apache_conf;
-			// ...
-		}
-	}
+function php_ini_true( $val )
+{
+	return ($val == '1' || strToLower($val) == 'on');
 }
-*/
-
-/*
-$err=0; $out=array();
-@exec( 'cat /etc/sudoers | grep -v -E '. escapeShellArg('^\s*(#|$)') .' 2>>/dev/null', $out, $err );
-if ($err !== 0) {
-	echo 'FEHLER';
-} else {
-	$user_info = posix_getpwuid(posix_geteuid());
-	print_r($out);
+function php_ini_false( $val )
+{
+	return ($val == '0' || strToLower($val) == 'off' || $val == '');
 }
-*/
-
-$user_info = @posix_getpwuid(@posix_geteuid());
-$username = @$user_info['name'];
-unset($user_info);
-
-$expect_basename = 'expect';
-$expect = find_executable($expect_basename, array(
-	'/usr/bin/',
-	'/usr/sbin/',
-	'/usr/local/bin/',
-	'/usr/local/sbin/'
-));
-if (! $expect) {
-	echo sPrintF('FEHLER (&quot;%s&quot; nicht gefunden)', $expect_basename);
-} else {
-	$gs_sbin_dir = '/opt/gemeinschaft/sbin/';
-	$sudo_check      = $gs_sbin_dir .'sudo-check';
-	$sudo_sudo_check = $gs_sbin_dir .'sudo-sudo-check';
-	if (! file_exists($sudo_check)
-	||  ! file_exists($sudo_sudo_check)) {
-		echo 'FEHLER (Test-Skript nicht gefunden)';
-	} else {
-		$err=0; $out=array();
-		@exec( $sudo_check .' 1>>/dev/null 2>>/dev/null', $out, $err );
-		if ($err !== 0) {
-			echo sPrintF('FEHLER (User &quot;%s&quot; hat nicht gen&uuml;gend Rechte)', $username);
-		} else {
-			$err=0; $out=array();
-			@exec( $sudo_sudo_check .' 1>>/dev/null 2>>/dev/null', $out, $err );
-			if ($err !== 0) {
-				echo sPrintF('FEHLER (User &quot;%s&quot; hat nicht gen&uuml;gend Rechte)', 'root');
-			} else {
-				echo 'OK';
-				$can_continue = true;
-			}
-		}
-	}
+function php_ini_bool_verbose( $val )
+{
+	return ($val == '1' || strToLower($val) == 'on') ? 'On' : 'Off';
 }
-echo '</b><br />' ,"\n";
-
+function _test_result( $str, $status )
+{
+	global $can_continue;
+	echo 'class="test_res test_',$status,'">', $str;
+	if (! in_array($status, array('ok', 'notice'), true))
+		$can_continue = false;
+}
 
 
 ?>
+
+<table cellspacing="1" border="0">
+<tbody>
+
+<tr>
+	<td width="120"><?php echo 'Betriebssystem'; ?>:</td>
+	<td width="240"><?php echo htmlEnt(PHP_OS); ?></td>
+	<td width="110" <?php
+		switch (PHP_OS) {
+			case 'Linux':
+				echo _test_result('OK'                     ,'ok'  );
+				break;
+			default     :
+				echo _test_result('NICHT UNTERST&Uuml;TZT', 'warn');
+				break;
+		}
+	?></td>
+</tr>
+
+<tr>
+	<td><?php echo 'PHP-Version'; ?>:</td>
+	<td><?php echo htmlEnt(PHP_VERSION); ?></td>
+	<td <?php
+		if     (version_compare(PHP_VERSION, '5.2') >= 0)
+			echo _test_result('OK', 'ok');
+		elseif (version_compare(PHP_VERSION, '5'  ) >= 0)
+			echo _test_result('NICHT UNTERST&Uuml;TZT', 'warn');
+		elseif (version_compare(PHP_VERSION, '4.3') >= 0)
+			echo _test_result('OK', 'ok');
+		else
+			echo _test_result('NICHT UNTERST&Uuml;TZT', 'warn');
+	?></td>
+</tr>
+
+<tr>
+	<td><?php echo 'PHP-Module'; ?>:</td>
+	<td><tt><?php echo 'ftp'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('ftp')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'ldap'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('ldap')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'mbstring'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('mbstring')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'mysql'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('mysql')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'pcre'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('pcre')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'posix'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('posix')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'session'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('session')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt><?php echo 'sockets'; ?> ?</tt></td>
+	<td <?php
+		echo (extension_loaded('sockets')
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn') );	?></td>
+</tr>
+
+<tr>
+	<td><?php echo 'PHP-Optionen'; ?>:</td>
+	<td><?php
+		$k = 'safe_mode';
+		$v = ini_get($k);
+		echo '<tt>', $k ,': ', php_ini_bool_verbose($v) ,'</tt>';
+	?></td>
+	<td <?php
+		echo php_ini_false($v)
+			? _test_result('OK', 'ok')
+			: _test_result('FEHLER', 'warn');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'register_globals';
+		$v = ini_get($k);
+		echo '<tt>', $k ,': ', php_ini_bool_verbose($v) ,'</tt>';
+	?></td>
+	<td <?php
+		echo php_ini_false($v)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT OPTIMAL', 'notice');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'register_long_arrays';
+		$v = ini_get($k);
+		echo '<tt>', $k ,': ', php_ini_bool_verbose($v) ,'</tt>';
+	?></td>
+	<td <?php
+		echo php_ini_false($v)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT OPTIMAL', 'notice');	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'magic_quotes_gpc';
+		$v = get_magic_quotes_gpc();
+		echo '<tt>', $k ,': ', php_ini_bool_verbose($v) ,'</tt>';
+	?></td>
+	<td <?php
+		echo php_ini_false($v)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT OPTIMAL', 'notice');	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'magic_quotes_runtime';
+		$v = get_magic_quotes_runtime();
+		echo '<tt>', $k ,': ', php_ini_bool_verbose($v) ,'</tt>';
+	?></td>
+	<td <?php
+		echo php_ini_false($v)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT OPTIMAL', 'notice');	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'max_execution_time';
+		$v = (int)ini_get('max_execution_time');
+		echo '<tt>', $k ,': ', $v ,'</tt>';
+	?></td>
+	<td <?php
+		echo ($v >= 30 || $v == 0)
+			? _test_result('OK', 'ok')
+			: _test_result('ZU KURZ', 'notice');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		set_time_limit(40);
+		$k = 'max_execution_time';
+		$v = (int)ini_get('max_execution_time');
+		$ok = ($v == 40);
+		echo '<tt>', $k ,' setzbar? ', ($ok ? 'ja' : 'nein') ,'</tt>';
+	?></td>
+	<td <?php
+		echo $ok
+			? _test_result('OK', 'ok')
+			: _test_result('FEHLER', 'notice');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><?php
+		$k = 'mbstring.func_overload';
+		$v = (int)ini_get('mbstring.func_overload');
+		echo '<tt>', $k ,': ', $v ,'</tt>';
+	?></td>
+	<td <?php
+		echo ($v == 0)
+			? _test_result('OK', 'ok')
+			: _test_result('FEHLER', 'warn');
+	?></td>
+</tr>
+
+<tr>
+	<td><?php echo 'SAPI'; ?>:</td>
+	<td><?php
+		$sapi = php_sapi_name();
+		switch ($sapi) {
+			case 'apache2handler': echo 'Apache 2 + <tt>mod_php</tt>'; break;
+			case 'apache2filter' : echo 'Apache 2 + <tt>mod_php</tt>'; break;
+			case 'apache'        : echo 'Apache 1 + <tt>mod_php</tt>'; break;
+			case 'cgi-fcgi'      : echo 'FCGI'                       ; break;
+			case 'cgi'           : echo 'CGI'                        ; break;
+			case 'cli'           : echo 'CLI (RedHat?)'              ; break;
+			default              : echo $sapi                        ; break;
+		}
+		?></td>
+		<td <?php
+		switch ($sapi) {
+			case 'apache2handler': echo _test_result('OK'                    , 'ok'    ); break;
+			case 'apache2filter' : echo _test_result('OK'                    , 'ok'    ); break;
+			case 'apache'        : echo _test_result('OK'                    , 'ok'    ); break;
+			case 'cgi-fcgi'      : echo _test_result('OK'                    , 'ok'    ); break;
+			case 'cgi'           : echo _test_result('NICHT OPTIMAL'         , 'notice'); break;
+			case 'cli'           : echo _test_result('NICHT OPTIMAL'         , 'notice'); break;
+			default              : echo _test_result('NICHT UNTERST&Uuml;TZT', 'warn'  ); break;
+		}
+	?></td>
+</tr>
+
+<tr>
+	<td><?php echo 'Apache-Module'; ?>:</td>
+	<td><tt><?php
+		$have_apache_get_modules = false;
+		if (subStr($sapi,0,6) !== 'apache') {
+			echo 'nicht zutreffend';
+		} else {
+			if (! function_exists('apache_get_modules')) {
+				echo 'keine Informationen';
+			} else {
+				$have_apache_get_modules = true;
+				$apache_mods = array_flip(apache_get_modules());
+				echo '&nbsp;';
+			}
+		}
+	?></tt></td>
+	<td>&nbsp;</td>
+</tr>
+<?php if ($have_apache_get_modules) { ?>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt>mod_env ?</tt></td>
+	<td <?php
+		echo array_key_exists('mod_env', $apache_mods)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt>mod_mime ?</tt></td>
+	<td <?php
+		echo array_key_exists('mod_mime', $apache_mods)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT GELADEN', 'warn');
+	?></td>
+</tr>
+<tr>
+	<td>&nbsp;</td>
+	<td><tt>mod_rewrite ?</tt></td>
+	<td <?php
+		echo array_key_exists('mod_rewrite', $apache_mods)
+			? _test_result('OK', 'ok')
+			: _test_result('NICHT OPTIMAL', 'notice');
+	?></td>
+</tr>
+<?php } ?>
+
+<tr>
+	<td><?php echo 'Benutzer'; ?>:</td>
+	<td><?php
+		$euser = posix_getpwuid(posix_geteuid());
+		if (is_array($euser))
+			echo '<tt>', @$euser['name'], '</tt>';
+		else
+			echo '?';
+		echo ' / ';
+		$egroup = posix_getpwuid(posix_getegid());
+		if (is_array($egroup))
+			echo '<tt>', @$egroup['name'] ,'</tt>';
+		else
+			echo '?';
+	?></td>
+	<td>&nbsp;</td>
+</tr>
+<?php /* ?>
+<tr>
+	<td><?php echo 'Benutzerrechte'; ?>:</td>
+	<td><?php
+		$ok = false;
+		$group_gemeinschaft = 'root';
+		$grp_gemeinschaft = posix_getgrnam($group_gemeinschaft);
+		if (! is_array($grp_gemeinschaft)) {
+			echo 'Gruppe <tt>gemeinschaft</tt> nicht vorhanden.';
+		} else {
+			$groups = posix_getgroups();
+			if (! is_array($groups)) {
+				echo '-';
+			} else {
+				foreach ($groups as $gid) {
+					$group = posix_getgrgid($gid);
+					if (is_array($group) && $group['name']===$group_gemeinschaft) {
+						echo 'Gruppe <tt>gemeinschaft</tt>';
+						$ok = true;
+						break;
+					}
+				}
+				echo 'Nicht Mitglied der Gruppe <tt>gemeinschaft</tt>';
+			}
+		}
+	?></td>
+	<td <?php
+		echo $ok
+			? _test_result('OK', 'ok')
+			: _test_result('FEHLER', 'warn');
+	?></td>
+</tr>
+<?php */ ?>
+<tr>
+	<td><?php echo 'Ausf&uuml;hrungsrechte'; ?>:</td>
+	<td>&nbsp;</td>
+	<td <?php
+		$user_info = @posix_getpwuid(@posix_geteuid());
+		$username = @$user_info['name'];
+		unset($user_info);
+		
+		$expect_basename = 'expect';
+		$expect = find_executable($expect_basename, array(
+			'/usr/bin/',
+			'/usr/sbin/',
+			'/usr/local/bin/',
+			'/usr/local/sbin/'
+		));
+		if (! $expect) {
+			_test_result( sPrintF('FEHLER (&quot;%s&quot; nicht gefunden)', $expect_basename), 'warn' );
+		} else {
+			$gs_sbin_dir = '/opt/gemeinschaft/sbin/';
+			$sudo_check      = $gs_sbin_dir .'sudo-check';
+			$sudo_sudo_check = $gs_sbin_dir .'sudo-sudo-check';
+			if (! file_exists($sudo_check)
+			||  ! file_exists($sudo_sudo_check)) {
+				_test_result( 'FEHLER (Test-Skript nicht gefunden)', 'warn' );
+			} else {
+				$err=0; $out=array();
+				@exec( $sudo_check .' 1>>/dev/null 2>>/dev/null', $out, $err );
+				if ($err !== 0) {
+					_test_result( sPrintF('FEHLER (User &quot;%s&quot; hat nicht gen&uuml;gend Rechte)', $username), 'warn' );
+				} else {
+					$err=0; $out=array();
+					@exec( $sudo_sudo_check .' 1>>/dev/null 2>>/dev/null', $out, $err );
+					if ($err !== 0) {
+						_test_result( sPrintF('FEHLER (User &quot;%s&quot; hat nicht gen&uuml;gend Rechte)', 'root'), 'warn' );
+					} else {
+						_test_result( 'OK', 'ok' );;
+						//$can_continue = true;
+					}
+				}
+			}
+		}
+	?></td>
+</tr>
+
+</tbody>
+</table>
+
 <br />
 <hr />
 <?php
+
+
+/*
+    check php-cli as well !?
+*/
+
+
 
 if (! $can_continue) {
 	echo 'In diesem Setup-Schritt sind Fehler aufgetreten!' ,"\n";
