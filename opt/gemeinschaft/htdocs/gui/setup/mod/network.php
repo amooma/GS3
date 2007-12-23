28268c5a6b231f84cd8c8ddc8a8ef28b7b960168
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2007-12-23 18:03:21 +0000

    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3013 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/mod/network.php
+++ opt/gemeinschaft/htdocs/gui/setup/mod/network.php
@@ -36,8 +36,7 @@ $can_continue = false;
 <h1><?php echo 'Netzwerk'; ?></h1>
 <p>
 <?php
-	$installation_type = gs_get_conf('GS_INSTALLATION_TYPE');
-	switch ($installation_type) {
+	switch ($GS_INSTALLATION_TYPE) {
 		case 'gpbx':
 			echo 'Bitte stellen Sie die gew&uuml;nschte IP-Adresse der GPBX ein. Geben Sie au&szlig;erdem die Netzmaske Ihres Netzwerkes sowie die Adresse Ihres Routers, Nameservers und Zeitservers an.' ,"\n";
 			echo 'Die Netzwerkkarte der GPBX wird dann auf diese Werte eingestellt.' ,"\n";
@@ -51,6 +50,75 @@ $can_continue = false;
 </p>
 <hr />
 <br />
+<?php
+
+echo "<pre>\n";
+print_r($_REQUEST);
+echo "</pre>\n";
+
+define( 'GS_VALIDATION_OK'  , 2 );
+define( 'GS_VALIDATION_WARN', 1 );
+define( 'GS_VALIDATION_ERR' , 0 );
+
+function _normalize_ip_addr( $addr )
+{
+	$addr = preg_replace('/[^0-9.]/', '', $addr);
+	if ($addr == '') return null;
+	$addr_parts = explode('.', $addr);
+	$addr = '';
+	for ($i=0; $i<=3; ++$i) {
+		$part = (int)lTrim(@$addr_parts[$i], '0 ');
+		if     ($part > 255) $part = 255;
+		elseif ($part <   0) $part =   0;
+		$addr .= $part;
+		if ($i < 3) $addr .= '.';
+	}
+	return $addr;
+}
+
+function _validate_input_ipaddr( &$addr, &$errmsg )
+{
+	$addr = _normalize_ip_addr( $addr );
+	$errmsg = '';
+	if ($addr === null) {
+		$errmsg = 'IP-Adresse leer!';
+		return GS_VALIDATION_ERR;
+	}
+	if (subStr($addr,0, 2) === '0.'
+	||  subStr($addr,0, 3) === '39.'
+	||  subStr($addr,0, 4) === '127.'
+	||  subStr($addr,0, 6) === '128.0.'
+	||  subStr($addr,0, 8) === '169.254.'
+	||  subStr($addr,0, 8) === '191.255.'
+	||  subStr($addr,0, 8) === '192.0.0.'
+	||  subStr($addr,0, 8) === '192.0.2.'
+	||  subStr($addr,0,10) === '192.88.99.'
+	||  subStr($addr,0,12) === '223.255.255.'
+	||  subStr($addr,0, 4) === '224.'
+	||  subStr($addr,0, 4) === '240.'
+	||  subStr($addr,0,15) === '255.255.255.255'
+	) {
+		$errmsg = 'Ung&uuml;ltige IP-Adresse!';
+		return GS_VALIDATION_ERR;
+	}
+	if (preg_match('/255/', $addr)
+	||  subStr($addr, -2) === '.0') {
+		$errmsg = 'Ung&uuml;ltige IP-Adresse!';
+		return GS_VALIDATION_ERR;
+	}
+	return GS_VALIDATION_OK;
+}
+$addr = @$_REQUEST['ipaddr'][0] .'.'. @$_REQUEST['ipaddr'][1] .'.'. @$_REQUEST['ipaddr'][2] .'.'. @$_REQUEST['ipaddr'][3];
+echo _validate_input_ipaddr( $addr, $errmsg );
+
+
+function _have_vlan_support()
+{
+	return @file_exists( '/proc/net/vlan' );
+}
+
+
+?>
 
 <form method="post" action="<?php echo GS_URL_PATH ,'setup/?step=network'; ?>">
 <input type="hidden" name="action" value="check" />
@@ -63,9 +131,8 @@ $can_continue = false;
 <?php
 		$ipaddr_parts = explode('.', @$_SERVER['SERVER_ADDR']);
 		for ($i=0; $i<=3; ++$i) {
-			$part = lTrim(@$ipaddr_parts[$i], '0');
-			if ($part == '') $part = '0';
-			echo '<input type="text" name="ipaddr_',$i ,'" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
+			$part = (int)lTrim(@$ipaddr_parts[$i], '0 ');
+			echo '<input type="text" name="ipaddr[',$i,']" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
 			if ($i < 3) echo '.';
 		}
 		echo ' &nbsp; <small>(', '<a target="_blank" href="http://de.wikipedia.org/wiki/IP-Adresse">http://de.wikipedia.org/wiki/IP-Adresse</a>' ,')</small>' ,"\n";
@@ -76,14 +143,26 @@ $can_continue = false;
 	<th><?php echo 'Netzmaske'; ?></th>
 	<td>
 <?php
-		$netmask = gs_keyval_get('vlan_data_netmask');
+		$netmask = gs_keyval_get('vlan_0_netmask');
+		/*
 		$netmask_parts = explode('.', $netmask);
 		for ($i=0; $i<=3; ++$i) {
-			$part = lTrim(@$netmask_parts[$i], '0');
-			if ($part == '') $part = '0';
-			echo '<input type="text" name="netmask_',$i ,'" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
+			$part = (int)lTrim(@$netmask_parts[$i], '0 ');
+			echo '<input type="text" name="netmask[',$i,']" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
 			if ($i < 3) echo '.';
 		}
+		*/
+		echo '<select name="netmask" class="pre">' ,"\n";
+		$options = array(
+			 8 => '255.0.0.0',
+			16 => '255.255.0.0',
+			24 => '255.255.255.0'
+		);
+		foreach ($options as $net => $mask) {
+			$net = '/'.$net;
+			echo '<option value="',$net,'"', ($net===$netmask ? ' selected="selected"' : '') ,'>',$net,' (',$mask,')</option>' ,"\n";
+		}
+		echo '</select>' ,"\n";
 		echo ' &nbsp; <small>(', '<a target="_blank" href="http://de.wikipedia.org/wiki/Netzmaske">http://de.wikipedia.org/wiki/Netzmaske</a>' ,')</small>' ,"\n";
 ?>
 	</td>
@@ -92,12 +171,11 @@ $can_continue = false;
 	<th><?php echo 'Router'; ?></th>
 	<td>
 <?php
-		$router = gs_keyval_get('vlan_data_router');
+		$router = gs_keyval_get('vlan_0_router');
 		$router_parts = explode('.', $router);
 		for ($i=0; $i<=3; ++$i) {
-			$part = lTrim(@$router_parts[$i], '0');
-			if ($part == '') $part = '0';
-			echo '<input type="text" name="router_',$i ,'" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
+			$part = (int)lTrim(@$router_parts[$i], '0 ');
+			echo '<input type="text" name="router[',$i,']" size="3" maxlength="3" class="r pre" value="', $part ,'" />';
 			if ($i < 3) echo '.';
 		}
 		echo ' &nbsp; <small>(', '<a target="_blank" href="http://de.wikipedia.org/wiki/Router">http://de.wikipedia.org/wiki/Router</a>' ,')</small>' ,"\n";
@@ -108,18 +186,17 @@ $can_continue = false;
 	<th><?php echo 'DNS-Server (1)'; ?></th>
 	<td>
 <?php
-		$dns1 = gs_keyval_get('vlan_data_dns1');
+		$dns1 = gs_keyval_get('vlan_0_dns1');
 		if (trim($dns1) != '') {
 			$dns1_parts = explode('.', $dns1);
 			for ($i=0; $i<=3; ++$i) {
-				$dns1_parts[$i] = lTrim(@$dns1_parts[$i], '0 ');
-				if ($dns1_parts[$i] == '') $dns1_parts[$i] = '0';
+				$dns1_parts[$i] = (int)lTrim(@$dns1_parts[$i], '0 ');
 			}
 		} else {
 			$dns1_parts = array('','','','');
 		}
 		for ($i=0; $i<=3; ++$i) {
-			echo '<input type="text" name="dns1_',$i ,'" size="3" maxlength="3" class="r pre" value="', @$dns1_parts[$i] ,'" />';
+			echo '<input type="text" name="dns1[',$i,']" size="3" maxlength="3" class="r pre" value="', @$dns1_parts[$i] ,'" />';
 			if ($i < 3) echo '.';
 		}
 ?>
@@ -129,18 +206,17 @@ $can_continue = false;
 	<th><?php echo 'DNS-Server (2)'; ?></th>
 	<td>
 <?php
-		$dns2 = gs_keyval_get('vlan_data_dns2');
+		$dns2 = gs_keyval_get('vlan_0_dns2');
 		if (trim($dns2) != '') {
 			$dns2_parts = explode('.', $dns2);
 			for ($i=0; $i<=3; ++$i) {
-				$dns2_parts[$i] = lTrim(@$dns2_parts[$i], '0 ');
-				if ($dns2_parts[$i] == '') $dns2_parts[$i] = '0';
+				$dns2_parts[$i] = (int)lTrim(@$dns2_parts[$i], '0 ');
 			}
 		} else {
 			$dns2_parts = array('','','','');
 		}
 		for ($i=0; $i<=3; ++$i) {
-			echo '<input type="text" name="dns2_',$i ,'" size="3" maxlength="3" class="r pre" value="', @$dns2_parts[$i] ,'" />';
+			echo '<input type="text" name="dns2[',$i,']" size="3" maxlength="3" class="r pre" value="', @$dns2_parts[$i] ,'" />';
 			if ($i < 3) echo '.';
 		}
 		echo ' &nbsp; <small>(', 'optional' ,')</small>' ,"\n";
@@ -151,7 +227,7 @@ $can_continue = false;
 	<th><?php echo 'NTP-Server (1)'; ?></th>
 	<td>
 <?php
-		$ntp1_addr = gs_keyval_get('vlan_data_ntp1');
+		$ntp1_addr = gs_keyval_get('vlan_0_ntp1');
 		echo '<input type="text" name="ntp1" size="30" maxlength="50" class="pre" value="', $ntp1_addr ,'" />' ,"\n";
 ?>
 	</td>
@@ -160,7 +236,7 @@ $can_continue = false;
 	<th><?php echo 'NTP-Server (2)'; ?></th>
 	<td>
 <?php
-		$ntp2_addr = '1.de.pool.ntp.org';
+		$ntp2_addr = gs_keyval_get('vlan_0_ntp2');
 		echo '<input type="text" name="ntp2" size="30" maxlength="50" class="pre" value="', $ntp2_addr ,'" />' ,"\n";
 		echo ' &nbsp; <small>(', 'optional' ,')</small>' ,"\n";
 ?>
@@ -170,7 +246,7 @@ $can_continue = false;
 	<th><?php echo 'NTP-Server (3)'; ?></th>
 	<td>
 <?php
-		$ntp3_addr = '2.de.pool.ntp.org';
+		$ntp3_addr = gs_keyval_get('vlan_0_ntp3');
 		echo '<input type="text" name="ntp3" size="30" maxlength="50" class="pre" value="', $ntp3_addr ,'" />' ,"\n";
 		echo ' &nbsp; <small>(', 'optional' ,')</small>' ,"\n";
 ?>
@@ -180,7 +256,7 @@ $can_continue = false;
 	<th><?php echo 'NTP-Server (4)'; ?></th>
 	<td>
 <?php
-		$ntp4_addr = '3.de.pool.ntp.org';
+		$ntp4_addr = gs_keyval_get('vlan_0_ntp4');
 		echo '<input type="text" name="ntp4" size="30" maxlength="50" class="pre" value="', $ntp4_addr ,'" />' ,"\n";
 		echo ' &nbsp; <small>(', 'optional' ,')</small>' ,"\n";
 ?>
