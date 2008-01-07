a1fbfc021b8fe1a261b2022ab2e7ca6805250d43
philipp@amooma.com <philipp@amooma.com@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-01-07 07:52:12 +0000

    für GS_INSTALLATION_TYPE_SINGLE die besondere Host-IP-Adresse "255.255.255.255" und Host-ID -1
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3063 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/get-listen-to-ids.php
+++ opt/gemeinschaft/inc/get-listen-to-ids.php
@@ -36,6 +36,11 @@ require_once( GS_DIR .'inc/log.php' );
 
 function gs_get_listen_to_ids( $primary_only=false )
 {
+	if (gs_get_conf('GS_INSTALLATION_TYPE_SINGLE')) {
+		# return special host ID
+		return array(-1);
+	}
+	
 	# get our IPs
 	#
 	$ips = @ gs_get_listen_to_ips( $primary_only );
