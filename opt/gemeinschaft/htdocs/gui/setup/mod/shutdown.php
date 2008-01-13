df3f8ca08c9de4b875f8cab8e2f6434b470fd0bb
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-01-13 02:19:38 +0000

    shutdown
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3094 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/htdocs/gui/setup/mod/shutdown.php
@@ -0,0 +1,54 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+defined('GS_VALID') or die('No direct access.');
+
+?>
+
+<br />
+<br />
+<br />
+<br />
+
+<div style="width:550px; border:1px solid #ccc; margin: 2em auto; padding:0 1em 1em 1em; background-color:#eee;">
+
+<h1><?php echo 'Ausschalten'; ?></h1>
+<p align="center">
+	<?php echo 'Das System wird jetzt heruntergefahren.'; ?>
+</p>
+<p align="center">
+	<img alt=" " src="<?php echo GS_URL_PATH; ?>img/wait-net.gif" />
+</p>
+<br />
+
+</div>
+
+<?php
+
+@exec( 'sudo sh -c \'sleep 2; /opt/gemeinschaft/sbin/gpbx-pre-shutdown 1>>/dev/null 2>>/dev/null; /sbin/shutdown -h -P now 1>>/dev/null 2>>/dev/null &\' 0<&- 1>&- 2>&- &' );
+
+?>
