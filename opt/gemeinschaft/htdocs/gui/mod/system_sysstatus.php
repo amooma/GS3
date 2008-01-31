f8abe64416dd143ded806a8d0724a25ca34843ca
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-01-31 11:40:10 +0000

    Formatierung
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3267 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/system_sysstatus.php
+++ opt/gemeinschaft/htdocs/gui/mod/system_sysstatus.php
@@ -155,16 +155,17 @@ if (file_exists('/proc/meminfo')) {
 	//echo 'Swap', "\n";
 	if (preg_match('/^SwapTotal:\s*([0-9]+)/mi', $out, $m)) {
 		$memtotal = (int)$m[1];
-    if($memtotal) //avoid division by 0
-		//echo '  Total: ', str_pad(ceil($memtotal/1024),4,' ',STR_PAD_LEFT) ,' MiB', "\n";
-		if (preg_match('/^SwapFree:\s*([0-9]+)/mi', $out, $m)) {
-			$memfree = (int)$m[1];
-			$memused = $memtotal - $memfree;
-			//echo '  Used : ', str_pad(ceil($memused/1024),4,' ',STR_PAD_LEFT) ,' MiB', "\n";
-			echo 'Swap: ',
-				'<b>', str_pad(ceil($memtotal/1024),4,' ',STR_PAD_LEFT) ,'</b> MiB, ',
-				'<b>', str_pad(ceil($memused/$memtotal*100),2,' ',STR_PAD_LEFT) ,' %</b> used',
-				"\n";
+		if ($memtotal > 0) {  # avoid division by 0
+			//echo '  Total: ', str_pad(ceil($memtotal/1024),4,' ',STR_PAD_LEFT) ,' MiB', "\n";
+			if (preg_match('/^SwapFree:\s*([0-9]+)/mi', $out, $m)) {
+				$memfree = (int)$m[1];
+				$memused = $memtotal - $memfree;
+				//echo '  Used : ', str_pad(ceil($memused/1024),4,' ',STR_PAD_LEFT) ,' MiB', "\n";
+				echo 'Swap: ',
+					'<b>', str_pad(ceil($memtotal/1024),4,' ',STR_PAD_LEFT) ,'</b> MiB, ',
+					'<b>', str_pad(ceil($memused/$memtotal*100),2,' ',STR_PAD_LEFT) ,' %</b> used',
+					"\n";
+			}
 		}
 	}
 	
