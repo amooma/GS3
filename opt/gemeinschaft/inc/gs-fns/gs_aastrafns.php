ec0f66b0cdaef87168d8261e17090de61da41b3e
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-02-04 19:03:11 +0000

    aastra_push_statusline() für $type != "alert" gefixt und $beep hinzugefügt
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3347 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/gs-fns/gs_aastrafns.php
+++ opt/gemeinschaft/inc/gs-fns/gs_aastrafns.php
@@ -104,12 +104,11 @@ function aastra_textscreen( $title, $text )
 	aastra_write('</AastraIPPhoneTextScreen>');
 }
 
-function aastra_push_statusline( $phone_ip, $text, $index=0, $type='', $timeout=3 )
+function aastra_push_statusline( $phone_ip, $text, $index=0, $type='', $timeout=3, $beep=false )
 {
-	aastra_write('<AastraIPPhoneStatus>');
+	aastra_write('<AastraIPPhoneStatus Beep="'. ($beep?'yes':'no') .'">');
 	aastra_write('<Session>gemeinschaft</Session>');
-	if ($type == 'alert') $type='type="alert"';
-	aastra_write('<Message index="'.$index.'" '.$type.' timeout="'.$timeout.'">'.$text.'</Message>');
+	aastra_write('<Message index="'.$index .'"'. ($type != '' ? ' type="'.$type.'"' : '') .' timeout="'.$timeout .'">'. $text .'</Message>');
 	aastra_write('</AastraIPPhoneStatus>');
 	
 	$ret_val = aastra_push($phone_ip);
