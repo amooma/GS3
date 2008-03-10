191c42e148ba73c44f87196dc4a5d92f547ca5a0
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-03-10 20:24:28 +0000

    s/Ausschalten/Trennen der Stromversorgung/
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3462 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/mod/reboot.php
+++ opt/gemeinschaft/htdocs/gui/setup/mod/reboot.php
@@ -39,7 +39,7 @@ defined('GS_VALID') or die('No direct access.');
 <h1><?php echo 'Neustart'; ?></h1>
 <p align="center">
 	<?php echo 'Das System wird jetzt neugestartet.'; ?><br />
-	<?php echo 'Bitte haben Sie etwas Geduld. Vorzeitiges Ausschalten kann zu Datenverlust f&uuml;hren.'; ?>
+	<?php echo 'Bitte haben Sie etwas Geduld. Trennen der Stromversorgung / Reset kann zu Datenverlust f&uuml;hren.'; ?>
 </p>
 <p align="center">
 	<img alt=" " src="<?php echo GS_URL_PATH; ?>img/wait-net.gif" />
