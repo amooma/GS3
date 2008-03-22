da39d1e04014b797f878f5dff807be3e39c26826
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-03-22 17:23:11 +0000

    if lt IE 7
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3541 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/index.php
+++ opt/gemeinschaft/htdocs/gui/index.php
@@ -192,7 +192,7 @@ function gs_form_hidden( $sect='', $mod='', $sudo_user=null )
 <meta http-equiv="cache-control" content="no-cache" />
 <!-- for stupid MSIE: -->
 <!--[if IE]><link rel="stylesheet" type="text/css" href="<?php echo GS_URL_PATH; ?>styles/msie-fix.css" /><![endif]-->
-<!--[if lte IE 6]><style type="text/css">img {behavior: url("<?php echo GS_URL_PATH; ?>js/pngbehavior.htc.php?msie-sucks=.htc");}</style><![endif]-->
+<!--[if lt IE 7]><style type="text/css">img {behavior: url("<?php echo GS_URL_PATH; ?>js/pngbehavior.htc.php?msie-sucks=.htc");}</style><![endif]-->
 </head>
 <body>
 
