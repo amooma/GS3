35adfd99e6ed4636ab535f10595cc51b93699d95
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-03-25 00:28:33 +0000

    2007-2008
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3561 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/monitor.php
+++ opt/gemeinschaft/htdocs/gui/monitor.php
@@ -101,7 +101,7 @@ function get_ext_ranges( $ext_ranges )
 <html xmlns="http://www.w3.org/1999/xhtml" lang="de-DE" xml:lang="de-DE">
 <head><!--<![CDATA[
                 Gemeinschaft
-  @(_)=====(_)  (c) 2007, amooma GmbH - http://www.amooma.de/
+  @(_)=====(_)  (c) 2007-2008, amooma GmbH - http://www.amooma.de/
  @   / ### \    Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
  @  |  ###  |   Philipp Kempgen <philipp.kempgen@amooma.de>
   @@|_______|   Peter Kozak <peter.kozak@amooma.de>
