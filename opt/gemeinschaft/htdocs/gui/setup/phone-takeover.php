d420ef67cc0d08460d5da185ddf00e505b67f507
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-03-30 01:57:38 +0000

    Konfiguration beachten
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3625 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/phone-takeover.php
+++ opt/gemeinschaft/htdocs/gui/setup/phone-takeover.php
@@ -257,8 +257,14 @@ elseif ($action === 'takeover') {
 				exit(1);
 			}
 			
+			/*
 			$myipaddr = trim(gs_keyval_get('vlan_0_ipaddr'));
-			$ret = _snom_setting( $ip, $https_open, $valid_user, $valid_pass, 'setting_server', 'http://'.$myipaddr.':80/gemeinschaft/prov/snom/' );
+			$ret = _snom_setting( $ip, $https_open, $valid_user, $valid_pass, 'setting_server', 'http://'.$myipaddr.':80/gemeinschaft/prov/snom/settings.php?mac={mac}' );
+			*/
+			$GS_PROV_PORT = (int)gs_get_conf('GS_PROV_PORT');
+			$setting_server_url = gs_get_conf('GS_PROV_SCHEME') .'://'. gs_get_conf('GS_PROV_HOST') . ($GS_PROV_PORT !== 80 ? ':'.$GS_PROV_PORT : '') . gs_get_conf('GS_PROV_PATH') .'snom/settings.php?mac={mac}';
+			$ret = _snom_setting( $ip, $https_open, $valid_user, $valid_pass, 'setting_server', $setting_server_url );
+			
 			if ($ret['http_code'] !== 200) {
 				echo 'Fehler!' ,"\n";
 				exit(1);
