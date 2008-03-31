a0c07cc00bc573d6df8e6ad7fe97c236fe479ead
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-03-31 06:32:06 +0000

    /usr/sbin/
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3660 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/setup/mod/shutdown.php
+++ opt/gemeinschaft/htdocs/gui/setup/mod/shutdown.php
@@ -52,7 +52,7 @@ defined('GS_VALID') or die('No direct access.');
 
 if (@file_exists('/usr/sbin/gs-pre-shutdown')) {
 	$err=0; $out=array();
-	@exec( 'sudo gs-pre-shutdown 2>>/dev/null', $out, $err );
+	@exec( 'sudo /usr/sbin/gs-pre-shutdown 2>>/dev/null', $out, $err );
 }
 
 //@exec( 'sudo sh -c \'sleep 2; /opt/gemeinschaft/sbin/gpbx-pre-shutdown 1>>/dev/null 2>>/dev/null; /sbin/shutdown -h -P now 1>>/dev/null 2>>/dev/null &\' 0<&- 1>&- 2>&- &' );
