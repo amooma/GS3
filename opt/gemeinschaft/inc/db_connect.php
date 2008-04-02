fc667b4a075b3262f7fa0fb46c854a7f30948a48
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-04-02 22:28:45 +0000

    DB-Verbindung innerhalb einer Instanz des PHP-Interpreters wiederverwenden
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@3694 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/db_connect.php
+++ opt/gemeinschaft/inc/db_connect.php
@@ -32,9 +32,37 @@ include_once( GS_DIR .'lib/yadb/yadb.php' );
 include_once( GS_DIR .'inc/log.php' );
 
 
-function gs_db_master_connect()
+$gs_db_conn_master = null;
+$gs_db_conn_slave  = null;
+
+
+function & gs_db_master_connect()
 {
-	if (!( $db = YADB_newConnection( 'mysql' ) )) return null;
+	global $gs_db_conn_master;
+	
+	if (GS_LOG_LEVEL >= GS_LOG_DEBUG) {
+		$bt = debug_backtrace();
+		if (array_key_exists(0, $bt)) {
+			$caller_info = @$bt[0]['file'] .':'. @$bt[0]['line'];
+			unset($bt);
+		} else {
+			$caller_info = 'unknown';
+		}
+	}
+	
+	if (getType($gs_db_conn_master) === 'object'
+	&&  method_exists($gs_db_conn_master, 'isConnected')
+	&&  $gs_db_conn_master->isConnected())
+	{
+		gs_log( GS_LOG_DEBUG, 'Using the existing master DB connection (for '. $caller_info .')');
+		return $gs_db_conn_master;
+	}
+	gs_log( GS_LOG_DEBUG, 'Opening a new master DB connection (for '. $caller_info .')');
+	
+	if (!( $db = YADB_newConnection( 'mysql' ) )) {
+		$null = null;
+		return $null;
+	}
 	if (!( $db->connect(
 		GS_DB_MASTER_HOST,
 		GS_DB_MASTER_USER,
@@ -44,15 +72,43 @@ function gs_db_master_connect()
 		)))
 	{
 		gs_log( GS_LOG_WARNING, 'Could not connect to master database!' );
-		return null;
+		$null = null;
+		return $null;
 	}
 	@ $db->setCharSet( 'utf8', 'utf8_unicode_ci' );
-	return $db;
+	
+	$gs_db_conn_master = $db;
+	return $gs_db_conn_master;
 }
 
-function gs_db_slave_connect()
+
+function & gs_db_slave_connect()
 {
-	if (!( $db = YADB_newConnection( 'mysql' ) )) return null;
+	global $gs_db_conn_slave;
+	
+	if (GS_LOG_LEVEL >= GS_LOG_DEBUG) {
+		$bt = debug_backtrace();
+		if (array_key_exists(0, $bt)) {
+			$caller_info = @$bt[0]['file'] .':'. @$bt[0]['line'];
+			unset($bt);
+		} else {
+			$caller_info = 'unknown';
+		}
+	}
+	
+	if (getType($gs_db_conn_slave) === 'object'
+	&&  method_exists($gs_db_conn_slave, 'isConnected')
+	&&  $gs_db_conn_slave->isConnected())
+	{
+		gs_log( GS_LOG_DEBUG, 'Using the existing slave DB connection (for '. $caller_info .')');
+		return $gs_db_conn_slave;
+	}
+	gs_log( GS_LOG_DEBUG, 'Opening a new slave DB connection (for '. $caller_info .')');
+	
+	if (!( $db = YADB_newConnection( 'mysql' ) )) {
+		$null = null;
+		return $null;
+	}
 	if (!( $db->connect(
 		GS_DB_SLAVE_HOST,
 		GS_DB_SLAVE_USER,
@@ -62,10 +118,13 @@ function gs_db_slave_connect()
 		)))
 	{
 		gs_log( GS_LOG_WARNING, 'Could not connect to slave database!' );
-		return null;
+		$null = null;
+		return $null;
 	}
 	@ $db->setCharSet( 'utf8', 'utf8_unicode_ci' );
-	return $db;
+	
+	$gs_db_conn_slave = $db;
+	return $gs_db_conn_slave;
 }
 
 
