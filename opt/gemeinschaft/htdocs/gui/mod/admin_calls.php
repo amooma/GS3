07c22c57faa4f409dc274e7997d20cdd3d4d0e63
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-05-09 21:54:49 +0000

    cleanup
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@4064 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/admin_calls.php
+++ opt/gemeinschaft/htdocs/gui/mod/admin_calls.php
@@ -129,12 +129,13 @@ function query_string( $period, $src, $dst, $dur, $stat )
 
 $per_page = (int)GS_GUI_NUM_RESULTS;
 
-$src    = trim(@$_REQUEST['src'   ]);
-$dst    = trim(@$_REQUEST['dst'   ]);
-$dur    = trim(@$_REQUEST['dur'   ]);
-$stat   = trim(@$_REQUEST['stat'  ]);
-$period = trim(@$_REQUEST['period']);
-$page   = (int)@$_REQUEST['page'  ] ;
+$src    =  trim(@$_REQUEST['src'   ]);
+$dst    =  trim(@$_REQUEST['dst'   ]);
+$dur    =  trim(@$_REQUEST['dur'   ]);
+$stat   =  trim(@$_REQUEST['stat'  ]);
+$period =  trim(@$_REQUEST['period']);
+$page   = (int)(@$_REQUEST['page'  ]);
+$stat = preg_replace('/[^a-z \-_]/', '', $stat);
 
 
 function _sanitize_dur( $dur )
@@ -168,7 +169,7 @@ function _sanitize_dur( $dur )
 
 
 $dur = _sanitize_dur( $dur );
-$query_string = query_string($period, $src, $dst, $dur, $stat);
+$query_string = query_string( $period, $src, $dst, $dur, $stat );
 
 /*
 echo 'SELECT SQL_CALC_FOUND_ROWS
@@ -216,8 +217,9 @@ $mod_url = gs_url($SECTION, $MODULE, null,
 	<th style="width:140px;"><?php echo __('Ziel'); ?></th>
 	<th style="width:100px;"><?php echo __('Anrufdauer'); ?></th>
 	<th style="width:140px;" class="r">
-		<?php echo __('Seite'), ' ', ($page+1), ' / ', $num_pages; ?></th>
-	<th style="width:50px;">
+		<?php echo __('Seite'), ' ', ($page+1), ' / ', $num_pages; ?>
+	</th>
+	<th style="width: 60px;">
 <?php
 
 if ($page > 0) {
@@ -248,15 +250,15 @@ if ($page < $num_pages-1) {
 <tr>
 	<td>
 		<select name="period" onchange="this.form.submit();"> 
-		<option value ="all" <?php echo ($period=='all') ? 'selected="selected">' : '>'; ?><?php echo __('alle'); ?></option>
-		<option value ="month" <?php echo ($period=='month') ? 'selected="selected">' : '>'; ?><?php echo __('ein Monat'); ?></option>
-		<option value ="tmonth" <?php echo ($period=='tmonth') ? 'selected="selected">' : '>'; ?><?php echo __('diesen Monat'); ?></option>
-		<option value ="" <?php echo ($period=='') ? 'selected="selected">' : '>'; ?><?php echo __('eine Woche'); ?></option>
-		<option value ="tweek" <?php echo ($period=='tweek') ? 'selected="selected">' : '>'; ?><?php echo __('diese Woche'); ?></option>
-		<option value ="day" <?php echo ($period=='day') ? 'selected="selected">' : '>'; ?><?php echo __('ein Tag'); ?></option>
-		<option value ="today" <?php echo ($period=='today') ? 'selected="selected">' : '>'; ?><?php echo __('heute'); ?></option>
-		<option value ="hour" <?php echo ($period=='hour') ? 'selected="selected">' : '>'; ?><?php echo __('eine Stunde'); ?></option>
-		<option value ="qhour" <?php echo ($period=='qhour') ? 'selected="selected">' : '>'; ?><?php echo __('15 Minuten'); ?></option>
+		<option value ="all"   <?php if ($period==='all'   ) echo ' selected="selected"'; ?>><?php echo __('alle'        ); ?></option>
+		<option value ="month" <?php if ($period==='month' ) echo ' selected="selected"'; ?>><?php echo __('ein Monat'   ); ?></option>
+		<option value ="tmonth"<?php if ($period==='tmonth') echo ' selected="selected"'; ?>><?php echo __('diesen Monat'); ?></option>
+		<option value =""      <?php if ($period===''      ) echo ' selected="selected"'; ?>><?php echo __('eine Woche'  ); ?></option>
+		<option value ="tweek" <?php if ($period==='tweek' ) echo ' selected="selected"'; ?>><?php echo __('diese Woche' ); ?></option>
+		<option value ="day"   <?php if ($period==='day'   ) echo ' selected="selected"'; ?>><?php echo __('ein Tag'     ); ?></option>
+		<option value ="today" <?php if ($period==='today' ) echo ' selected="selected"'; ?>><?php echo __('heute'       ); ?></option>
+		<option value ="hour"  <?php if ($period==='hour'  ) echo ' selected="selected"'; ?>><?php echo __('eine Stunde' ); ?></option>
+		<option value ="qhour" <?php if ($period==='qhour' ) echo ' selected="selected"'; ?>><?php echo __('15 Minuten'  ); ?></option>
 		</select>
 	</td>
 	<td>
@@ -265,23 +267,23 @@ if ($page < $num_pages-1) {
 	<td>
 		<input type="text" name="dst" value="<?php echo htmlEnt($dst); ?>" size="15" style="width:130px;" />
 	</td>
-	<td>
+	<td class="nobr">
 		<input type="text" name="dur" value="<?php echo htmlEnt($dur); ?>" size="8" style="width:60px;" />&nbsp;s
 	</td>
 	<td>
 		<select name="stat" style="width:135px;" onchange="this.form.submit();">
-		<option value="" <?php echo ($stat=='') ? 'selected="selected">' : '>'; ?><?php echo __('alle Verb.'); ?></option>
-		<option value="answered" <?php echo ($stat=='answered') ? 'selected="selected">' : '>'; ?><?php echo __('beantwortet'); ?></option>
-		<option value="no answer" <?php echo ($stat=='no answer') ? 'selected="selected">' : '>'; ?><?php echo __('keine Antwort'); ?></option>
-		<option value="busy" <?php echo ($stat=='busy') ? 'selected="selected">' : '>'; ?><?php echo __('besetzt'); ?></option>
-		<option value="failed" <?php echo ($stat=='failed') ? 'selected="selected">' : '>'; ?><?php echo __('fehlgeschlagen'); ?></option>
+		<option value=""         <?php if ($stat===''         ) echo ' selected="selected"'; ?>><?php echo __('alle Verb.'    ); ?></option>
+		<option value="answered" <?php if ($stat==='answered' ) echo ' selected="selected"'; ?>><?php echo __('angenommen'    ); /*//TRANSLATEME*/ ?></option>
+		<option value="no answer"<?php if ($stat==='no answer') echo ' selected="selected"'; ?>><?php echo __('keine Antwort' ); ?></option>
+		<option value="busy"     <?php if ($stat==='busy'     ) echo ' selected="selected"'; ?>><?php echo __('besetzt'       ); ?></option>
+		<option value="failed"   <?php if ($stat==='failed'   ) echo ' selected="selected"'; ?>><?php echo __('fehlgeschlagen'); ?></option>
 		</select>
 	</td>
 	<td>
-		<button type="submit" title="<?php echo __('Suchen'); ?>" class="plain">
+		<button class="plain" type="submit" title="<?php echo __('Suchen'); ?>">
 			<img alt="<?php echo __('Suchen'); ?>" src="<?php echo GS_URL_PATH; ?>crystal-svg/16/act/search.png" />
 		</button>
-		<?php echo '<a href="', gs_url($SECTION, $MODULE, null, 'page='.$page) ,'"><img alt="', __('Abbrechen'), '" src="', GS_URL_PATH,'crystal-svg/16/act/cancel.png" /></a>'; ?>
+		<?php /*echo '<a href="', gs_url($SECTION, $MODULE, null, 'page='.$page) ,'"><img alt="', __('Abbrechen'), '" src="', GS_URL_PATH,'crystal-svg/16/act/cancel.png" /></a>';*/ ?>
 	</td>
 </tr>
 
@@ -320,16 +322,16 @@ if (@$rs) {
 		
 		switch ($r['disposition']) {
 		case 'ANSWERED':
-			echo '<img src="', GS_URL_PATH, 'crystal-svg/16/act/greenled.png" /> ', __('beantwortet');			
+			echo '<img alt=" " src="', GS_URL_PATH, 'crystal-svg/16/act/greenled.png" /> ', __('angenommen') /*//TRANSLATEME*/;			
 			break;
 		case 'NO ANSWER':
-			echo '<img src="', GS_URL_PATH, 'crystal-svg/16/act/free_icon.png" /> ', __('keine Antwort');			
+			echo '<img alt=" " src="', GS_URL_PATH, 'crystal-svg/16/act/free_icon.png" /> ', __('keine Antwort');			
 			break;
 		case 'FAILED':
-			echo '<img src="', GS_URL_PATH, 'crystal-svg/16/act/redled.png" /> ', __('fehlgeschlagen');			
+			echo '<img alt=" " src="', GS_URL_PATH, 'crystal-svg/16/act/redled.png" /> ', __('fehlgeschlagen');			
 			break;
 		case 'BUSY':
-			echo '<img src="', GS_URL_PATH, 'crystal-svg/16/act/yellowled.png" /> ', __('besetzt');			
+			echo '<img alt=" " src="', GS_URL_PATH, 'crystal-svg/16/act/yellowled.png" /> ', __('besetzt');			
 			break;
 		default:
 			echo __('unbekannt');
@@ -350,46 +352,42 @@ if (@$rs) {
 <table cellspacing="1" class="phonebook">
 <thead>
 <tr>
-	<th style="width:260px;">
+	<th colspan="2">
 		<span class="sort-col"><?php echo __('Verbindungsstatistik'); ?></span>
-	</th>
-	<th style="width:140px;">
-		&nbsp;
 	</th>
 </tr>
 </thead>
 <tbody>
 <tr>
 	<th><?php echo __('Anrufe'); ?>:</th>
-	<td><?php echo $num_total; ?></td>
+	<td class="r" style="min-width:6em;"><?php echo $num_total; ?></td>
 </tr>
 <tr>
-	<th><?php echo __('Anrufdauer insges.'); ?>:</th>
-	<td><?php echo sec_to_hours( $sum_talktime ); ?></td>
+	<th><?php echo __('Anrufdauer insgesamt'); /*//TRANSLATEME*/ ?>:</th>
+	<td class="r"><?php echo sec_to_hours( $sum_talktime ); ?></td>
 </tr>
 <tr>
 	<th><?php echo __('Anrufdauer im Durchschnitt'); ?>:</th>
-	<td><?php
+	<td class="r"><?php
+		
+		$where = trim( $query_string );
+		if (strToUpper(subStr($where,0,5)) == 'WHERE')
+			$where = $where.' AND `billsec` > 0';
+		else
+			$where = 'WHERE `billsec` > 0';
+		
+		$num_total_with_talktime = $DB->executeGetOne( 'SELECT COUNT(*) FROM `ast_cdr` '. $where );
+		
+		if ($num_total_with_talktime > 0)
+			echo sec_to_hours( $sum_talktime / $num_total_with_talktime );
+		//else echo '<i>', __('keine Anrufe'), '</i>';
+		else echo '<i>', sec_to_hours( 0 ), '</i>';
 
-$where = trim( $query_string );
-if (strToUpper(subStr($where,0,5)) == 'WHERE')
-	$where = $where.' AND `billsec` > 0';
-else
-	$where = 'WHERE `billsec` > 0';
-
-$num_total_with_talktime = $DB->executeGetOne( 'SELECT COUNT(*) FROM `ast_cdr` '. $where );
-
-if ($num_total_with_talktime > 0)
-	echo sec_to_hours( $sum_talktime / $num_total_with_talktime );
-//else echo '<i>', __('keine Anrufe'), '</i>';
-else echo '<i>', sec_to_hours( 0 ), '</i>';
-
-?>
-	</td>
+	?></td>
 </tr>
 <tr>
 	<th><?php echo __('Verbindungszeit'); ?>:</th>
-	<td><?php echo sec_to_hours( $sum_calltime ); ?></td>
+	<td class="r"><?php echo sec_to_hours( $sum_calltime ); ?></td>
 </tr>
 </tbody>
 </table>
