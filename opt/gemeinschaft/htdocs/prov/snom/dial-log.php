9f43b9ff3afe93e77aa992f490d77f1030693280
henning@loca.net <henning@loca.net@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-05-13 17:31:14 +0000

    Ausgabe von Uhrzeit oder Datum in Anruflisten
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@4151 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/prov/snom/dial-log.php
+++ opt/gemeinschaft/htdocs/prov/snom/dial-log.php
@@ -172,10 +172,11 @@ LIMIT 20';
 		if ($r['remote_name'] != '') {
 			$entry_name .= ' '. $r['remote_name'];
 		}
-		if ($type=='missed') {
+		if (date('dm') == date('dm', (int)$r['ts']))
 			$when = date('H:i', (int)$r['ts']);
-			$entry_name = $when .'  '. $entry_name;
-		}
+		else
+			$when = date('d.m.', (int)$r['ts']);
+		$entry_name = $when .'  '. $entry_name;
 		if ($r['num_calls'] > 1) {
 			$entry_name .= ' ('. $r['num_calls'] .')';
 		}
@@ -189,7 +190,7 @@ LIMIT 20';
 	
 	echo "\n",
 	     '</SnomIPPhoneDirectory>';
-	
+
 }
 #################################### DIAL LOG }
 
