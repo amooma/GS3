5837e14ea35861c048a2c9d5ff9470b76337fe3b
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-08-30 19:26:59 +0000

    svn-diff-incremental.sh
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@4866 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ svn-diff-incremental.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Philipp Kempgen
+
+if [ -z $1 ]; then
+	echo "Arg. 1 must be the file/folder or URL"
+	exit 1
+fi
+ARG_FILE=$1
+
+if [ -z $2 ]; then
+	echo "Arg. 2 must be the revision"
+	exit 1
+fi
+ARG_REV=$2
+
+if [ ! -z $3 ]; then
+	ARG_REV_FROM=$ARG_REV
+	ARG_REV_TO=$3
+else
+	ARG_REV_FROM=$ARG_REV
+	ARG_REV_TO=$ARG_REV
+fi
+
+
+for (( REV_TO = $ARG_REV_FROM; $REV_TO <= $ARG_REV_TO; REV_TO++ ))
+do
+	echo ""
+	REV_FROM=$(( $REV_TO - 1 ))
+	svn log -r "${REV_TO}" "${ARG_FILE}"
+	svn diff --notice-ancestry -r "${REV_FROM}:${REV_TO}" "${ARG_FILE}"
+	
+	echo ""
+	echo ""
+	echo ""
+done
+
