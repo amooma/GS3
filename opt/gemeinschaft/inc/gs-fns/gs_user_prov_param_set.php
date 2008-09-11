df45929ffc77417ebec2a05c0a55e025d04ad747
soeren.sprenger@amooma.de <soeren.sprenger@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-09-11 11:35:06 +0000

    added function for changing user-specific provisining-parameters
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@4982 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/inc/gs-fns/gs_user_prov_param_set.php
@@ -0,0 +1,99 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+defined('GS_VALID') or die('No direct access.');
+
+include_once( GS_DIR .'inc/gs-lib.php' );
+include_once( GS_DIR .'inc/db_connect.php' );
+
+/***********************************************************
+*    set an provisioning parameter for the user
+***********************************************************/
+
+function gs_user_prov_param_set( $username, $index ,$phone_type , $param, $value )
+{
+	if (! preg_match( '/^[a-zA-Z\d\-]+$/', $username ))
+		return new GsError( 'User must be alphanumeric.' );
+
+	# connect to db
+	#
+	$db = gs_db_master_connect();
+	if (! $db)
+		return new GsError( 'Could not connect to database.' );
+	
+	# get user_id
+	#
+	$res = $db->execute("SELECT * from users where user = ".$db->escape($username));
+
+	if (! $res)
+		return new GsError( 'Error.' );
+	
+	$user = $res->fetchRow();
+	if (! $user)
+		return new GsError( 'Error.' );
+
+	print_r($user);
+
+	if ($user['id'] < 1)
+		return new GsError( 'Unknown user.' );
+
+	$id = 0;
+
+	# does an provisioning parameter profile already exists for this user?
+	if(!$user['prov_profile_id'] )	{
+		# no -> create a new one
+		echo "profile_id does not exist. creating a new one\n";
+		$ok = $db->execute( 'INSERT INTO `prov_param_profiles` (`is_group_profile`, `title`) VALUES (0 , "u-' . $db->escape($username) .'")' );
+		if (! $ok)
+			return new GsError( 'Failed to add a new prov_param_profile' );
+
+		$id = $db->executeGetOne('SELECT `id` FROM `prov_param_profiles` WHERE title="u-'.$db->escape($username).'"');
+
+		if(!$id)
+			return new GsError( 'Error');
+		#update user
+		$ok = $db->execute( 'UPDATE `users` SET `prov_profile_id`='. $id .' WHERE `id`='. $user['id'] );
+		if (! $ok)
+			return new GsError( 'Failed to assing the new prov_param_profile to the user' );
+
+		}
+	else	{
+		$id = $user['prov_profile_id'];
+	}
+
+	#add the parameter to the Profile
+	echo "add parameter";
+	$ok = $db->execute( 'REPLACE INTO `prov_params` (`profile_id`, `phone_type` , `param` , `index` , `value`) VALUES ("'.$id.'", "siemens-os60", "'.$db->escape($param).'", "-1", "'.$db->escape($value).'")' );
+	if (! $ok)
+		return new GsError( 'Failed to add the Parameter to the Profile' );
+
+	return true;
+}
+
+
+?>
