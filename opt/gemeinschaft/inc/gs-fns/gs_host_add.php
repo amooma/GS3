e0f0c43146749c5c961ea13aa1ea724dc2110b45
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-09-23 19:16:08 +0000

    small fix
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5111 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/gs-fns/gs_host_add.php
+++ opt/gemeinschaft/inc/gs-fns/gs_host_add.php
@@ -35,28 +35,39 @@ include_once( GS_DIR .'inc/gs-lib.php' );
 *    adds a new host
 ***********************************************************/
 
-function gs_host_add( $host_ip_or_name, $comment,  $sip_proxy_from_wan=null, $sip_sbc_from_wan=null )
+function gs_host_add( $host_ip_or_name, $comment, $sip_proxy_from_wan=null, $sip_sbc_from_wan=null )
 {
-	if ($host_ip_or_name) {
-		$host = normalizeIPs($host_ip_or_name);
-		$bInvalHostName = false;
-		if (! preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $host)) {
-			# not an IP address. => resolve hostname
-			$addresses = @gethostbynamel($host);
-			
-			if (count($addresses) < 1) 
-				return new GsError('Could not resolve hostname: ' .$host);
-			elseif (count($addresses) > 1)
-				return new GsError('Hostname '. $host . ' cannot be used because its resolved to more than one IP.');
-			elseif (count($addresses) == 1) {
-				if (strlen($addresses[0]) == 0) 
-					return new GsError('Could not resolve hostname: ' .$host);
-				$host = $addresses[0];
-			}
+	if (! $host_ip_or_name)
+		return new GsError('Invalid host.');
+	
+	$host = normalizeIPs($host_ip_or_name);
+	$bInvalHostName = false;
+	if (! preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $host)) {
+		# not an IP address. => resolve hostname
+		$addresses = @gethostbynamel($host);
+		
+		if (count($addresses) < 1) {
+			return new GsError('Could not resolve hostname: '. $host);
+		} elseif (count($addresses) > 1) {
+			return new GsError('Hostname '. $host .' cannot be used because it\'s resolved to more than one IP addr.');
+		} elseif (count($addresses) == 1) {
+			if (strlen($addresses[0]) == 0)
+				return new GsError('Could not resolve hostname: '. $host);
+			$host = $addresses[0];
 		}
-		else 
+	}
+	else {
 		$host = $host_ip_or_name;
 	}
+	
+	if (! preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $sip_proxy_from_wan)) {
+		return new GsError( 'Invalid IP address of SIP proxy from WAN.' );
+	}
+	if (! preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $sip_sbc_from_wan)) {
+		return new GsError( 'Invalid IP address of SIP SBC from WAN.' );
+	}
+	
+	$api = gs_get_conf('GS_BOI_API_DEFAULT');
 	
 	# connect to db
 	#
@@ -66,7 +77,7 @@ function gs_host_add( $host_ip_or_name, $comment,  $sip_proxy_from_wan=null, $si
 	
 	$db->execute('OPTIMIZE TABLE `hosts`');  # recalculate next auto-increment value
 	$db->execute('ANALYZE TABLE `hosts`');
-
+	
 	$sql_query =
 	'INSERT INTO `hosts` (
 		`id`,
@@ -80,17 +91,16 @@ function gs_host_add( $host_ip_or_name, $comment,  $sip_proxy_from_wan=null, $si
 		0
 	)';
 	$ok = $db->execute($sql_query);
-	if ($ok) 
-		$host_id = (int)$db->getLastInsertId();
-	else 
-		return new GsError( 'Fehler beim Hinzufuegen von Host: ' .$host);
-			
-	if ($host_id > 0) {
-		$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'api\', \''. $db->escape(trim(@$_REQUEST['hp_api'])) .'\')' );
-		$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'sip_proxy_from_wan\', \''. $db->escape(trim(@$_REQUEST['hp_sip_proxy_from_wan'])) .'\')' );
-		$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'sip_server_from_wan\', \''. $db->escape(trim(@$_REQUEST['hp_sip_server_from_wan'])) .'\')' );
-	}
-		
+	if (! $ok)
+		return new GsError( 'Failed to add host '. $host );
+	$host_id = (int)$db->getLastInsertId();
+	if ($host_id < 1)
+		return new GsError( 'Failed to add host '. $host );
+	
+	$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'api\', \''. $db->escape($api) .'\')' );
+	$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'sip_proxy_from_wan\', \''. $db->escape($sip_proxy_from_wan) .'\')' );
+	$db->execute( 'REPLACE INTO `host_params` (`host_id`, `param`, `value`) VALUES ('. $host_id .', \'sip_server_from_wan\', \''. $db->escape($sip_sbc_from_wan) .'\')' );
+	
 	return true;
 }
 
