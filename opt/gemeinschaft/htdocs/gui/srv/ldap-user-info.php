2af0ef08a7bb32831eb5f3be2dd0c5f4d972b72d
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-10-04 22:58:26 +0000

    Code ausgelagert nach inc/gs-fns/gs_ldap_user_search.php
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5218 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/srv/ldap-user-info.php
+++ opt/gemeinschaft/htdocs/gui/srv/ldap-user-info.php
@@ -84,90 +84,13 @@ if (! array_key_exists('u', $_REQUEST)) {
 	_not_found( 'Username not specified.' );
 }
 $user = $_REQUEST['u'];
-if (! preg_match('/^[a-z0-9\-_]+$/', $user)) {
-	_not_found( 'Invalid username "'.$user.'".' );
-}
 
-$GS_LDAP_HOST = gs_get_conf('GS_LDAP_HOST');
-if (in_array($GS_LDAP_HOST, array(null, false, '', '0.0.0.0'), true)) {
-	_server_error( 'LDAP not configured.' );
-}
-if (! preg_match('/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/', $GS_LDAP_HOST)) {
-	$tmp = getHostByName($GS_LDAP_HOST);
-	if ($tmp == $GS_LDAP_HOST) {
-		_server_error( 'Failed to look up LDAP server.' );
-	}
-	$GS_LDAP_HOST = $tmp;
-}
-$tmp = @ip2long($GS_LDAP_HOST);
-if (in_array($tmp, array(false, null, -1, 0), true)) {
-	_server_error( 'LDAP not configured (bad IP address).' );
-}
-
-require_once( GS_DIR .'inc/ldap.php' );
-if (!($ldap_conn = gs_ldap_connect(
-	$GS_LDAP_HOST
-))) {
-	_server_error( 'Could not connect to LDAP server.' );
-}
-
-
-$req_props = array();
-$GS_LDAP_PROP_FIRSTNAME = trim(gs_get_conf('GS_LDAP_PROP_FIRSTNAME'));
-$GS_LDAP_PROP_LASTNAME  = trim(gs_get_conf('GS_LDAP_PROP_LASTNAME'));
-$GS_LDAP_PROP_EMAIL     = trim(gs_get_conf('GS_LDAP_PROP_EMAIL'));
-$GS_LDAP_PROP_PHONE     = trim(gs_get_conf('GS_LDAP_PROP_PHONE'));
-if ($GS_LDAP_PROP_FIRSTNAME != '') $req_props[] = $GS_LDAP_PROP_FIRSTNAME;
-if ($GS_LDAP_PROP_LASTNAME  != '') $req_props[] = $GS_LDAP_PROP_LASTNAME;
-if ($GS_LDAP_PROP_EMAIL     != '') $req_props[] = $GS_LDAP_PROP_EMAIL;
-if ($GS_LDAP_PROP_PHONE     != '') $req_props[] = $GS_LDAP_PROP_PHONE;
-
-
-$users_arr = gs_ldap_get_list( $ldap_conn, gs_get_conf('GS_LDAP_SEARCHBASE'),
-	gs_get_conf('GS_LDAP_PROP_USER') .'='. $user,
-	$req_props,
-	2
-);
-//print_r($users_arr);
-
-@gs_ldap_disconnect( $ldap_conn );
-if (isGsError($users_arr))
-	_server_error( $user_arr->getMsg() );
-if (! is_array($users_arr) || count($users_arr) < 1)
-	_not_found( 'User "'.$user.'" not found in LDAP.' );
-if (count($users_arr) > 1)
-	_server_error( 'LDAP search did not return a unique user for "'.$user.'".' );
-$user_arr = $users_arr[0];
-unset($users_arr);
-
-$user_info = array(
-	'fn'    => null,
-	'ln'    => null,
-	'email' => null,
-	'exten' => null
-);
-if (array_key_exists($GS_LDAP_PROP_FIRSTNAME, $user_arr)) {
-	$user_info['fn'] = @$user_arr[$GS_LDAP_PROP_FIRSTNAME][0];
-}
-if (array_key_exists($GS_LDAP_PROP_LASTNAME, $user_arr)) {
-	$user_info['ln'] = @$user_arr[$GS_LDAP_PROP_LASTNAME][0];
-}
-if (array_key_exists($GS_LDAP_PROP_EMAIL, $user_arr)) {
-	$user_info['email'] = @$user_arr[$GS_LDAP_PROP_EMAIL][0];
-}
-if (array_key_exists($GS_LDAP_PROP_PHONE, $user_arr)) {
-	require_once( GS_DIR .'inc/canonization.php' );
-	$phone = @$user_arr[$GS_LDAP_PROP_PHONE][0];
-	$phone = preg_replace('/[^0-9+#*]/', '', $phone);
-	$cpn = new CanonicalPhoneNumber($phone);
-	if ($cpn->in_prv_branch) {
-		$user_info['exten'] = $cpn->extn;
-	}
-	unset($cpn);
-}
-unset($user_arr);
-gs_log( GS_LOG_DEBUG, 'Found user "'.$user.'" ('. trim($user_info['fn'].' '.$user_info['ln']) .') in LDAP' );
-
+include_once( GS_DIR .'inc/gs-fns/gs_ldap_user_search.php' );
+$user_info = gs_ldap_user_search( $user );
+if (isGsError($user_info))
+	_server_error( $user_info->getMsg() );
+if (! is_array($user_info))
+	_server_error( 'Failed to look up user "'.$user.'" in LDAP.' );
 
 
 require_once( GS_DIR .'lib/utf8-normalize/gs_utf_normal.php' );
