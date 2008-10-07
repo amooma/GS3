0e34461805bebc4f7a1a00fe40ebd9d61274104d
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-10-07 18:19:08 +0000

    added missing parameter $soap_faultcode, $soap_faultcode vorher initialisieren, s/soap_fault/soap_faultcode/
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5257 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/gs-fns/gs_user_change.php
+++ opt/gemeinschaft/inc/gs-fns/gs_user_change.php
@@ -240,7 +240,8 @@ function gs_user_change( $user, $pin, $firstname, $lastname, $host_id_or_ip, $fo
 						gs_log( GS_LOG_DEBUG, "Mapping ext. $ext to $sub_ext for SOAP call" );
 						
 						include_once( GS_DIR .'inc/boi-soap/boi-soap.php' );
-						$ok = gs_boi_update_extension( $api, $host['host'], $hp_route_prefix, $sub_ext, $user, $sip_pwd, $pin, $firstname, $lastname, $email, $soap_fault );
+						$soap_faultcode = null;
+						$ok = gs_boi_update_extension( $api, $host['host'], $hp_route_prefix, $sub_ext, $user, $sip_pwd, $pin, $firstname, $lastname, $email, $soap_faultcode );
 						if (! $ok) {
 							gs_db_rollback_trans($db);
 							return new GsError( 'Failed to add user on new foreign host (SOAP error).' );
@@ -280,7 +281,8 @@ function gs_user_change( $user, $pin, $firstname, $lastname, $host_id_or_ip, $fo
 						gs_log( GS_LOG_DEBUG, "Mapping ext. $ext to $sub_ext for SOAP call" );
 						
 						include_once( GS_DIR .'inc/boi-soap/boi-soap.php' );
-						$ok = gs_boi_update_extension( $api, $host['host'], $hp_route_prefix, $sub_ext, $user, $sip_pwd, $pin, $firstname, $lastname, $email );
+						$soap_faultcode = null;
+						$ok = gs_boi_update_extension( $api, $host['host'], $hp_route_prefix, $sub_ext, $user, $sip_pwd, $pin, $firstname, $lastname, $email, $soap_faultcode );
 						if (! $ok) {
 							gs_db_rollback_trans($db);
 							return new GsError( 'Failed to modify user on foreign host (SOAP error).' );
