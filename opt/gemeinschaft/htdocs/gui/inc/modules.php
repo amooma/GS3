67eb7e60403ab0d26f15c2fda6b77ff6470f8b1d
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-11-08 14:45:44 +0000

    einheitlich Pickup-Gruppe/Rufgruppe -> Rufannahmegruppe
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5480 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/inc/modules.php
+++ opt/gemeinschaft/htdocs/gui/inc/modules.php
@@ -231,7 +231,7 @@ $MODULES['admin'    ]=  array(
 		'users'        => array('title' => __('Benutzer')),
 		'groups'       => array('title' => __('Benutzergruppen')),
 		'queues'       => array('title' => __('Warteschlangen')),
-		'pgroups'      => array('title' => __('PickUp-Gruppen')),
+		'pgroups'      => array('title' => __('Rufannahmegrp.')),
 		//'ivrs'         => array('title' => __('IVRs')),
 		'calls'        => array('title' => __('CDRs')),
 		'reload'       => array('title' => __('Reload'))
