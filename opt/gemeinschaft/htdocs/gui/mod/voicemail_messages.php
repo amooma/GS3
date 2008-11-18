ec1282e63384627af723584ea3def0b3987a78ad
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-11-18 21:42:06 +0000

    Unterstützung für .wav
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5588 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/voicemail_messages.php
+++ opt/gemeinschaft/htdocs/gui/mod/voicemail_messages.php
@@ -45,6 +45,15 @@ $folders = array(
 	'Cust5'  => __('Verzeichnis 9')
 );
 
+$formats = array( # internal name to info
+	'mp3'  => array( 'title'=>'MP3' , 'ext'=>'mp3' , 'mime'=>'audio/mpeg'   ),  # RFC 3003
+	//'al'   => array( 'title'=>'Alaw', 'ext'=>'al'  , 'mime'=>'audio/PCMA'   ),  # RFC 4856
+	//'ul'   => array( 'title'=>'Ulaw', 'ext'=>'au'  , 'mime'=>'audio/basic'  ),
+	'wav'  => array( 'title'=>'Wave', 'ext'=>'wav' , 'mime'=>'audio/x-wav'  ),
+);
+# For MIME types see http://www.iana.org/assignments/media-types/audio/
+# Keep in sync with srv/vm-play.php
+
 
 $GS_INSTALLATION_TYPE_SINGLE = gs_get_conf('GS_INSTALLATION_TYPE_SINGLE');
 if (! $GS_INSTALLATION_TYPE_SINGLE) {
@@ -73,10 +82,28 @@ if (! $GS_INSTALLATION_TYPE_SINGLE) {
 
 
 
+//echo "<pre>"; print_r($_REQUEST); echo "</pre>";
+
+
+?>
+<form method="get" action="<?php echo gs_url($SECTION, $MODULE, null, 'action=play'); ?>">
+<?php echo gs_form_hidden($SECTION, $MODULE); ?>
+<input type="hidden" name="action" value="play" />
+<?php
+
 if (@$_REQUEST['action']==='play') {
 	
+	/*
 	$fld  = preg_replace('/[^a-z0-9\-_]/i', '', @$_REQUEST['fld' ]);
 	$file = preg_replace('/[^a-z0-9\-_]/i', '', @$_REQUEST['file']);
+	*/
+	$play = explode('--', @$_REQUEST['play']);
+	$fld  = preg_replace('/[^a-z0-9\-_]/i', '', @$play[0]);
+	$file = preg_replace('/[^a-z0-9\-_]/i', '', @$play[1]);
+	$fmt  = preg_replace('/[^a-z0-9\-_]/i', '', @$_REQUEST['fmt' ]);
+	if (! array_key_exists($fmt, $formats)) {
+		$fmt = 'mp3';
+	}
 	
 	/*
 	$vm_dir = '/var/spool/asterisk/voicemail/default/';
@@ -165,48 +192,63 @@ if (@$_REQUEST['action']==='play') {
 	# http://www.iana.org/assignments/media-types/audio/
 	# http://www.rfc-editor.org/rfc/rfc3003.txt
 	
-	echo '<div class="fr" style="width:250px; border:1px solid #ccc; padding:4px; background:#eee;">' ,"\n";
-	echo __('Player') ,"\n";
+	echo '<div class="fr" style="clear:right; width:250px; padding:0 3px 4px 3px; margin-top:-5px;">' ,"\n";
+	foreach ($formats as $fmt_name => $fmt_info) {
+		echo '<input type="radio" name="fmt" value="',$fmt_name,'" id="ipt-fmt-',$fmt_name,'"';
+		if ($fmt_name === $fmt) echo ' checked="checked"';
+		echo '/><label for="ipt-fmt-',$fmt_name,'"><small>',htmlEnt($fmt_info['title']),'</small></label>' ,"\n";
+	}
+	echo "\n", '</div>' ,"\n";
+	
+	echo '<div class="fr" style="clear:right; width:250px; border:1px solid #ccc; padding:3px; background:#eee;">' ,"\n";
+	//echo __('Player') ,"\n";
+	
+	$audio_url_esc = GS_URL_PATH .'srv/vm-play.php?sudo='. @$_SESSION['sudo_user']['name']
+		.'&amp;fld='.$fld
+		.'&amp;msg='.$file
+		.'&amp;fmt='.$fmt
+		.'&amp;msie=.'. $formats[$fmt]['ext']
+		;
 	
 	if (strPos(@$_SERVER['HTTP_USER_AGENT'], 'MSIE')===false) {
-	?>
+?>
 	
 	<!-- W3 compliant version: -->
 	<object
 		id="player"
-		type="audio/mpeg"
-		data="<?php echo GS_URL_PATH, 'srv/vm-play.php?sudo=', @$_SESSION['sudo_user']['name'], '&amp;fld=',$fld, '&amp;msg=',$file, '&amp;msie=.mp3'; ?>"
+		type="<?php echo $formats[$fmt]['mime']; ?>"
+		data="<?php echo $audio_url_esc; ?>"
 		width="250"
 		height="22"
 		align="right"
 		>
 		<param name="autoplay" value="true" />
 		<param name="controller" value="true" />
-		<small><?php echo sPrintF(__('Datei nicht gefunden oder Ihr Browser kann die %s-Datei nicht abspielen.'), 'MP3'); ?></small>
+		<small><?php echo sPrintF(__('Datei nicht gefunden oder Ihr Browser kann die %s-Datei nicht abspielen.'), $formats[$fmt]['title']); ?></small>
 	</object>
 	
-	<?php
+<?php
 	} else {
-	?>
+?>
 	
 	<!-- MSIE version (for QuickTime ActiveX): -->
 	<object
 		id="player"
-		type="audio/mpeg"
+		type="<?php echo $formats[$fmt]['mime']; ?>"
 		classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B"
 		codebase="http://www.apple.com/qtactivex/qtplugin.cab"
-		data="<?php echo GS_URL_PATH, 'srv/vm-play.php?sudo=', @$_SESSION['sudo_user']['name'], '&amp;fld=',$fld, '&amp;msg=',$file, '&amp;msie=.mp3'; ?>"
+		data="<?php echo $audio_url_esc; ?>"
 		width="250"
 		height="22"
 		align="right"
 		>
-		<param name="src" value="<?php echo GS_URL_PATH, 'srv/vm-play.php?sudo=', @$_SESSION['sudo_user']['name'], '&amp;fld=',$fld, '&amp;msg=',$file, '&amp;msie=.mp3'; ?>" />
+		<param name="src" value="<?php echo $audio_url_esc; ?>" />
 		<param name="autoplay" value="true" />
 		<param name="controller" value="true" />
-		<small><?php echo sPrintF(__('Datei nicht gefunden oder Ihr Browser kann die %s-Datei nicht abspielen.'), 'MP3'); ?></small>
+		<small><?php echo sPrintF(__('Datei nicht gefunden oder Ihr Browser kann die %s-Datei nicht abspielen.'), $formats[$fmt]['title']); ?></small>
 	</object>
 	
-	<?php
+<?php
 	}
 	echo "\n", '</div>' ,"\n";
 }
@@ -290,14 +332,16 @@ while ($msg = $rs->fetchRow()) {
 }
 
 
-
+?>
+<table cellspacing="1" class="phonebook" style="clear:right;">
+<tbody>
+<?php
 foreach ($folders as $folder => $folder_title) {
 	if ($folder=='INBOX' || $folder=='Old' || @count(@$msgs[$folder])>0) {
 ?>
-
-<h3><?php echo $folder_title; ?></h3>
-<table cellspacing="1" class="phonebook">
-<thead>
+<tr>
+	<th colspan="5" class="transp"><h3><?php echo $folder_title; ?></h3></th>
+</tr>
 <tr>
 	<th style="width:10px;">&nbsp;</th>
 	<th style="width:135px;"><?php echo __('Datum'); ?> <small>&darr;</small></th>
@@ -305,8 +349,6 @@ foreach ($folders as $folder => $folder_title) {
 	<th style="width:45px;" class="r"><?php echo __('Dauer'); ?></th>
 	<th style="width:105px;">&nbsp;</th>
 </tr>
-</thead>
-<tbody>
 <?php
 
 if (! is_array(@$msgs[$folder]) || count($msgs[$folder]) < 1) {
@@ -343,7 +385,8 @@ if (! is_array(@$msgs[$folder]) || count($msgs[$folder]) < 1) {
 		echo '<td class="r">', $dur_m, ':', str_pad($dur_s, 2, '0', STR_PAD_LEFT), '</td>', "\n";
 		
 		echo '<td class="r">';
-		echo ' <a href="', gs_url($SECTION, $MODULE, null, 'action=play&amp;fld='. rawUrlEncode( @$msg['fld'] ) .'&amp;file='. rawUrlEncode( @$msg['file'] )), '" title="', __('abspielen'), '"><img alt="', __('abspielen'), '" src="', GS_URL_PATH, 'crystal-svg/16/app/kmix.png" /></a>';
+		//echo ' <a href="', gs_url($SECTION, $MODULE, null, 'action=play&amp;fld='. rawUrlEncode(@$msg['fld']) .'&amp;file='. rawUrlEncode(@$msg['file'])), '" title="', __('abspielen'), '"><img alt="', __('abspielen'), '" src="', GS_URL_PATH, 'crystal-svg/16/app/kmix.png" /></a>';
+		echo ' <button class="submit plain" name="play" value="', rawUrlEncode(@$msg['fld'].'--'.@$msg['file']) ,'" title="', __('abspielen'), '"><img alt="', __('abspielen'), '" src="', GS_URL_PATH, 'crystal-svg/16/app/kmix.png" /></button>';
 		if (@$msg['cidnum'] != '') {
 			$sudo_url =
 				(@$_SESSION['sudo_user']['name'] == @$_SESSION['real_user']['name'])
@@ -352,7 +395,7 @@ if (! is_array(@$msgs[$folder]) || count($msgs[$folder]) < 1) {
 		} else {
 			echo ' &nbsp; <img alt="', __('w&auml;hlen'), '" src="', GS_URL_PATH, 'crystal-svg/16/app/yast_PhoneTTOffhook-cust-dis.png" />';
 		}
-		echo ' &nbsp;&nbsp; <a href="', gs_url($SECTION, $MODULE, null, 'action=del&amp;fld='. rawUrlEncode( @$msg['fld'] ) .'&amp;file='. rawUrlEncode( @$msg['file'] )), '" title="', __('l&ouml;schen'), '"><img alt="l&ouml;schen" src="', GS_URL_PATH, 'img/trash.gif" /></a>';
+		echo ' &nbsp;&nbsp; <a href="', gs_url($SECTION, $MODULE, null, 'action=del&amp;fld='. rawUrlEncode(@$msg['fld']) .'&amp;file='. rawUrlEncode(@$msg['file'])), '" title="', __('l&ouml;schen'), '"><img alt="l&ouml;schen" src="', GS_URL_PATH, 'img/trash.gif" /></a>';
 		echo '</td>', "\n";
 		
 		echo '</tr>', "\n";
@@ -360,20 +403,24 @@ if (! is_array(@$msgs[$folder]) || count($msgs[$folder]) < 1) {
 }
 
 ?>
-</tbody>
-</table>
-<br />
-
+<tr>
+	<td colspan="5" class="transp" style="height:0.8em;"></td>
+</tr>
 <?php
 	}
 }
-
+?>
+</tbody>
+</table>
+</form>
+<?php
 
 
 if (@$_REQUEST['action']==='play') {
 ?>
 
 <script type="text/javascript">
+/*<![CDATA[*/
 
 function check_player()
 {
@@ -402,6 +449,7 @@ function check_player()
 
 var player_interval = window.setInterval('check_player();', 2000);
 
+/*]]>*/
 </script>
 
 <?php
