e75120daea30dfb57091b79d52ac5f97b75d7777
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-12-09 22:28:54 +0000

    urlencode(), xhtml, etc.
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5755 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/forwards_extnumbers.php
+++ opt/gemeinschaft/htdocs/gui/mod/forwards_extnumbers.php
@@ -45,42 +45,63 @@ include_once( GS_DIR .'inc/gs-fns/gs_user_external_numbers_get.php' );
 include_once( GS_DIR .'inc/gs-fns/gs_user_external_number_add.php' );
 include_once( GS_DIR .'inc/gs-fns/gs_user_external_number_del.php' );
 
-$save_number   =  trim(@$_REQUEST['number']);
-$delete_number =  trim(@$_REQUEST['delete']);
+
+$add_number    = trim(@$_REQUEST['add'   ]);
+$delete_number = trim(@$_REQUEST['delete']);
 
 if ($delete_number != '') {
 	$ret = gs_user_external_number_del( $_SESSION['sudo_user']['name'], $delete_number );
-	if (isGsError( $ret )) {
+	if (isGsError($ret)) {
 		echo '<div class="errorbox">';
-		echo $ret->getMsg();
+		echo htmlEnt(__("Fehler beim L\xC3\xB6schen")) ,'<br />', htmlEnt($ret->getMsg());
+		echo '</div>',"\n";
+	}
+	elseif (! $ret) {
+		echo '<div class="errorbox">';
+		echo htmlEnt(__("Fehler beim L\xC3\xB6schen"));
 		echo '</div>',"\n";
 	}
 }
 
-if ($save_number != '') {
-	$ret = gs_user_external_number_add( $_SESSION['sudo_user']['name'], $save_number );
-	if (isGsError( $ret )) {
+if ($add_number != '') {
+	$ret = gs_user_external_number_add( $_SESSION['sudo_user']['name'], $add_number );
+	if (isGsError($ret)) {
 		echo '<div class="errorbox">';
-		echo $ret->getMsg();
+		echo htmlEnt(__("Fehler beim Speichern")) ,'<br />', htmlEnt($ret->getMsg());
+		echo '</div>',"\n";
+	}
+	elseif (! $ret) {
+		echo '<div class="errorbox">';
+		echo htmlEnt(__("Fehler beim Speichern"));
 		echo '</div>',"\n";
 	}
 }
 
 $enumbers = gs_user_external_numbers_get( $_SESSION['sudo_user']['name'] );
 if (isGsError($enumbers)) {
-	echo __('Fehler beim Abfragen.'), '<br />', $enumbers->getMsg();
-	die();
+	echo '<div class="errorbox">';
+	echo htmlEnt(__("Fehler beim Abfragen")) ,'<br />', htmlEnt($enumbers->getMsg());
+	echo '</div>',"\n";
+	return;
+}
+elseif (! is_array($enumbers)) {
+	echo '<div class="errorbox">';
+	echo __("Fehler beim Abfragen");
+	echo '</div>',"\n";
+	return;
 }
 
 ?>
 
 <p><?php echo __('Au&szlig;er auf interne Nummern d&uuml;rfen Sie auf folgende externe Nummern weiterleiten:'); ?></p>
 
+<form method="post" action="<?php echo GS_URL_PATH; ?>">
+<?php echo gs_form_hidden($SECTION, $MODULE); ?>
 <table cellspacing="1" class="phonebook">
 <thead>
 <tr>
 	<th style="width:200px;"><?php echo __('Externe Nummern'); ?></th>
-	<th style="width:20px;" class="nobr"></th>
+	<th style="width:20px;"></th>
 </tr>
 </thead>
 <tbody>
@@ -94,28 +115,26 @@ if (count($enumbers) < 1) {
 		echo '<tr>';
 		echo '<td>', htmlEnt( $enumber ), '</td>';
 		echo '<td>';
-		echo '<a href="', gs_url($SECTION, $MODULE, null, 'delete='.$enumber), '" title="', __('entfernen'), '"><img alt="', __('entfernen'), '" src="', GS_URL_PATH, 'crystal-svg/16/act/editdelete.png" /></a>';
-		echo '</td>';	
+		echo '<a href="', gs_url($SECTION, $MODULE, null, 'delete='.urlEncode($enumber)), '" title="', __('entfernen'), '"><img alt="', __('entfernen'), '" src="', GS_URL_PATH, 'crystal-svg/16/act/editdelete.png" /></a>';
+		echo '</td>';
 		echo '</tr>', "\n";
 	}
 }
 
 echo '<tr>';
 echo '<td>';
-echo '<form method="post" action="', GS_URL_PATH, '">', "\n";
-echo gs_form_hidden($SECTION, $MODULE), "\n";
-echo '<input type="text" name="number" value="" size="15" maxlength="40" style="width:100px;" />';
+echo '<input type="text" name="add" value="" size="20" maxlength="30" />';
+echo '</td>';
 echo '<td>';
-echo '<button type="submit" title="'. __('Eintrag speichern').'" class="plain">	<img alt="'.__('Speichern').'" src="'.GS_URL_PATH.'crystal-svg/16/act/filesave.png" /></button>';
+echo '<button type="submit" title="', __('Eintrag speichern') ,'" class="plain"><img alt="', __('Speichern') ,'" src="', GS_URL_PATH,'crystal-svg/16/act/filesave.png" /></button>';
+echo '</td>';
+echo '</tr>';
 ?>
 
-</form>
-</td>
-</tr>
 </tbody>
 </table>
+</form>
 <br />
 <br />
-
 
 
