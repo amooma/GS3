94ff394974e27c6ecd2f9300d404408183068ae0
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2008-12-23 23:38:14 +0000

    {}
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@5826 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/inc_keyprof.php
+++ opt/gemeinschaft/htdocs/gui/mod/inc_keyprof.php
@@ -737,17 +737,17 @@ function gs_key_fn_h( el )
 			<?php
 			$innerhtml = '<br />'. __('Bitte w&auml;hlen Sie eine Applikation aus') .':<br /><br />';
 			$innerhtml.= '<select name="helper_apps" size="1">';
-			$SIEMENS_XML_APPS = split(',' , gs_get_conf('GS_SIEMENS_PROV_SHOW_APPS'));
-			if (is_array($SIEMENS_XML_APPS))
+			$SIEMENS_XML_APPS = preg_split('/[,\\s]+/', gs_get_conf('GS_SIEMENS_PROV_SHOW_APPS'), -1, PREG_SPLIT_NO_EMPTY);
+			if (is_array($SIEMENS_XML_APPS)) {
 				foreach ($SIEMENS_XML_APPS as $app => $appname) {
 					$innerhtml.= '<option>'. $appname .'</option>';
 				}
+			}
 			$innerhtml.= '</select>';
 			$innerhtml.= '<br /><br /><div align="center"><a href="#" title="'. __('OK') .'" onclick="return gs_dlg_ok();"><img alt="'. __('OK') .'" src="'. GS_URL_PATH .'crystal-svg/32/act/button_ok.png" /></a>';
 			$innerhtml.= ' <a href="#" title="'. __('Abbrechen') .'" onclick="return gs_dlg_abort();"><img alt="'. __('Abbrechen') .'" src="'. GS_URL_PATH .'crystal-svg/32/act/button_cancel.png" /></a></div>';
-			?>			
+			?>
 			showDialog('<?php echo __("Applikation ausw&auml;hlen"); ?>', '<?php echo $innerhtml; ?>');
-			
 		}
 	}
 	catch(e){}
