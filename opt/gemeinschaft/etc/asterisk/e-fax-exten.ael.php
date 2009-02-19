acaaded451d95a09fedc16badbdc1904ff5b63da
peter.kozak@amooma.de <peter.kozak@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-02-19 11:00:27 +0000

    Integrated HylaFAX+ support updated.
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6096 2d91074e-20bd-43ae-8273-9033e1f9829e

---
+++ opt/gemeinschaft/etc/asterisk/e-fax-exten.ael.php
@@ -0,0 +1,54 @@
+#!/usr/bin/php -q
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2009, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* Soeren Sprenger <soeren.sprenger@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+define( 'GS_VALID', true );  /// this is a parent file
+
+ini_set('implicit_flush', 1);
+ob_implicit_flush(1);
+
+error_reporting(0);
+
+require_once( dirName(__FILE__) .'/../../inc/conf.php' );
+require_once( GS_DIR .'inc/util.php' );
+set_error_handler('err_handler_quiet');
+
+echo "\n";
+
+$FAX_PREFIX = gs_get_conf('GS_FAX_PREFIX');
+$FAX_PREFIX = preg_replace('/[^0-9*]/', '', $FAX_PREFIX);
+
+if ($FAX_PREFIX != '') {
+	echo '_', $FAX_PREFIX ,'. => jump fax-${EXTEN:', strlen($FAX_PREFIX) ,'}@to-internal-users;',"\n";
+} 
+
+
+echo "\n";
+
+?>
