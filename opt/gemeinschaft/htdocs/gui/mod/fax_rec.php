9843b983a4c8d31f79f0b638854e847f88b1f5c2
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-02-28 09:19:53 +0000

    pdf- und tiff-icon
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6169 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/fax_rec.php
+++ opt/gemeinschaft/htdocs/gui/mod/fax_rec.php
@@ -138,8 +138,8 @@ for ($i=($page*$per_page); $i < ($per_page*$page)+$per_page; $i++) {
 		echo "<td>", $jobs_rec[$i][1] ,"</td>\n";
 		echo "<td>\n";
 		echo '<a href="',$mod_url,'&amp;delete=',$jobs_rec[$i][4],'&amp;page=',$page,'" title="', __('l&ouml;schen'), '"><img alt="', __('l&ouml;schen'), '" src="', GS_URL_PATH, 'crystal-svg/16/act/editdelete.png" /></a>';
-		echo ' &nbsp; <a href="',GS_URL_PATH ,'srv/faxdown.php?file='.rawUrlEncode($jobs_rec[$i][4]).'&amp;sudo='.rawUrlEncode(@$_SESSION['sudo_user']['name']).'"><img alt="PDF" src="', GS_URL_PATH, 'crystal-svg/16/act/pdf.png" /></a>'."\n";
-		echo ' &nbsp; <a href="',GS_URL_PATH ,'srv/faxdown.php?raw='.rawUrlEncode($jobs_rec[$i][4]).'&amp;sudo='.rawUrlEncode(@$_SESSION['sudo_user']['name']).'"><img alt="TIFF" src="', GS_URL_PATH, 'crystal-svg/16/act/colorize.png" /></a>'."\n";
+		echo ' &nbsp; <a href="',GS_URL_PATH ,'srv/faxdown.php?file='.rawUrlEncode($jobs_rec[$i][4]).'&amp;sudo='.rawUrlEncode(@$_SESSION['sudo_user']['name']).'"><img alt="PDF" src="', GS_URL_PATH, 'crystal-svg/16/mime/pdf.png" /></a>'."\n";
+		echo ' &nbsp; <a href="',GS_URL_PATH ,'srv/faxdown.php?raw='.rawUrlEncode($jobs_rec[$i][4]).'&amp;sudo='.rawUrlEncode(@$_SESSION['sudo_user']['name']).'"><img alt="TIFF" src="', GS_URL_PATH, 'crystal-svg/16/mime/image.png" /></a>'."\n";
 		echo "</td>\n";
 		
 		echo "</tr>\n";
