86a849ff9737f9d1130b968d31e31cd922d18c81
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-03-03 19:44:59 +0000

    msie-button-fix.htc
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6207 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/js/msie-button-fix.htc.php
+++ opt/gemeinschaft/htdocs/gui/js/msie-button-fix.htc.php
@@ -1,13 +1,36 @@
 <?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision: 5712 $
+* 
+* Copyright 2009, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
 
 header( 'Content-Type: text/x-component' );
 //header( 'Content-Type: text/plain' );
 
-header( 'Cache-Control: public, max-age=120, must-revalidate' );
-header( 'Expires: '. gmDate('D, d M Y H:i:s', time()+120) .' GMT');
+header( 'Cache-Control: public, max-age=300' );
+header( 'Expires: '. gmDate('D, d M Y H:i:s', time()+300) .' GMT');
 
 define( 'GS_VALID', true );  /// this is a parent file
-require_once( dirName(__FILE__) .'/../../../inc/conf.php' );
 
 
 header( 'ETag: '. gmDate('Ymd').'-'.md5($_SERVER['SCRIPT_NAME']) );
@@ -22,7 +45,7 @@ function _not_modified()
 
 if (array_key_exists('HTTP_IF_NONE_MATCH', $_SERVER)
 &&  $_SERVER['HTTP_IF_NONE_MATCH'] === gmDate('Ymd').'-'.md5($_SERVER['SCRIPT_NAME']) ) {
-	//_not_modified();
+	_not_modified();
 }
 /*
 $tmp = gmDate('D, d M Y');
@@ -40,14 +63,38 @@ ob_start();
 <script type="text/javascript" language="JavaScript">
 
 /*
--- Philipp Kempgen
+Gemeinschaft
+(c) Amooma GmbH, Philipp Kempgen
+GNU GPL
+$Revision: 3960 $
 */
 
 function button_click()
 {
-	alert('auf einen Button geklickt');
-	//...
-	
+	try {
+		var btn = (this || element || null);
+		if (! btn) return;
+		if (! btn.attributes) return;
+
+		var val_attr = null;
+		if (btn.attributes['value']) val_attr = btn.attributes['value'];
+		if (! val_attr) {
+			if (btn.attributes.getNamedItem) val_attr = btn.attributes.getNamedItem('value');
+			if (! val_attr) return;
+		}
+		var orig_val = (val_attr.value || val_attr.nodeValue || '');
+		
+		if (btn.offsetWidth && btn.offsetHeight) {
+			btn.style.width  = parseInt(btn.offsetWidth ) +'px';
+			btn.style.height = parseInt(btn.offsetHeight) +'px';
+			if ((typeof btn.style.overflow)=='string')
+				btn.style.overflow = 'hidden';
+			if ((typeof btn.style.filter)=='string')
+				btn.style.filter = 'Alpha(opacity=20)';
+		}
+		btn.value = orig_val;
+	}
+	catch(e){}
 }
 
 </script>
@@ -56,8 +103,8 @@ function button_click()
 <?php
 
 if (! headers_sent())
-	header( 'Content-Length: '. (int)ob_get_length() );
-ob_end_flush();
+	header( 'Content-Length: '. (int)@ob_get_length() );
+@ob_end_flush();
 exit();
 
 ?>
