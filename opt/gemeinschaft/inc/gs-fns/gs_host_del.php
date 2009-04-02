89043eb64517203039ff5e2c17fddc3df86bd5ae
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-04-02 10:10:18 +0000

    whitespace, keyword substitution
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6313 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/gs-fns/gs_host_del.php
+++ opt/gemeinschaft/inc/gs-fns/gs_host_del.php
@@ -29,10 +29,10 @@
 defined('GS_VALID') or die('No direct access.');
 include_once( GS_DIR .'inc/gs-lib.php' );
 include_once( GS_DIR .'inc/gs-fns/gs_host_by_id_or_ip.php' );
-include_once( GS_DIR .'inc/gs-fns/gs_prov_phone_checkcfg.php' );
-include_once( GS_DIR .'inc/gs-fns/gs_asterisks_reload.php' );
-include_once( GS_DIR .'inc/gs-fns/gs_user_del.php' );
-include_once( GS_DIR .'inc/log_auth.php' );
+//include_once( GS_DIR .'inc/gs-fns/gs_prov_phone_checkcfg.php' );
+//include_once( GS_DIR .'inc/gs-fns/gs_asterisks_reload.php' );
+//include_once( GS_DIR .'inc/gs-fns/gs_user_del.php' );
+
 
 /***********************************************************
 *    delete a host
@@ -41,30 +41,30 @@ include_once( GS_DIR .'inc/log_auth.php' );
 function gs_host_del( $host,  $force=FALSE )
 {
 	if (! preg_match( '/^[0-9\.]+$/', $host ))
-		return new GsError( 'Host must be a numeric ID or IP.' );
-
+		return new GsError( 'Host must be a numeric ID or IP address.' );
+	
 	$host = gs_host_by_id_or_ip( $host );
-		
+	
 	if (isGsError($host))   return new GsError(  $host->getMsg() );
 	if (! is_array($host))  return new GsError( 'Cannot retrieve host ID.' ); 
-
+	
 	# connect to db
 	#
 	$db = gs_db_master_connect();
 	if (! $db)
 		return new GsError( 'Could not connect to database.' );
-
+	
 	$count_users = $db->executeGetOne( 'SELECT COUNT(`id`) FROM `users` WHERE `host_id`=\''. $db->escape($host['id']) .'\'' );
-
-	if ($count_users > 0) 
-		 return new GsError( 'Cannot delete host. Delete '.$count_users.' user(s) attached to this host first.' );
-
+	
+	if ($count_users > 0)
+		 return new GsError( 'Cannot delete host. Delete '.$count_users.' user(s) on this host first.' );
+	
 	# delete host
 	#
 	$rs = $db->execute( 'DELETE from `hosts` WHERE `id`=\''. $db->escape($host['id']) .'\'' );
 	if (! $rs)
-		return new GsError( 'Cannot delete host '.$host['id'] );
-
+		return new GsError( 'Could not delete host '.$host['id'] );
+	
 	return true;
 }
 
