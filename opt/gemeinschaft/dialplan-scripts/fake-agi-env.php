d60e4d20e8880cf1b5d403b692eb2e00e0ddccf9
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-07-03 02:07:41 +0000

    fake-agi verbessert
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6656 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/dialplan-scripts/fake-agi-env.php
+++ opt/gemeinschaft/dialplan-scripts/fake-agi-env.php
@@ -156,6 +156,9 @@ while (true) {
 	elseif (preg_match('/^VERBOSE /i', $agi_cmd_line)) {
 		_fake_agi_send( $pipes[0], '200 result=1' );
 	}
+	elseif (preg_match('/^HANGUP\s*$/i', $agi_cmd_line)) {
+		_fake_agi_send( $pipes[0], '200 result=1' );
+	}
 	else {
 		gs_log( GS_LOG_WARNING, 'AGI script "'.baseName($AGI_SCRIPT).'" sent "'.trim($agi_cmd_line).'" which we cannot handle.' );
 		_fake_agi_send( $pipes[0], '500 result=-1' );
