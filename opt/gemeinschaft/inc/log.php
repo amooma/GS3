2ef7362306be6759fec104bf54acb23bee94541e
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-07-03 20:38:13 +0000

    optional in eine FIFO loggen
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6668 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/log.php
+++ opt/gemeinschaft/inc/log.php
@@ -32,7 +32,7 @@ require_once( GS_DIR .'inc/quote_shell_arg.php' );
 
 $gs_is_in_gs_log = false;
 
-function gs_log( $level, $msg, $logfile=null )
+function gs_log( $level, $msg, $logfile=null, $fifo=false )
 {
 	global $gs_is_in_gs_log;
 	
@@ -72,6 +72,8 @@ function gs_log( $level, $msg, $logfile=null )
 		$line = 0;
 	}
 	
+	if ($fifo) $log_to = 'file';
+	
 	if ($log_to === 'file') {
 		
 		$dateFn = GS_LOG_GMT ? 'gmDate' : 'date';
@@ -84,6 +86,19 @@ function gs_log( $level, $msg, $logfile=null )
 		
 		if (! @array_key_exists($logfile, $logfiles)) {
 			$sudo = (posix_getEUid()==0 ? '' : 'sudo ');
+						
+			# if the logfile should be a fifo but isn't then remove it so
+			# it will be created as a fifo
+			if ($fifo && file_exists($logfile) && ! @is_fifo($logfile)) {
+				$err=0; $out=array();
+				@exec( $sudo.'rm -f '. qsa($logfile) .' 1>>/dev/null 2>>/dev/null', $out, $err );
+				if ($err != 0) {  # probably permission denied
+					$gs_is_in_gs_log = false;
+					return false;
+				}
+				clearStatCache();
+			}
+						
 			if (! @file_exists($logfile)) {
 				 $err=0; $out=array();
 				 @exec( $sudo.'mkdir -p '. qsa(dirName($logfile)) .' 1>>/dev/null 2>>/dev/null', $out, $err );
@@ -91,8 +106,25 @@ function gs_log( $level, $msg, $logfile=null )
 					$gs_is_in_gs_log = false;
 					return false;
 				 }
+				if ($fifo) {
+					$err=0; $out=array();
+					@exec( $sudo.'mkfifo '. qsa($logfile) .' 1>>/dev/null 2>>/dev/null', $out, $err );
+					if ($err != 0) {  # probably permission denied
+						$gs_is_in_gs_log = false;
+						return false;
+					}
+				}
 			}
-			$logfiles[$logfile] = @fOpen($logfile, 'ab');  # might fail if permission denied
+						
+			if (! $fifo) {
+				$logfiles[$logfile] = @fOpen($logfile, 'ab');  # might fail if permission denied
+			} else {
+				$logfiles[$logfile] = fOpen($logfile, 'ab+');
+				# The trick is to open the FIFO for reading *and writing*.
+				# "a" : open(..., O_WRONLY|O_CREAT|O_APPEND|O_LARGEFILE, ...)
+				# "a+": open(..., O_RDWR  |O_CREAT|O_APPEND|O_LARGEFILE, ...)
+				# O_WRONLY blocks for FIFOs. O_RDWR does not.
+			}
 			if (! $logfiles[$logfile]) {
 				$gs_is_in_gs_log = false;
 				return false;
@@ -100,6 +132,20 @@ function gs_log( $level, $msg, $logfile=null )
 			//@chmod($logfile, 0666);  # in octal mode!
 			@exec( $sudo.'chmod 0666 '. qsa($logfile) .' 1>>/dev/null 2>>/dev/null');
 		}
+				
+		if ($fifo) {
+			@stream_set_blocking($fd, false);  # not even really needed
+			
+			# Just to be sure check that the stream will not block.
+			# However by using the above trick it will not block anyways.
+			$select = array($logfiles[$logfile]);  # needs to be passed by reference
+			$null   = null;                        # needs to be passed by reference
+			if (stream_select($null, $select, $null, 0, 0) < 1) {
+				$gs_is_in_gs_log = false;
+				return false;
+			}
+		}
+		
 		$ok = @fWrite( $logfiles[$logfile], $msg, strLen($msg) );
 		
 	}
@@ -145,6 +191,13 @@ function gs_log( $level, $msg, $logfile=null )
 	
 	$gs_is_in_gs_log = false;
 	return $ok;
+}
+
+if (! function_exists('is_fifo')) {
+function is_fifo( $filename )
+{
+	return fileType($filename) === 'fifo';
+}
 }
 
 
