02d94f335068f4afa77d3248827228379617802b
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-07-06 00:54:41 +0000

    log AGI cmdline (fix offset)
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6698 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/inc/agi-fns.php
+++ opt/gemeinschaft/inc/agi-fns.php
@@ -45,7 +45,7 @@ function gs_agi_log( $level, $msg )
 
 gs_agi_log( GS_LOG_DEBUG, 'Launched ---------------------------' );
 $log_cmdline = baseName($argv[0]);
-for ($i=1; $i<=$argc; ++$i) {
+for ($i=1; $i<$argc; ++$i) {
 	$log_cmdline.= ' '. ($argv[$i] != '' ? escapeShellArg($argv[$i]) : '\'\'');
 }
 gs_agi_log( GS_LOG_DEBUG, 'Cmdline: '.$log_cmdline );
