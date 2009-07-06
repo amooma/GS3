092278468593ae4f68ac4a7d1899b226fca26241
philipp.kempgen@amooma.de <philipp.kempgen@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-07-06 17:32:06 +0000

    whitespace
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@6711 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft-boi-proxy-reverse/startup.pl
+++ opt/gemeinschaft-boi-proxy-reverse/startup.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
 # directory containing GSProxy.pm (without trailing "/"):
-use lib qw(/opt/gemeinschaft-boi-proxy-reverse); 
+use lib qw(/opt/gemeinschaft-boi-proxy-reverse);
 1;
 
