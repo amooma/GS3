80d0c6292c03aaa4d032a7206d8166bc2199e167
soeren.sprenger@amooma.de <soeren.sprenger@amooma.de@2d91074e-20bd-43ae-8273-9033e1f9829e>
2009-10-05 18:56:23 +0000

    Fixed typos
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@7050 2d91074e-20bd-43ae-8273-9033e1f9829e

--- opt/gemeinschaft/htdocs/gui/mod/help_keys.php
+++ opt/gemeinschaft/htdocs/gui/mod/help_keys.php
@@ -37,16 +37,16 @@ if (count( $MODULES[$SECTION]['sub'] ) > 1 )
 echo $MODULES[$SECTION]['sub'][$MODULE]['title'];
 echo '</h2>', "\n";
 
-echo __("Gemeinschaft bietet die M&ouml;glichkeit, Tasten an unterst&uuml;tzen Engeräten &uuml;ber diese Benutzeroberfl&auml;che zu Konfigurieren.")."<br>";
-echo __("Um Tasten f&uuml;r Ihr Endger&auml;t zu Konfigurieren, folgen Sie einfach den folgenden Schritten, nachdem Sie Links im Men&uuml; auf Tastenbelegung geklickt haben:")."<br><br>";
+echo __("Gemeinschaft bietet die M&ouml;glichkeit, Tasten an unterst&uuml;tzten Endgeräten &uuml;ber diese Benutzeroberfl&auml;che zu konfigurieren.")."<br>";
+echo __("Um Tasten f&uuml;r Ihr Endger&auml;t zu konfigurieren, folgen Sie einfach den folgenden Schritten, nachdem Sie links im Men&uuml; auf Tastenbelegung geklickt haben:")."<br><br>";
 echo '<h3>' .__("Schritt 1:") .'</h3>';
-echo __("W&auml;hlen Sie ihr Telefonmodell aus (1) und dr&uuml;cken Sie auf 'Zeigen' (2)")."<br>";
+echo __("W&auml;hlen Sie Ihr Telefonmodell aus (1) und dr&uuml;cken Sie auf 'Zeigen' (2)")."<br>";
 echo '<img alt="Step1" src="'.GS_URL_PATH.'img/help_keys/step1.jpg"/>';
 echo '<br>';
 echo '<br>';
 
 echo '<h3>' .__("Schritt 2:") .'</h3>';
-echo __("Dannach sehen Sie die folgenden &Uuml;bersicht (oder &auml;hnlich):")."<br>";
+echo __("Dannach sehen Sie die folgende &Uuml;bersicht (oder &auml;hnlich):")."<br>";
 echo '<img alt="Step2" src="'.GS_URL_PATH.'img/help_keys/step2.jpg"/>';
 ?>
 <br><br>
@@ -60,19 +60,19 @@ echo '<img alt="Step2" src="'.GS_URL_PATH.'img/help_keys/step2.jpg"/>';
 <tr>
 	<td style="width:140px;"><?php echo __('Taste'); ?></td>
 	<td style="width:420px;">
-		<?php echo __('Beschreibt die Taste am Telefon im Bespiel ist F001 die oberste Taste am Display vom Siemens Openstage 60.'); ?>
+		<?php echo __('Beschreibt die Taste am Telefon, im Beispiel ist F001 die oberste Taste am Display vom Siemens Openstage 60.'); ?>
 	</td>
 </tr>
 <tr>
 	<td>:=</td>
 	<td>
-		<?php echo __('Zeigt an ob die Taste Aktiviert ist oder nicht, Sie k&ouml;nnen die Taste nur definieren, wenn Sie diese vorher mit dem Haken in dieser Box aktivieren.'); ?>
+		<?php echo __('Zeigt an, ob die Taste aktiviert ist oder nicht, Sie k&ouml;nnen die Taste nur definieren, wenn Sie diese vorher mit dem Haken in dieser Box aktivieren.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Nummer/Daten'); ?></td>
 	<td>
-		<?php echo __('In dieses Feld werden Funktionsspezifische Daten eingetragen, z.B.: die Zielnummer bei einer Zielwahl. Mehr dazu erfahren Sie im n&auml;chsten Schritt.'); ?>
+		<?php echo __('In dieses Feld werden funktionsspezifische Daten eingetragen, z.B. die Zielnummer bei einer Zielwahl. Mehr dazu erfahren Sie im n&auml;chsten Schritt.'); ?>
 	</td>
 </tr>
 <tr>
@@ -84,7 +84,7 @@ echo '<img alt="Step2" src="'.GS_URL_PATH.'img/help_keys/step2.jpg"/>';
 <tr>
 	<td><?php echo __('Gesch&uuml;tzt?'); ?></td>
 	<td>
-		<?php echo __('Wenn in dieser Spalte ein kleines Schloss angezeigt wird (im Beispiel in den letzten beiden Zeilen) dann ist diese Taste vom Administrator gegen eine Bearbeitung gesch&uuml;tzt. Sie k&ouml;nnen diese Taste nicht Konfigurieren.'); ?>
+		<?php echo __('Wenn in dieser Spalte ein kleines Schloss angezeigt wird (im Beispiel in den letzten beiden Zeilen), dann ist diese Taste vom Administrator gegen eine Bearbeitung gesch&uuml;tzt. Sie k&ouml;nnen diese Taste nicht konfigurieren.'); ?>
 	</td>
 </tr>
 </tbody>
@@ -92,11 +92,11 @@ echo '<img alt="Step2" src="'.GS_URL_PATH.'img/help_keys/step2.jpg"/>';
 <br><br>
 <?php
 echo '<h3>' .__("Schritt 3:") .'</h3>';
-echo __("Aktivieren Sie die Taste durch anklicken des Hakens in der 2. Spalte (1)")."<br>";
+echo __("Aktivieren Sie die Taste durch Anklicken des Hakens in der 2. Spalte (1)")."<br>";
 echo __("W&auml;hlen Sie aus der Liste (2) die gew&uuml;nschte Tastenfunktion (3) aus:")."<br>";
 echo '<img alt="Step3" src="'.GS_URL_PATH.'img/help_keys/step3.jpg"/>';
 echo '<br>';
-echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier eine Liste f&uuml;r das Openstage 60, welches die Tastenfunktionen der meisten Telefone abdecken sollte:")."<br>";
+echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t abh&auml;ngig. Hier eine Liste f&uuml;r das Openstage 60, welches die Tastenfunktionen der meisten Telefone abdecken sollte:")."<br>";
 ?>
 <br><br>
 <table cellspacing="1" class="phonebook">
@@ -109,7 +109,7 @@ echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier ei
 <tr>
 	<td style="width:140px;"> <?php echo __('-erben-'); ?></td>
 	<td style="width:420px;">
-		<?php echo __('Wenn "-erben-" aktiviert ist, wird die vom Administrator voreinstellte Funktion benutzt.'); ?>
+		<?php echo __('Wenn "-erben-" aktiviert ist, wird die vom Administrator voreingestellte Funktion benutzt.'); ?>
 	</td>
 </tr>
 <tr>
@@ -121,13 +121,13 @@ echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier ei
 <tr>
 	<td><?php echo __('Nebenstelle/BLF'); ?></td>
 	<td>
-		<?php echo __('Aktiviert die Besetztlampenfeldfunktion f&uuml;r eine bestimmte Nebenstelle. Mit der Besetztlampenfeldfunktion k&ouml;nnen Sie den Status der Nebenstelle sehen (Besetzt=Leuchten, Klingeln=Blinken). Desweiteren k&ouml;nnen Sie die Nebenstelle durch einen druck auf diese Taste Anrufen oder eine Ruf&uuml;bernahme durchf&uuml;hren, wenn Sie die Taste dr&uuml;cken w&auml;hrend die Besetztlampe blinkt.'); ?>
+		<?php echo __('Aktiviert die Besetztlampenfeldfunktion f&uuml;r eine bestimmte Nebenstelle. Mit der Besetztlampenfeldfunktion k&ouml;nnen Sie den Status der Nebenstelle sehen (Besetzt=Leuchten, Klingeln=Blinken). Desweiteren k&ouml;nnen Sie die Nebenstelle durch einen Druck auf diese Taste anrufen oder eine Ruf&uuml;bernahme durchf&uuml;hren, wenn Sie die Taste dr&uuml;cken w&auml;hrend die Besetztlampe blinkt.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Rufton aus'); ?></td>
 	<td>
-		<?php echo __('Schaltet den Rufton der Endger&auml;tes an oder aus. Wenn der Rufton aus ist, leuchtet die Lampe der Taste.'); ?>
+		<?php echo __('Schaltet den Rufton des Endger&auml;tes an oder aus. Wenn der Rufton aus ist, leuchtet die Lampe der Taste.'); ?>
 	</td>
 </tr>
 <tr>
@@ -157,31 +157,31 @@ echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier ei
 <tr>
 	<td><?php echo __('Weiterleiten'); ?></td>
 	<td>
-		<?php echo __('Mit dieser Funktion k&ouml;nnen sie den Anrufer zu einem bestimmten Ziel weiterleiten.'); ?>
+		<?php echo __('Mit dieser Funktion k&ouml;nnen Sie den Anrufer zu einem bestimmten Ziel weiterleiten.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Shift'); ?></td>
 	<td>
-		<?php echo __('Mit der Shift-Funktion hat man die M&ouml;glichkeit Tasten doppelt zu belegen. Die Funktion schaltet zwischen den Ebenen hin und her. Die eigentlichen Tastenfunktionen kann man dann in "Shift-Ebene" festlegen'); ?>
+		<?php echo __('Mit der Shift-Funktion hat man die M&ouml;glichkeit, Tasten doppelt zu belegen. Die Funktion schaltet zwischen den Ebenen hin und her. Die eigentlichen Tastenfunktionen kann man dann in "Shift-Ebene" festlegen.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Kopfh&ouml;rer'); ?></td>
 	<td>
-		<?php echo __('Mit dieser Taste k&ouml;nnen Sie das gespr&auml;ch zwischen Kopfh&ouml;rer und Lautsprecher umschalten.'); ?>
+		<?php echo __('Mit dieser Taste k&ouml;nnen Sie das Gespr&auml;ch zwischen Kopfh&ouml;rer und Lautsprecher umschalten.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Nicht st&ouml;ren'); ?></td>
 	<td>
-		<?php echo __('Sie k&ouml;nnen diese Funktion aktvieren, falls Sie z.B.: im Kundengespr&auml;ch sind und nicht gest&ouml;rt werden wollen. Das Telefon wird dann nicht klingeln und auch nicht Anzeigen wenn es angerufen wird.'); ?>
+		<?php echo __('Sie k&ouml;nnen diese Funktion aktivieren, falls Sie z.B. im Kundengespr&auml;ch sind und nicht gest&ouml;rt werden wollen. Das Telefon wird dann nicht klingeln und auch nicht anzeigen, wenn es angerufen wird.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Rufanahmegrp.'); ?></td>
 	<td>
-		<?php echo __('Wenn Sie in einer Rufanahmegruppe sind, k&ouml;nnen Sie ein ankommendes Gespr&auml;ch mit dieser Taste aufnehmen.'); ?>
+		<?php echo __('Wenn Sie in einer Rufannahmegruppe sind, k&ouml;nnen Sie ein ankommendes Gespr&auml;ch mit dieser Taste aufnehmen.'); ?>
 	</td>
 </tr>
 <tr>
@@ -193,19 +193,19 @@ echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier ei
 <tr>
 	<td><?php echo __('R&uuml;ckfrage'); ?></td>
 	<td>
-		<?php echo __('Setzt den Anrufer auf "Halten" und Ruft die Nummer die bei Nummer/Daten eingegeben wurden für eine R&uuml;ckfrage an.'); ?>
+		<?php echo __('Setzt den Anrufer auf "Halten" und ruft die Nummer, die bei Nummer/Daten eingegeben wurde, für eine R&uuml;ckfrage an.'); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Fn.Schalter'); ?></td>
 	<td>
-		<?php echo __('Mit dem Funktionsschalter kann man bestimmte Telefoniefunktionen aktivieren oder Deaktivieren (z.B.: die Rufumleitung). W&auml;hlen Sie einfach die gew&uuml;nschte Funktion aus dem Hilfe-Dialog aus. '); ?>
+		<?php echo __('Mit dem Funktionsschalter kann man bestimmte Telefoniefunktionen aktivieren oder deaktivieren (z.B. die Rufumleitung). W&auml;hlen Sie einfach die gew&uuml;nschte Funktion aus dem Hilfe-Dialog aus. '); ?>
 	</td>
 </tr>
 <tr>
 	<td><?php echo __('Appl. aufrufen'); ?></td>
 	<td>
-		<?php echo __('Erm&ouml;glicht das Belegen einer Appliaktion auf eine Taste, damit ist es m&ouml;glich, z.B.: das Telefonbuch zu starten. W&auml;hlen Sie einfach die gew&uuml;nschte Applikation aus dem Hilfe-Dialog aus.'); ?>
+		<?php echo __('Erm&ouml;glicht das Belegen einer Applikation auf eine Taste, damit ist es m&ouml;glich, z.B. das Telefonbuch zu starten. W&auml;hlen Sie einfach die gew&uuml;nschte Applikation aus dem Hilfe-Dialog aus.'); ?>
 	</td>
 </tr>
 </tbody>
@@ -214,16 +214,16 @@ echo __("Die Tastenfunktionen sind stark vom benutzten Endger&auml;t ab. Hier ei
 <?php
 echo '<br>';
 echo '<h3>' .__("Schritt 4:") .'</h3>';
-echo __("Bei manchen Tastenfunktionen bietet das Programm eine Hilfestellung zum ausf&uuml;llen der 3.Spalte 'Nummern/Daten' an, wenn man - wie im Beispiel - 'Nebenstelle/BLF' ausw&auml;hlt, dann kommt dieser Dialog:")."<br>";
+echo __("Bei manchen Tastenfunktionen bietet das Programm eine Hilfestellung zum Ausf&uuml;llen der 3.Spalte 'Nummern/Daten' an, wenn man - wie im Beispiel - 'Nebenstelle/BLF' ausw&auml;hlt, dann kommt dieser Dialog:")."<br>";
 echo '<img alt="Step4" src="'.GS_URL_PATH.'img/help_keys/step4.jpg"/>';
 echo '<br>';
-echo __("In diesem Dialog k&ouml;nnen Sie die Nummer (1) die Sie im Besetztlampenfeld anzeigen wollen eintragen und den Dialog mit dem Haken (2) best&auml;tigen.")."<br>";
+echo __("In diesem Dialog k&ouml;nnen Sie die Nummer (1) die Sie im Besetztlampenfeld anzeigen wollen, eintragen und den Dialog mit dem Haken (2) best&auml;tigen.")."<br>";
 echo '<br>';
 echo '<br>';
 
 
 echo '<h3>' .__("Schritt 5:") .'</h3>';
-echo __("Dannach sehen Sie, wie der Hilfedialog die Daten in Spalte 3 eingetragen hat, Sie m&uuml;ssen an diesem Text nichts mehr &auml;ndern. Sie k&ouml;nnen nun noch eine Beschriftung (1) eingeben, die die Taste am Telefon haben soll:")."<br>";
+echo __("Dannach sehen Sie, wie der Hilfedialog die Daten in Spalte 3 eingetragen hat. Sie m&uuml;ssen an diesem Text nichts mehr &auml;ndern. Sie k&ouml;nnen zus&auml;tzlich noch eine Beschriftung (1) eingeben, die die Taste am Telefon haben soll:")."<br>";
 echo '<img alt="Step5" src="'.GS_URL_PATH.'img/help_keys/step5.jpg"/>';
 echo '<br>';
 echo '<br>';
