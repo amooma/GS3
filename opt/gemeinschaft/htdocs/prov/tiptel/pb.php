9e9494eb40245a5d24db7435333103ed67ea818a
Sebastian Ertz <gemeinschaft@swastel.eisfair.net>
2010-06-08 12:46:50 +0200

    Tiptel Prov: Suchen im XML-Browser PB eingebaut. div kleinigkeiten (cherry picked from commit bae4ee146437aacce9cd16d34682ce7bd4e2e4bd)

--- opt/gemeinschaft/htdocs/prov/tiptel/pb.php
+++ opt/gemeinschaft/htdocs/prov/tiptel/pb.php
@@ -89,6 +89,9 @@ if (! in_array( $type, array('gs','prv','imported'), true )) {
 	$type = false;
 }
 
+$search = (int)trim( @$_REQUEST['s'] );
+$query  = trim( @$_REQUEST['q'] );
+
 
 $db = gs_db_slave_connect();
 
@@ -126,24 +129,26 @@ $url_snom_pb = GS_PROV_SCHEME .'://'. GS_PROV_HOST . (GS_PROV_PORT ? ':'.GS_PROV
 
 
 #################################### SEARCH SCREEN {
-/*if ($_REQUEST['s'] === '1') {
+if ($search === 1) {
+
+	$user = trim( @$_REQUEST['u'] );
+	$user_id = getUserID( $user );
+	
 	ob_start();
 	
 	echo '<?','xml version="1.0" encoding="utf-8"?','>',"\n";
 	echo 
 		'<TiptelIPPhoneInputScreen>', "\n",
 		'<Title>Suchen</Title>', "\n",
+		'<URL>', tiptelXmlEsc($url_snom_pb.'?u='.$user.'&t='.$type) ,'</URL>', "\n",
 		'<InputField type="string" password="no" editable="yes">', "\n",
 			'<Prompt>Suche nach</Prompt>', "\n",
-			'<URL>', tiptelXmlEsc($url_snom_pb) ,'</URL>', "\n",
-			'<Parameter>search</Parameter>',"\n",
-			'<Selection>1</Selection>', "\n",
+			'<Parameter>q</Parameter>',"\n",
 		'</InputField>', "\n",
 		'</TiptelIPPhoneInputScreen>', "\n";
 
-
 	_ob_send();
-}*/
+}
 #################################### SEARCH SCREEN }
 
 
@@ -189,13 +194,17 @@ if (! $type) {
 
 #################################### PHONEBOOK {
 if ($type === 'gs') {
-
+	
 	$user = trim( @ $_REQUEST['u'] );
 	$user_id = getUserID( $user );
 	
 	$user_groups       = gs_group_members_groups_get(array($user_id), 'user');
 	$permission_groups = gs_group_permissions_get($user_groups, 'phonebook_user');
 	$group_members     = gs_group_members_get($permission_groups);
+	
+	$where = '';
+	if (strLen($query) > 0)
+		$where = 'AND (`u`.`lastname` LIKE \'%'. $query .'%\' OR `u`.`firstname` LIKE \'%'. $query .'%\')';
 	
 	$query =
 'SELECT `u`.`lastname` `ln`, `u`.`firstname` `fn`, `s`.`name` `ext`
@@ -205,29 +214,36 @@ FROM
 WHERE
 	`u`.`id` IN ('.implode(',',$group_members).') AND
 	`u`.`id`!='.$user_id.'
+	'. ($where ? $where : '') .'
 ORDER BY `u`.`lastname`, `u`.`firstname`';
 //LIMIT '. $num_results;
 	
 }
 elseif ($type === 'prv') {
-
+	
 	$user = trim( @ $_REQUEST['u'] );
 	$user_id = getUserID( $user );
-
+	
+	$where = '';
+	if (strLen($query) > 0)
+		$where = 'AND (`pb`.`lastname` LIKE \'%'. $query .'%\' OR `pb`.`firstname` LIKE \'%'. $query .'%\')';
+	
 	$query =
 'SELECT `pb`.`lastname` `ln`, `pb`.`firstname` `fn`, `pb`.`number` `ext`
 FROM `pb_prv` `pb`
-WHERE `pb`.`user_id`='.$user_id.'
+WHERE
+	`pb`.`user_id`='.$user_id.'
+	'. ($where ? $where : '') .'
 ORDER BY `pb`.`lastname`, `pb`.`firstname`';
-
+	
 }
 elseif ($type === 'imported') {
-
+	
 	$query =
 'SELECT `lastname` `ln`, `firstname` `fn`, `number` `ext`
 FROM `pb_ldap`
 ORDER BY `lastname`, `firstname`';
-
+	
 }
 	
 if (in_array( $type, array('gs','prv','imported'), true )) {
@@ -257,8 +273,8 @@ if (in_array( $type, array('gs','prv','imported'), true )) {
 			'</SoftKey>', "\n",
 			"\n",
 			'<SoftKey index="2">', "\n",
-				'<Label>', /*tiptelXmlEsc(__('Suchen')),*/ '</Label>', "\n",
-				'<URI>', /*tiptelXmlEsc($url_snom_pb.'?u='.$user.'&t='.$type.'&s=1'),*/ '</URI>', "\n",
+				'<Label>', tiptelXmlEsc(__('Suchen')), '</Label>', "\n",
+				'<URI>', tiptelXmlEsc($url_snom_pb.'?u='.$user.'&t='.$type.'&s=1'), '</URI>', "\n",
 			'</SoftKey>', "\n",
 			"\n",
 			'<SoftKey index="3">', "\n",
@@ -270,7 +286,7 @@ if (in_array( $type, array('gs','prv','imported'), true )) {
 				'<Label>', tiptelXmlEsc(__("W\xC3\xA4hlen")), '</Label>', "\n",
 				'<URI>SoftKey:Dial</URI>', "\n",
 			'</SoftKey>', "\n";
-	
+		
 		echo '</TiptelIPPhoneDirectory>', "\n";
 		
 	} else {
