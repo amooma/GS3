3a6f0b3e01c53f2681f879bca131ed4edad5daa0
Henning Holtschneider <henning@loca.net>
2010-07-27 10:58:16 +0200

    Anpassungen fuer QueueMetrics 1.6.1 (Hotdesking) Neue Version von qloader.pl

--- usr/share/doc/gemeinschaft/qloader.pl
+++ usr/share/doc/gemeinschaft/qloader.pl
@@ -1,29 +1,8 @@
 #! /usr/bin/perl
+
 #
 # Upload a given queue_log file to a partition of the queue_log table.
-#
-# $Revision: 5318 $
-#
-# modified from the original version (c) 2005-2008 Loway to support 
-# agents in Gemeinschaft
-#
-# Copyright 2008, LocaNet oHG
-# Author: Henning Holtschneider <henning@loca.net> - LocaNet oHG
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-# MA 02110-1301, USA.
+# $Id: qloader.pl,v 1.18 2010/01/20 10:17:30 marcos Exp $
 #
 # usage:
 # queueLoader.pl [flags] /my/queue_log/file partition_name /my/activity/log
@@ -34,6 +13,11 @@
 #  -u user       : user name
 #  -p password   : password
 #
+# ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION  
+# If this file does not seem to work from the shell, do a 
+#        dos2unix queueLoader.pl
+# to set things right.
+# ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION  ATTENTION  
 #
 
 use strict;
@@ -54,18 +38,21 @@ my $dberr  = 1;       # setto a 0 quando tutto va bene
 my $file	  = $ARGV[0] || "/var/log/asterisk/queue_log";
 my $partition = $ARGV[1] || "P000";
 my $importLog = $ARGV[2] || "/var/log/asterisk/qloader.log";
+my $breakloop = $ARGV[3] || "0";
 
 my $pidfile   = "/var/run/qloader.pid";
 
 my $log_every_num = 100;
+my $log_queries   = 0;            # set to 1 to log all queries 
 
 my $timezone_offset  = 0 * 3600;  # in seconds
 my $heartbeat_delay  = 15 * 60;   # in seconds
-my $use_subqueue     = 1;         # 0 no; 1 yes
+my $use_subqueue     = 0;         # 0 no; 1 yes
 my $split_subq_name  = 0;         # 0 no; 1 yes - turn a subqueue name from 'xxx/yyy" to "xxx"
-my $rewriteToAgent   = 1;         # 0 no; 1 yes
+my $rewriteToAgent   = 0;         # 0 no; 1 yes
 my @channelsToAgent  = ( 'Local', 'SIP' );
 my $dbAgentRewrite   = 0;         # 0 no; 1 yes - rewrite according to rules in table qlog_rewrite
+
 
 # =================================================================
 # 
@@ -74,11 +61,12 @@ my $dbAgentRewrite   = 0;         # 0 no; 1 yes - rewrite according to rules in 
 # =================================================================
 
 
-syslog( "QueueMetrics MySQL loader - " . '$Revision: 5318 $' );
-syslog( "Partition $partition - PID $$ - TZ offset: $timezone_offset s. - Heartbeat after $heartbeat_delay s." );
+syslog( "QueueMetrics MySQL loader - " . '$Revision: 1.18 $' );
+syslog( "Partition $partition - PID $$ - TZ offset: $timezone_offset s. - Heartbeat after $heartbeat_delay s." ) if ($breakloop eq "0");
+syslog( "Partition $partition - PID $$ - TZ offset: $timezone_offset s." ) unless ($breakloop eq "0");
 #syslog( "H: $mysql_host D: $mysql_db U: $mysql_user P: $mysql_pass" );
 
-savePID();
+savePID() if ($breakloop eq "0");
 
 # controlla il valore massimo del timestamp
 my $highWaterMark = scalarQuery( "SELECT max(time_id) FROM queue_log WHERE partition='$partition' AND verb != 'HEARTBEAT'" );
@@ -96,36 +84,36 @@ my $nImportLines = 0;
 my $tstLastInsert = 0;
 my $last_query = time();
 
-my %agentToSipMap = {};
-
 open FX, $file or die "$! $file";
 while ( 1 ) {
 	while ( <FX> ) {
+		# innanzi tutto, controllo di avere una riga completa
+		# se non c'e', loop
+		if ( index( $_, "\n") < 0 ) {
+			seek(FX, -length($_), 1);
+			syslog( "Incomplete line read: pushing back '$_'" );
+			sleep 1;
+			next;
+		}
+		
 		# ottiene la riga
 		# aggiusta il primo valore con l'offset orario
+		syslogquery( "Row: " . $_ );
 		my @rowdata  = ((split /\|/, $_), ( "", "", "", "", "", "", "")) ;
+
+                # skip all lines with an invalid offset
+                next unless ($rowdata[0] =~ m/^\d{10}$/);
+
 		$rowdata[0] = ($rowdata[0] * 1.0) + $timezone_offset;
-		
+
 		my $tst = $rowdata[0];
-		
-		if ($rowdata[4] eq 'ADDMEMBER') {
-			chomp($rowdata[5]);
-			$agentToSipMap{$rowdata[5]} = $rowdata[3];
-		}
-                
-		if ($rowdata[4] eq 'REMOVEMEMBER') {
-			chomp($rowdata[5]);
-			undef $agentToSipMap{$rowdata[5]};
-		}
 		
 		if ($use_subqueue) {
 			$rowdata[2] = rewriteSubqueue( @rowdata );
 		}
 	
 		if ( $rewriteToAgent ) {
-			if ($agentToSipMap{$rowdata[3]}) {
-				$rowdata[3] = $agentToSipMap{$rowdata[3]};
-			}
+			$rowdata[3] = rewriteToAgent( $rowdata[3] );
 		}
 		
 		if ( $dbAgentRewrite ) {
@@ -151,6 +139,14 @@ while ( 1 ) {
 		}
 		
 	}
+    
+    	# Terminate the loop if requested by the command line parameters
+    	unless ($breakloop eq "0")
+    	{
+        	syslog("Exiting at the end of file, as requested by user parameters");
+        	last;
+    	}
+    
 	sleep 1;
 	checkHeartBeat();
 }
@@ -175,15 +171,16 @@ sub insertRow {
 	
 	# salta le linee che non hanno verbo
 	if ( length($verb) == 0 ) {
+		syslog( "Skipping line without verb at $tst $cid $que $age " );
 		return;
 	}
 	
 	($tst, $cid, $que, $age, $verb, $d1, $d2, $d3)  = map { myQuote($_) } ($tst, $cid, $que, $age, $verb, $d1, $d2, $d3);
 		
 	my $sql = " INSERT INTO `queue_log` 
-		( `partition` , `time_id` , `call_id` , `queue` , `agent` , `verb` , `data1` , `data2` , `data3` , `data4` ) 
-		VALUES 
-		( '$partition', $tst, $cid, $que, $age, $verb, $d1, $d2, $d3, '' );";
+	        ( `partition` , `time_id` , `call_id` , `queue` , `agent` , `verb` , `data1` , `data2` , `data3` , `data4` ) 
+	         VALUES 
+	        ( '$partition', $tst, $cid, $que, $age, $verb, $d1, $d2, $d3, '' );";
 	
 	execQuery( $sql );
 	
@@ -197,9 +194,16 @@ sub checkExistingRow {
 	($tst, $cid, $que, $age, $verb, $d1, $d2, $d3)  = map { myQuote($_) } ($tst, $cid, $que, $age, $verb, $d1, $d2, $d3);
 
 	my $sql = "SELECT COUNT(*) 
-			FROM queue_log
-			WHERE partition = '$partition' and time_id=$tst
-			and call_id = $cid and verb=$verb ";
+	             FROM queue_log
+	             WHERE partition = '$partition' 
+	               and time_id=$tst
+	               and call_id = $cid
+	               and queue=$que
+	               and agent=$age 
+	               and verb=$verb 
+	               and data1=$d1
+	               and data2=$d2
+	               ";
 
 	my $nRows = scalarQuery( $sql ) * 1.0;
 	
@@ -255,19 +259,19 @@ sub rewriteSubqueue() {
 sub getSubQueue() {
 	my ( $part, $cid ) = @_;
 	return scalarQuery( "SELECT subqueue FROM qlog_opencalls "
-				." WHERE callId='$cid' AND partition = '$part' " );
+	                   ." WHERE callId='$cid' AND partition = '$part' " );
 }
 
 sub updateCall() {
 	my ( $part, $cid, $queue, $sq, $verb, $tst ) = @_;
 	updateOrInsert( 
 		"UPDATE qlog_opencalls 
-		SET queue = '$queue', subqueue='$sq', lastVerb='$verb', lastTst = '$tst', lastUpd = NOW()
-		WHERE callId='$cid' AND partition = '$part' ",
+		    SET queue = '$queue', subqueue='$sq', lastVerb='$verb', lastTst = '$tst', lastUpd = NOW()
+		    WHERE callId='$cid' AND partition = '$part' ",
 		"INSERT INTO qlog_opencalls
-		( callId, partition, queue, subqueue, lastVerb, lastTst, lastUpd ) values
-		( '$cid', '$part', '$queue', '$sq', '$verb', '$tst', NOW() ) "
-	);
+		    ( callId, partition, queue, subqueue, lastVerb, lastTst, lastUpd ) values
+		    ( '$cid', '$part', '$queue', '$sq', '$verb', '$tst', NOW() ) "
+	    );
 }
 
 sub getSubQueueName() {
@@ -294,8 +298,9 @@ sub rewriteToAgent() {
 	foreach $ctype ( @channelsToAgent ) {
 		if ($channel =~ /^${ctype}\/([a-zA-Z0-9_]+)/i ) {
 			return "Agent/$1";
-		}
-	}	
+		} 
+	}
+	
 	return $channel;
 }
 
@@ -350,6 +355,8 @@ sub execSafeQuery {
 	my ($query, $returnScalar) = @_;
 	my $retval = '';
 	
+	syslogquery( "Attempting query: $query" );
+	
 	while ( $dberr > 0 ) {
 		reconnectDb();		
 	}	
@@ -357,10 +364,15 @@ sub execSafeQuery {
 	my $sth = $dbh->prepare($query);
 	$sth->execute();
 	
+	
+	
 	if ( length( $sth->errstr ) > 0 ) {
+		syslogquery( "ERROR: " . $sth->errstr );
 		reconnectDb();
+		syslogquery( "Retrying query: " . $query );
 		$sth = $dbh->prepare($query);
-		$sth->execute() or die $sth->errstr;;			
+		$sth->execute() or die $sth->errstr;			
+		syslogquery( "Query run: " . $query );
 	}
 	
 	if ( $returnScalar == 1 ) {
@@ -381,6 +393,7 @@ sub execSafeQuery {
 
 
 	$last_query = time();	
+	syslogquery( "Returns: " . $retval );
 	return $retval;
 	
 }
@@ -388,17 +401,18 @@ sub execSafeQuery {
 sub reconnectDb {
 	# tenta la riconnessione
 	# manda una query
-	# riprova finchè il risultato della query non è corretto
+	# riprova finchï¿½ il risultato della query non ï¿½ corretto
 	my $expval = 2006;
 	my $retval = -1;
 	while ( $expval != $retval ) {
 		syslog( "Now connecting to DB $mysql_db on $mysql_host as user $mysql_user with password $mysql_pass" );
 		$dbh = DBI->connect( "DBI:mysql:dbname=$mysql_db;host=$mysql_host", $mysql_user, $mysql_pass,
-			{
-				PrintError => 1,
-				ShowErrorStatement => 1,
-				HandleError => \&errDb
-			}
+		       {
+		       	#RaiseError => 1,
+		       	PrintError => 1,
+		       	ShowErrorStatement => 1,
+		       	HandleError => \&errDb
+			    }
 		);
 		
 		# testa la connessione
@@ -455,7 +469,7 @@ sub trim {
 }
 
 sub updateOrInsert() {
-	# se l'update è su 0 righe, faccio la insert
+	# se l'update ï¿½ su 0 righe, faccio la insert
 	my ( $sqlUpdate, $sqlInsert ) = @_;
 	
 	my $nRows = execSafeQuery( $sqlUpdate, 2 );
@@ -483,8 +497,17 @@ sub syserr {
 	syslogger( "E", $s );
 }
 
+sub syslogquery {
+	my ($s) = @_;
+	if ( $log_queries > 0 ) {
+		syslogger( "Q", $s );
+	}
+}
+
+
 sub syslogger {
 	my ($l, $s) = @_;
+	$s =~ s/\n/ /gm;
 #	print "$l|$s\n";
 	open EL, ">>$importLog" or die "$! $importLog";
 	print EL "$l|" . (scalar localtime) . "|$s\n";
@@ -497,3 +520,69 @@ sub savePID {
 	print FP $$;
 	close FP;
 }
+
+
+
+#
+# $Log: qloader.pl,v $
+# Revision 1.18  2010/01/20 10:17:30  marcos
+# #792: Overflow on timestamps have to invalidate the line.
+#
+# Revision 1.17  2009/03/30 08:14:38  lenz-mobile
+# Bug #670: handling of incomplete rows.
+#
+# Revision 1.16  2009/03/27 10:05:21  lenz-mobile
+# Minor fixes in logging
+#
+# Revision 1.15  2009/03/27 08:58:24  lenz-mobile
+# Better syncing algorithm
+#
+# Revision 1.14  2009/03/27 08:51:51  lenz-mobile
+# Extra-verbose query logging
+#
+# Revision 1.13  2009/02/04 11:50:44  marcos
+# SavePid is not required for not daemon mode.
+#
+# Revision 1.12  2009/01/22 17:22:59  marcos
+# #586: NFON issue #6: Partial database update of already uploaded queue data activities
+#
+# Revision 1.11  2008/05/16 14:16:39  lenz
+# Ver 1.11
+#
+# Revision 1.10  2008/05/16 14:15:28  lenz
+# Parameters on the command line.
+#
+# Revision 1.9  2008/01/19 16:29:57  lenz
+# Database-driven agent rewriting.
+# Minor fixes to Perl database access code.
+#
+# Revision 1.8  2007/08/05 11:12:56  lenz
+# Bug #179: Ignoring HEARTBEAT record when finding High Water Mark
+#
+# Revision 1.7  2007/05/07 16:10:10  lenz
+# Documentazione.
+# Spente caratteristiche Fancy nella versione distribuita.
+#
+# Revision 1.6  2007/04/01 16:49:02  lenz
+#
+# Toglie il nome della coda se espresso nel formato '0279/mia coda' per la riscrittura dinamica.
+# Tolto il log su STDOUT.
+#
+# Revision 1.5  2007/03/15 16:48:17  lenz
+# Versione corretta.
+#
+# Revision 1.4  2007/03/12 11:35:13  lenz
+# Riscrittura canali e subqueue
+#
+# Revision 1.3  2007/02/16 11:15:40  lenz
+# Added heartbeat and whitespace trimming.
+#
+# Revision 1.2  2006/09/08 23:58:53  lenz
+# Automagic timezone offset.
+#
+# Revision 1.1  2006/08/22 16:04:50  lenz
+# First CVS version.
+#
+#
+#
+
