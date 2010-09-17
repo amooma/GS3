922c238967ed920f909c241b40c431d0b4527a37
Andreas <neugebauer@loca.net>
2010-09-17 12:02:50 +0000

    Gefixt: Tabelle queue_callforwards, queue_cf_timerules, cf_timerules für die neuen Rufumleitungssachen, es haben div Spalten gefehlt, weswegen die Rufumleitung nicht funktionierte.
    git-svn-id: https://svn.amooma.com/gemeinschaft/trunk@7099 2d91074e-20bd-43ae-8273-9033e1f9829e
    (cherry picked from commit 7bb235801be4c4169b9f2383e14f909358d86a33)
    
    Conflicts:
    
    	usr/share/doc/gemeinschaft/asterisk.sql

--- usr/share/doc/gemeinschaft/asterisk.sql
+++ usr/share/doc/gemeinschaft/asterisk.sql
@@ -5763,7 +5763,7 @@ UNLOCK TABLES;
 --
 
 DROP TABLE IF EXISTS `cf_timerules`;
-CREATE TABLE IF NOT EXISTS `cf_timerules` (
+CREATE TABLE `cf_timerules` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `ord` int(10) unsigned NOT NULL,
   `_user_id` int(10) unsigned NOT NULL,
@@ -6705,12 +6705,14 @@ LOCK TABLES `queue_cf_parallelcall` WRITE;
 /*!40000 ALTER TABLE `queue_cf_parallelcall` ENABLE KEYS */;
 UNLOCK TABLES;
 
+
 --
 -- Table structure for table `queue_cf_timerules`
 --
 
 DROP TABLE IF EXISTS `queue_cf_timerules`;
 CREATE TABLE `queue_cf_timerules` (
+  `id` int(10) unsigned NOT NULL auto_increment,
   `_queue_id` int(10) unsigned NOT NULL,
   `ord` int(10) unsigned NOT NULL,
   `d_mo` tinyint(1) unsigned NOT NULL default '1',
@@ -6722,7 +6724,8 @@ CREATE TABLE `queue_cf_timerules` (
   `d_su` tinyint(1) unsigned NOT NULL default '1',
   `h_from` time NOT NULL default '00:00:00',
   `h_to` time NOT NULL default '24:00:00',
-  `target` varchar(20) character set ascii NOT NULL
+  `target` varchar(20) character set ascii NOT NULL,
+  PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
