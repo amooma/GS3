c9bf7fc2fac6350ba0a3a350ca4a5882fffebf38
Andreas <neugebauer@loca.net>
2010-09-23 15:49:49 +0000

    Revert "set_magic_quotes_runtime() is deprecated"
    This reverts commit bd84c7b5f592716448b4e93b9e33c04b624c0dd7.

--- opt/gemeinschaft/inc/conf.php
+++ opt/gemeinschaft/inc/conf.php
@@ -95,9 +95,7 @@ ini_set('output_handler', '');
 	//ini_set('zlib.output_compression_level', 5);
 	//ini_set('zlib.output_handler', '');
 //}
-
-if (get_magic_quotes_runtime()) set_magic_quotes_runtime(0);
-
+@set_magic_quotes_runtime(0);
 //set_include_path('.');
 set_time_limit(65);
 
