f15a3757126c2c5aa8292bda9db16878a6b13162
Philipp Kempgen <lists@kempgen.net>
2010-09-24 13:51:22 +0800

    Blooper gefixt

--- opt/gemeinschaft/htdocs/prov/twinkle/index.php
+++ opt/gemeinschaft/htdocs/prov/twinkle/index.php
@@ -1,28 +1,29 @@
 <?php
 /*******************************************************************\
- * *            Gemeinschaft - asterisk cluster gemeinschaft
- * * 
- * * $Revision$
- * * 
- * * Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
- * * http://www.amooma.de/
- * * Sascha Daniels <sd@alternative-solution.de> 
- * * 
- * * This program is free software; you can redistribute it and/or
- * * modify it under the terms of the GNU General Public License
- * * as published by the Free Software Foundation; either version 2
- * * of the License, or (at your option) any later version.
- * * 
- * * This program is distributed in the hope that it will be useful,
- * * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * * GNU General Public License for more details.
- * * 
- * * You should have received a copy of the GNU General Public License
- * * along with this program; if not, write to the Free Software
- * * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * * MA 02110-1301, USA.
- * \*******************************************************************/
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Sascha Daniels <sd@alternative-solution.de> 
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
 /*
  * Needs: login_user and login_pwd as parameter
  * Provides: twinkle profile
@@ -32,6 +33,7 @@
  * You should be online ;-)
  *
  */
+
 define('GS_VALID', true);
 require_once( dirName(__FILE__) .'/../../../inc/conf.php' );
 require_once( GS_DIR .'inc/gs-lib.php' );
