87a526c865bd8e5318732d1c3b9f23c9c044106e
pk (amooma) <philipp.kempgen@amooma.de>
2010-09-28 09:25:33 -0700

    GEMEINSCHAFT_VERS="3.1-rc1"

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@
 # http://creativecommons.org/licenses/by-nc-nd/3.0/de/
 
 
-#GEMEINSCHAFT_VERS="3.0"
-GEMEINSCHAFT_VERS="master"
+#GEMEINSCHAFT_VERS="master"
+GEMEINSCHAFT_VERS="3.1-rc1"
 
 GEMEINSCHAFT_TGZ_URL_DIR="https://github.com/amooma/GemeinschaftPBX/tarball"
 
