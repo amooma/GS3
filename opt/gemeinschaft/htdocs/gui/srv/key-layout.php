5a63cd3ed206beebbbf73ae047c6a5f2ef80a682
Henning Holtschneider <henning@loca.net>
2010-10-17 00:44:00 +0800

    Bugfix: Tastenbeschriftung fuer sudo_user korrekt erzeugen und Beschriftung fuer alle Erweiterungsmodule ausdrucken

--- opt/gemeinschaft/htdocs/gui/srv/key-layout.php
+++ opt/gemeinschaft/htdocs/gui/srv/key-layout.php
@@ -72,13 +72,16 @@ function _not_modified( $etag='', $attach=false, $fake_filename='' )
 function _get_key_label($knump)
 {
 	global $softkeys;
-
 	$key_defs = @$softkeys['f'.$knump];
 	if ( is_array($key_defs) ) {
 		if (array_key_exists('slf', $key_defs)) {
-			return $key_defs['slf']['label'];
+			if (strlen($key_defs['slf']['label']) > 12)
+				$key_defs['slf']['label'] = substr($key_defs['slf']['label'], 0, 10) . '...';
+			return utf8_decode($key_defs['slf']['label']);
 		} elseif (array_key_exists('inh', $key_defs)) {
-			return $key_defs['inh']['label'];
+			if (strlen($key_defs['inh']['label']) > 12)
+				$key_defs['inh']['label'] = substr($key_defs['inh']['label'], 0, 10) . '...';
+			return utf8_decode($key_defs['inh']['label']);
 		}
 	}
 	
@@ -93,7 +96,8 @@ class PDF extends FPDF {
 
 	function Header() {
 		$this->SetFont('Arial', 'B', 15);
-		$this->Cell(0, 10, __('Tastenbeschriftung') .' '. __('von') .' '. $this->firstname .' '. $this->lastname .' ('. $this->ext .')', 0, 0, 'C');
+		$label = __('Tastenbeschriftung') .' '. __('von') .' '. $this->firstname .' '. $this->lastname .' ('. $this->ext .')';
+		$this->Cell(0, 10, utf8_decode($label), 0, 0, 'C');
 		//$this->Ln(20);
 	}
 
@@ -115,12 +119,9 @@ class PDF extends FPDF {
 	
 
 
-
 $phone_types = array();
 if (gs_get_conf('GS_SNOM_PROV_ENABLED')) {
 	$enabled_models = preg_split('/[,\\s]+/', gs_get_conf('GS_PROV_MODELS_ENABLED_SNOM'));
-	if (in_array('*', $enabled_models) || in_array('300', $enabled_models))
-		$phone_types['snom-300'] = 'Snom 300';
 	if (in_array('*', $enabled_models) || in_array('320', $enabled_models))
 		$phone_types['snom-320'] = 'Snom 320';
 	if (in_array('*', $enabled_models) || in_array('360', $enabled_models))
@@ -139,33 +140,28 @@ if (gs_get_conf('GS_GRANDSTREAM_PROV_ENABLED')) {
 }
 
 
-
-
-if (! is_array($_SESSION)
-||  ! @array_key_exists('sudo_user', @$_SESSION)
-||  ! @array_key_exists('info'     , @$_SESSION['sudo_user'])
-||  ! @array_key_exists('id'       , @$_SESSION['sudo_user']['info']) )
-{
-	_not_allowed();
-}
-
-
 # get phone_type
 $phone_type = preg_replace('/[^a-z0-9\-]/', '', @$_REQUEST['phone_type']);
+
+$user_id = preg_replace('/[^0-9]/', '', @$_REQUEST['user_id']);
 
 if ( $phone_type == '' || ! array_key_exists($phone_type, $phone_types) ) { // TODO ? FIXME
 	_not_allowed();
 }
 
+if ( $user_id == ''  ) { // TODO ? FIXME
+	_not_allowed();
+}
+
 # get DB infos
-$user_id = (int)@$_SESSION['sudo_user']['info']['id'];
-$rs = $DB->execute( 'SELECT `u`.`lastname` `ln`, `u`.`firstname` `fn`, `s`.`name` `ext`
+$rs = $DB->execute( 'SELECT `u`.`user`, `u`.`lastname` `ln`, `u`.`firstname` `fn`, `s`.`name` `ext`
 FROM
 	`users` `u` JOIN
 	`ast_sipfriends` `s` ON (`s`.`_user_id`=`u`.`id`)
 WHERE `u`.`id`='. $user_id );
 $user = $rs->fetchRow();
 
+$user_name = $user['user'];
 
 # PDF erzeugen
 $pdf=new PDF('P', 'mm', 'A4');
@@ -180,18 +176,19 @@ $pdf->ext       = $user['ext'];
 
 foreach ($phone_types as $phone_type_name => $phone_type_title) {
 	if ($phone_type_name === $phone_type) {
-		
+	
 		$softkeys = null;
 		$GS_Softkeys = gs_get_key_prov_obj( $phone_type_name );
-		$GS_Softkeys->set_user( @$_SESSION['sudo_user']['name'] );
+		$GS_Softkeys->set_user( $user_name );
 		$GS_Softkeys->retrieve_keys( $phone_type_name );
 		$softkeys = $GS_Softkeys->get_keys();
-		
+				
 		$pdf->AddPage();
-		$pdf->SetFont('Arial', '', 12);
-		$pdf->SetFillColor(224, 235, 255);
+		$pdf->SetFont('Arial', '', 8);
+		//$pdf->SetFillColor(224, 235, 255);
+		$pdf->SetFillColor(100, 149, 237);
 		$pdf->SetTextColor(  0,   0,   0);
-		
+
 		$key_levels = null;
 		if (subStr($phone_type_name,0,4)==='snom') {
 			$phone_layout = 'snom';
@@ -199,18 +196,14 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 			$key_levels = array(
 				0 => array('from'=> 0, 'to'=>11, 'title'=>$phone_type_title             , 'X'=> 10, 'Y'=>30, 'width'=>19),
 				1 => array('from'=>12, 'to'=>32, 'title'=>__('Erweiterungs-Modul') .' 1', 'X'=> 75, 'Y'=>30, 'width'=>19),
-				2 => array('from'=>33, 'to'=>53, 'title'=>__('Erweiterungs-Modul') .' 2', 'X'=>140, 'Y'=>30, 'width'=>19)
+				2 => array('from'=>33, 'to'=>53, 'title'=>__('Erweiterungs-Modul') .' 2', 'X'=>140, 'Y'=>30, 'width'=>19),
+				3 => array('from'=>54, 'to'=>74, 'title'=>__('Erweiterungs-Modul') .' 3', 'X'=> 10, 'Y'=>30, 'width'=>19),
+				4 => array('from'=>75, 'to'=>95, 'title'=>__('Erweiterungs-Modul') .' 4', 'X'=> 75, 'Y'=>30, 'width'=>19)
 			);
-			
-			switch ($phone_type_name) {
-				case 'snom-300':
-					$key_levels[0]['to'] = 5;
-					break;
-			}
 			
 		} elseif (subStr($phone_type_name,0,11)==='grandstream') {
 			$phone_layout = 'grandstream';
-			
+
 			$key_levels = array(
 				0 => array('from'=> 0, 'to'=> 6, 'title'=>$phone_type_title                                   , 'X'=>10, 'Y'=>30, 'width'=>30),
 				1 => array('from'=> 0, 'to'=>13, 'title'=>$phone_type_title.' '.__('Erweiterungs-Modul') .' 1', 'X'=>10, 'Y'=>30, 'width'=>24),
@@ -218,33 +211,35 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 			);
 			
 			switch ($phone_type_name) {
-				case 'grandstream-gxp2000':
-					$key_levels[1]['width'] = 14.7;
-					$key_levels[2]['width'] = 14.7;
-					break;
-				case 'grandstream-gxp2010':
-					$key_levels[0]['to']    =  8;
-					$key_levels[0]['width'] = 24;
-					break;
+			case 'grandstream-gxp2000':
+				$key_levels[1]['width'] = 14.7;
+				$key_levels[2]['width'] = 14.7;
+				break;
+			case 'grandstream-gxp2010':
+				$key_levels[0]['to']    =  8;
+				$key_levels[0]['width'] = 24;
+				break;
 			}
 			
 		}
-		
+
 		/*$pdf->SetDash(0.5,1); //TODO Schnittlinien
 		$pdf->Line(10+20, 30+15, 10+20, 30+10);
 		$pdf->SetDash();*/
 		
-		
+
 		foreach ($key_levels as $key_level_idx => $key_level_info) {
-			
+
 			$fill = false;
-			
+
 			switch ($phone_layout) {
 				case 'snom':
 					switch ($key_level_idx) {
 						case 0: $left =  0; $right =  6; $fill = false; break;
 						case 1: $left = 12; $right = 23; $fill = true;  break;
 						case 2: $left = 33; $right = 44; $fill = true;  break;
+						case 3: $left = 54; $right = 65; $fill = true;  break;
+						case 4: $left = 75; $right = 86; $fill = true;  break;
 					}
 					break;
 			}		
@@ -256,7 +251,7 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 			$pdf->Ln(10);
 			
 			for ($i=$key_level_info['from']; $i<=$key_level_info['to']; ++$i) {
-				
+
 				if ($phone_layout === 'snom') {
 					$knum  = ($i%2===($key_level_idx+1)%2 ? $left : $right);
 					$knump = str_pad($knum, 3, '0', STR_PAD_LEFT);
@@ -268,7 +263,6 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 				$keynum = $knum + 1; //TODO ? FIXME
 				
 				if ($phone_layout==='snom') {
-					
 					$pdf->SetX($key_level_info['X']);
 					
 					# set border (L = Left, R = Right, T = Top, B = Bottom)
@@ -280,9 +274,26 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 					$height = 6;
 					
 					# Table
-					$pdf->Cell($width, $height, ($i%2===($key_level_idx+1)%2 ? $keynum : ''), 1      , 0, 'C'             , false);
-					$pdf->Cell($width, $height, _get_key_label($knump)                      , $border, 0, ($i%2 ? 'L':'R'), $fill);
-					$pdf->Cell($width, $height, ($i%2===($key_level_idx+1)%2 ? '' : $keynum), 1      , 0, 'C'             , false);
+					switch ($key_level_idx) {
+						case 0:
+						case 2:
+						case 4:
+							if ($i%2)
+								$align = 'L';
+							else
+								$align = 'R';
+							break;
+						case 1:
+						case 3:
+							if ($i%2)
+								$align = 'R';
+							else
+								$align = 'L';
+							break;
+					}
+					$pdf->Cell($width, $height, ($i%2===($key_level_idx+1)%2 ? $keynum : ''), 1      , 0, 'C'   , false);
+					$pdf->Cell($width, $height, _get_key_label($knump)                      , $border, 0, $align, $fill);
+					$pdf->Cell($width, $height, ($i%2===($key_level_idx+1)%2 ? '' : $keynum), 1      , 0, 'C'   , false);
 					
 					$pdf->Ln();
 		                        
@@ -290,23 +301,23 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 					else ++$right;
 					
 				} elseif ($phone_layout==='grandstream') {
-					
+				
 					$pdf->SetX($key_level_info['X']);
-					
+
 					# set border (L = Left, R = Right, T = Top, B = Bottom)
 					$border = 'LR';
 					if ($i==$key_level_info['from']) $border .= 'T';
 					if ($i==$key_level_info['to'  ]) $border .= 'B';
 					
 					$width = $key_level_info['width'];
-					
+
 					if ($key_level_idx == 0) {
 						$height = 10 + ($i%2 ? 1 : 0);
 						
 						# Spalte 1
 						$pdf->Cell(20, $height, $keynum               , 1      , 0, 'C', false);
 						$pdf->Cell($width, $height, _get_key_label($knump), $border, 0, 'C', $fill);
-						
+
 						if ($phone_type_name === 'grandstream-gxp2010') {
 							# Spalte 2 (only gxp2010)
 							$knump = str_pad($knum+9, 4, '0', STR_PAD_LEFT);
@@ -344,26 +355,28 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 						$knump = str_pad($knum + 28, 4, '0', STR_PAD_LEFT);
 						$pdf->Cell(20    , $height, $keynum + 28          , 1      , 0, 'C', false );
 						$pdf->Cell($width, $height, _get_key_label($knump), $border, 0, 'C', $fill);
-						
+
 						# Spalte 4
 						$knump = str_pad($knum + 42, 4, '0', STR_PAD_LEFT);
 						$pdf->Cell($width, $height, _get_key_label($knump), $border, 0, 'C', !$fill);
 						$pdf->Cell(20    , $height, $keynum + 42          , 1      , 0, 'C', false );
-						
+
 					}
 					
 					$pdf->Ln();
 					
 				}
-				
+
 				$fill = !$fill;
+			}
+			
+			if ($phone_layout === 'snom' && $key_level_idx == 2) {
+				$pdf->AddPage();
 			}
 			
 			if ($phone_layout === 'grandstream' && $key_level_idx < 2 ) {
 				$pdf->AddPage();
-			} /*else {
-				$pdf->Ln();
-			}*/
+			}
 		}
 		
 	}
@@ -373,6 +386,6 @@ foreach ($phone_types as $phone_type_name => $phone_type_title) {
 
 $pdf->SetCreator('Gemeinschaft');
 $pdf->SetAuthor('Gemeinschaft');	
-$pdf->Output( __('Tastenbeschriftung').'_'.$_SESSION['sudo_user']['info']['user'].'_'.$phone_type.'.pdf', 'D');
+$pdf->Output( __('Tastenbeschriftung').'_'.$phone_type.'_'.$user_name.'.pdf', 'D');
 
 ?>
