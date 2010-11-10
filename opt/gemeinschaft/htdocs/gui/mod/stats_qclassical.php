1838ca612d5ce954e71ff4c356865e87b660d434
d-mark <dm@markwardt-software.de>
2010-11-10 12:41:30 +0100

    Workaround for PHP-Bug in date calculations (strToTime with months) in statistics

--- opt/gemeinschaft/htdocs/gui/mod/stats_qclassical.php
+++ opt/gemeinschaft/htdocs/gui/mod/stats_qclassical.php
@@ -102,7 +102,7 @@ if (count($queue_groups) > 0) {
 <label for="ipt-month"><?php echo __('Monat'); ?>:</label>
 <select name="month" id="ipt-month">
 <?php
-$t = time();
+$t = mktime(0,0,0,date("n"),1,date("Y"));
 for ($i=-3; $i<=0; ++$i) {
 	echo '<option value="',$i,'"', ($i==$month_d ? ' selected="selected"' : ''),'>', date('m / Y', (int)strToTime("$i months", $t)) ,'</option>' ,"\n";
 }
