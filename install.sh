918bf746ef9be498e3e3172cc342ef56a409a67e
Sascha Daniels <sd@alternative-solution.de>
2012-01-17 21:28:48 +0100

    Renamed repo

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ GEMEINSCHAFT_VERS="3.1"
 #GEMEINSCHAFT_VERS="3.1-rc3"
 
 #GEMEINSCHAFT_TGZ_URL_DIR="https://github.com/amooma/GemeinschaftPBX/tarball"
-GEMEINSCHAFT_CLONE_URL_DIR="http://github.com/amooma/GemeinschaftPBX.git"
+GEMEINSCHAFT_CLONE_URL_DIR="http://github.com/amooma/GS3.git"
 
 GEMEINSCHAFT_SIEMENS_VERS="trunk-r00358"
 GEMEINSCHAFT_SIEMENS_TGZ_IN_TGZ_DIR="misc/provisioning/siemens"
