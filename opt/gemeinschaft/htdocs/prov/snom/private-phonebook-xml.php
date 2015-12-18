0ede77e6af4ed16b7ff890598f4776baaef2be37
Markus Neubauer <neubauer@email-online.org>
2015-12-18 13:29:54 +0100

    Create private-phonebook-xml.php

---
+++ opt/gemeinschaft/htdocs/prov/snom/private-phonebook-xml.php
@@ -0,0 +1,43 @@
+<?php
+/*******************************************************************\
+*            Gemeinschaft - asterisk cluster gemeinschaft
+* 
+* $Revision$
+* 
+* Copyright 2007, amooma GmbH, Bachstr. 126, 56566 Neuwied, Germany,
+* http://www.amooma.de/
+* Stefan Wintermeyer <stefan.wintermeyer@amooma.de>
+* Philipp Kempgen <philipp.kempgen@amooma.de>
+* Peter Kozak <peter.kozak@amooma.de>
+* Soeren Sprenger <soeren.sprenger@amooma.de>
+* Markus Neubauer <markus.neubauer@email-online.org> - 2015
+*  - extending db and preparing for vcards
+* you need to alter the db for this version to work:
+* 
+* 
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+* MA 02110-1301, USA.
+\*******************************************************************/
+
+define('GS_VALID', true); // arriving nativ here
+
+header('Content-type: application/xml');
+
+?>
+<SnomIPPhoneDirectory>
+<?php
+include_once( '../private-phonebook-xml.inc' );
+?>
+</SnomIPPhoneDirectory>
